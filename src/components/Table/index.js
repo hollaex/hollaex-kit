9f632b88fbe01daa2772edffbb1163cc69bcca6e
Jose Miguel Navarro <jmn8718@gmail.com>
2017-10-25 10:40:32 +0900

    Refactor and split table in components

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import { IconTitle } from '../';
 
+import TableHeader from './TableHeader';
+import TableBody from './TableBody';
+// import TableFooter from './TableFooter';
 import Paginator from './paginator';
 
 const RENDER_CELL = (row, key) => <td key={key}>{row[key]}</td>;
@@ -15,6 +18,7 @@ class Table extends Component{
 
   componentDidMount() {
     this.goToPage(0, this.props.data, this.props.count);
+    this.setPageSize(this.props.pageSize);
   }
 
   componentWillReceiveProps(nextProps, nextState) {
@@ -23,55 +27,9 @@ class Table extends Component{
     }
   }
 
-  renderHeader = (headers = []) => (
-    <thead className="table_header-wrapper">
-      <tr className="table_header-row">
-        {headers.map(({ label }, index) => <td key={index}>{label}</td>)}
-      </tr>
-    </thead>
-  )
-
-  renderRow = (headers = []) =>  (row, rowIndex) => (
-    <tr className="table_body-row" key={`row_${rowIndex}`}>
-      {headers.map(({ key, renderCell = RENDER_CELL }, cellIndex) => renderCell(row, key, cellIndex))}
-    </tr>
-  );
-
-  renderBody = (headers = [], data = []) => (
-    <tbody className={classnames(
-      'table_body-wrapper',
-      {
-        'with_icon': this.props.withIcon || false,
-      }
-    )}>
-      {data.map(this.renderRow(headers))}
-    </tbody>
-  );
-
-  renderProps = ({ data, headers, count }) => {
-    return (
-      <div>
-        <div>count: {count}</div>
-        <div>headers: {JSON.stringify(headers)}</div>
-        <div>data: {JSON.stringify(data)}</div>
-      </div>
-    )
+  setPageSize = (pageSize = 10) => {
+    this.setState({ pageSize });
   }
-
-  renderFooter = () => {
-    return (
-      <tfoot className="table_footer-wrapper">
-        <tr className="table_footer-row">
-          <td>tf1</td>
-          <td>tf2</td>
-          <td>tf3</td>
-          <td>tf4</td>
-          <td>tf5</td>
-        </tr>
-      </tfoot>
-    )
-  }
-
 
   goToPreviousPage = () => {
     this.goToPage(this.state.page - 1, this.props.data, this.props.count);
@@ -92,17 +50,20 @@ class Table extends Component{
   }
 
   render() {
-    const { headers } = this.props;
-    const { data, page, pageSize } = this.state;
     const count = this.props.count || this.props.data.length;
+
     if (count === 0) {
       return <div className="d-flex justify-content-center align-items-center">NO DATA</div>;
     }
+
+    const { headers, withIcon } = this.props;
+    const { data, page, pageSize } = this.state;
+
     return (
       <div className="table_container">
         <table className={classnames('table-wrapper')}>
-          {this.renderHeader(headers)}
-          {this.renderBody(headers, data)}
+          <TableHeader headers={headers} />
+          <TableBody headers={headers} data={data} withIcon={withIcon} />
         </table>
         <Paginator
           currentPage={page + 1}
@@ -120,5 +81,7 @@ Table.defaultProps = {
   data: [],
   headers: [],
   withIcon: false,
-}
+  pageSize: 10,
+};
+
 export default Table;
