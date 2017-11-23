164f8527ea3074ee2f6f2cb086248cc68f869b78
Jose Miguel Navarro <jmn8718@gmail.com>
2017-11-23 12:25:51 +0900

    Add countdown component

--- src/components/index.js
+++ src/components/index.js
@@ -24,4 +24,5 @@ export { default as QuickTrade } from './QuickTrade';
 export { default as Legal } from './Legal';
 export { default as AuthForm } from './Form/AuthForm';
 export { default as HocForm } from './Form/HocForm';
+export { default as Countdown } from './Countdown';
 export { CandleChart, AreaChart } from './Chart';
