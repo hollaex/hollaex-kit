7de36b5048b4fe3ac609d8d88ecc5d1ebe48146f
Jose Miguel Navarro <jmn8718@gmail.com>
2017-12-29 23:24:16 +0100

    Adapt request email to use localized strings

--- src/containers/RequestResetPassword/RequestResetPasswordSuccess.js
+++ src/containers/RequestResetPassword/RequestResetPasswordSuccess.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import { ICONS } from '../../config/constants';
+import STRINGS from '../../config/localizedStrings';
 
 import { IconTitle, Button } from '../../components';
-
-import { REQUEST_RESET_PASSWORD_SUCCESS } from './constants';
 
 const RequestResetPasswordSuccess = ({ onLoginClick, onContactUs }) => (
   <div className="auth_wrapper">
     <IconTitle
       iconPath={ICONS.PASSWORD_RESET}
-      text={REQUEST_RESET_PASSWORD_SUCCESS.TITLE}
+      text={STRINGS.REQUEST_RESET_PASSWORD_SUCCESS.TITLE}
       textType="title"
       className="w-100"
     />
     <div className="text-center">
-      {REQUEST_RESET_PASSWORD_SUCCESS.TEXT}
+      {STRINGS.REQUEST_RESET_PASSWORD_SUCCESS.TEXT}
     </div>
     <div className="button-margin d-flex">
       <Button
-        label={REQUEST_RESET_PASSWORD_SUCCESS.BUTTON_1}
+        label={STRINGS.LOGIN_TEXT}
         onClick={onLoginClick}
       />
       <div className="separator"></div>
       <Button
-        label={REQUEST_RESET_PASSWORD_SUCCESS.BUTTON_2}
+        label={STRINGS.CONTACT_US_TEXT}
         onClick={onContactUs}
       />
     </div>
