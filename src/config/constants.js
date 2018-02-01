f6514ebccaa5a0a4db2d4663be23f89de40e8dac
Jose Miguel Navarro <jmn8718@gmail.com>
2018-02-02 00:14:22 +0100

    Fix .env files

--- src/config/constants.js
+++ src/config/constants.js
@@ -375,5 +375,4 @@ export const BLOCKTRAIL_ENDPOINT = `https://www.blocktrail.com/${
 export const BALANCE_ERROR = 'Insufficient balance to perform the order';
 
 export const CAPTCHA_SITEKEY =
-	process.env.REACT_APP_CAPTCHA_SITE_KEY ||
-	'6LcSOUIUAAAAAEbu2RXTpm-hdvQnTcTy12qG2y86';
+	process.env.REACT_APP_CAPTCHA_SITE_KEY;
