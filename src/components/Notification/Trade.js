5b890ec8aea5aed6e7c430af1aa79a839833f935
Jose Miguel <jmn8718@gmail.com>
2018-05-16 17:52:42 +0900

    Fix trade notification to use currencies

--- src/components/Notification/Trade.js
+++ src/components/Notification/Trade.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import math from 'mathjs';
-import { ICONS } from '../../config/constants';
+import { ICONS, PAIRS, CURRENCIES } from '../../config/constants';
 import STRINGS from '../../config/localizedStrings';
 import { formatBtcAmount, formatFiatAmount } from '../../utils/currency';
 import {
@@ -37,54 +37,58 @@ const getTitleAndIcon = (side, type) => {
 	return data;
 };
 
-const calculateValues = (data = []) => {
-	let btcAccumulated = math.fraction(0);
+const calculateValues = (data = [], pair) => {
+	let baseAccumulated = math.fraction(0);
 	let fiatAccumulated = math.fraction(0);
 	const averages = [];
 	data.forEach(({ size, price }) => {
-		btcAccumulated = math.add(btcAccumulated, math.fraction(size));
+		baseAccumulated = math.add(baseAccumulated, math.fraction(size));
 		const orderValue = math.multiply(math.fraction(price), math.fraction(size));
 		fiatAccumulated = math.add(fiatAccumulated, orderValue);
 		averages.push(math.number(orderValue));
 	});
 
 	return {
-		btc: formatBtcAmount(btcAccumulated),
-		fiat: formatFiatAmount(fiatAccumulated),
-		average: formatFiatAmount(math.median(averages))
+		btc: CURRENCIES[pair.pair_base].formatToCurrency(baseAccumulated),
+		fiat: CURRENCIES[pair.pair_2].formatToCurrency(fiatAccumulated),
+		average: CURRENCIES[pair.pair_2].formatToCurrency(math.median(averages))
 	};
 };
 
-const TradeDisplay = ({ side, data }) => {
+const TradeDisplay = ({ side, data, ...rest }) => {
+	const pair = PAIRS[data[0].symbol];
+	const basePair = pair.pair_base.toUpperCase();
+	const payPair = pair.pair_2.toUpperCase();
+
 	const actionText =
 		side === 'sell' ? STRINGS.ORDER_SOLD : STRINGS.ORDER_BOUGHT;
 	const resultText =
 		side === 'sell' ? STRINGS.ORDER_RECEIVED : STRINGS.ORDER_SPENT;
-	const { btc, fiat, average } = calculateValues(data);
+	const { btc, fiat, average } = calculateValues(data, pair);
 	return (
 		<NotificationContent>
 			<InformationRow
 				label={actionText}
 				value={STRINGS.formatString(
-					STRINGS.BTC_PRICE_FORMAT,
+					STRINGS[`${basePair}_PRICE_FORMAT`],
 					btc,
-					STRINGS.BTC_SHORTNAME
+					STRINGS[`${basePair}_SHORTNAME`]
 				)}
 			/>
 			<InformationRow
 				label={STRINGS.ORDER_AVERAGE_PRICE}
 				value={STRINGS.formatString(
-					STRINGS.FIAT_PRICE_FORMAT,
+					STRINGS[`${payPair}_PRICE_FORMAT`],
 					average,
-					STRINGS.FIAT_SHORTNAME
+					STRINGS[`${payPair}_SHORTNAME`]
 				)}
 			/>
 			<InformationRow
 				label={resultText}
 				value={STRINGS.formatString(
-					STRINGS.FIAT_PRICE_FORMAT,
+					STRINGS[`${payPair}_PRICE_FORMAT`],
 					fiat,
-					STRINGS.FIAT_SHORTNAME
+					STRINGS[`${payPair}_SHORTNAME`]
 				)}
 			/>
 		</NotificationContent>
