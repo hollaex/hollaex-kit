3329b66ab68fbba5bf47f38f4e208f02fcc81194
Jose Miguel <jmn8718@gmail.com>
2018-05-31 18:07:08 +0900

    remove test files

--- src/config/localizedStrings.js
+++ src/config/localizedStrings.js
@@ -1,11 +1,9 @@
 import LocalizedStrings from 'react-localization';
 
 import en from './en';
-import ko from './ko';
 
 const strings = new LocalizedStrings({
-	en,
-	ko
+	en
 });
 
 export default strings;
