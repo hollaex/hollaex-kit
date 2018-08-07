3142f85f2e4c38dc40a7b6cb1101a0daec93c8ca
Seungho Choi <ksngho7@gmail.com>
2018-08-07 10:58:47 +0900

    examples for hollaex node library

--- example/hollaex.js
+++ example/hollaex.js
@@ -1,22 +1,46 @@
 const HollaEx = require('../index');
 
-var client = new HollaEx({accessToken:''});
+const ACCESS_TOCKEN = '';
+
+var client = new HollaEx({accessToken : ACCESS_TOCKEN});
+
 
 client.getTicker('btc-eur')
 	.then(res => {
 		let data = JSON.parse(res)
-		console.log("Get Ticker", data)
+		console.log("Get Ticker: ", data)
+	});
+//
+// client.getUser()
+// 	.then(res => {
+// 		let data = JSON.parse(res)
+// 		console.log("Get User: ", data)
+// 	});
+
+// client.getDeposits()
+// 	.then(res => {
+// 		let data = JSON.parse(res)
+// 		console.log("Get Deposit: ", data)
+// 	});
+
+// client.getWithdrawals()
+// 	.then(res => {
+// 		let data = JSON.parse(res)
+// 		console.log("Get Withdrawal: ", data)
+// 	});
+
+client.getUserTrades()
+	.then(res => {
+		let data = JSON.parse(res)
+		console.log("Get User Trades: ", data)
 	});
 
-
-client.getUser()
-	.then(res => {
-		let data = JSON.parse(res)
-		console.dir({'b':data}, {depth: null})
-	})
-
-client.getUserAdmin(67)
-	.then(res => {
-		let data = JSON.parse(res)
-		console.dir({'b':data}, {depth: null})
-	})
+// client.createOrder('btc-eur', 'buy', 1, 'market', 1);
+// client.createOrder('btc-eur', 'buy', 1, 'market', 1);
+// client.createOrder('btc-eur', 'sell', 1, 'market', 1);
+//
+// client.getAllOrders ()
+// 	.then(res => {
+// 		let data = JSON.parse(res)
+// 		console.log("Get Orders: ", data)
+// 	});
