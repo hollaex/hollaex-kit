380df088af72b338b880149fbfcee61e7fa45a4e
admin <admin@admin-ui-MacBook-Air.local>
2018-08-08 16:38:02 +0900

    fixed token typo

--- example/hollaex.js
+++ example/hollaex.js
@@ -2,16 +2,19 @@ const HollaEx = require('../index');
 
 require('dotenv').load();
 
-const ACCESS_TOCKEN = process.env.ACCESS_TOCKEN;
-var client = new HollaEx({accessToken : ACCESS_TOCKEN});
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+// var client = new HollaEx({accessToken : ACCESS_TOCKEN});
+var client = new HollaEx({accessToken : ACCESS_TOKEN});
 
 
-// client.getTicker('btc-eur')
-// 	.then(res => {
-// 		let data = JSON.parse(res)
-// 		console.log("Get Ticker: ", data)
-// 	});
-//
+client.getTicker('btc-eur')
+	.then(res => {
+		let data = JSON.parse(res)
+		console.log("Get Ticker: ", data)
+	});
+
+	client.connectSocket();
+
 // client.getUser()
 // 	.then(res => {
 // 		let data = JSON.parse(res)
@@ -30,11 +33,11 @@ var client = new HollaEx({accessToken : ACCESS_TOCKEN});
 // 		console.log("Get Withdrawal: ", data)
 // 	});
 
-client.getUserTrades()
-	.then(res => {
-		let data = JSON.parse(res)
-		console.log("Get User Trades: ", data)
-	});
+// client.getUserTrades()
+// 	.then(res => {
+// 		let data = JSON.parse(res)
+// 		console.log("Get User Trades: ", data)
+// 	});
 
 // client.createOrder('btc-eur', 'buy', 1, 'market', 1);
 // client.createOrder('btc-eur', 'buy', 1, 'market', 1);
