b4f53ce706e8589da3b86840938a5349fcdf93e2
Seungho Choi <ksngho7@gmail.com>
2018-08-17 16:56:46 +0900

    organizing hollaex node library examples

--- example/hollaex.js
+++ example/hollaex.js
@@ -5,54 +5,55 @@ require('dotenv').load();
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 var client = new HollaEx({accessToken : ACCESS_TOKEN});
 
+//Public rest API
+client.getTickers('btc-eur')
+	.then(res => {
+		let data = JSON.parse(res)
+		console.log("Get Ticker: ", data)
+	});
 
-// client.getTickers('btc-eur')
-// 	.then(res => {
-// 		let data = JSON.parse(res)
-// 		console.log("Get Ticker: ", data)
-// 	});
-
-
-// client.getUser()
-// 	.then(res => {
-// 		let data = JSON.parse(res)
-// 		console.log("Get User: ", data)
-// 	});
+//Private rest API
+client.getUser()
+	.then(res => {
+		let data = JSON.parse(res)
+		console.log("Get User: ", data)
+	});
 
 // client.getDeposits()
 // 	.then(res => {
 // 		let data = JSON.parse(res)
 // 		console.log("Get Deposit: ", data)
 // 	});
-
 // client.getWithdrawals()
 // 	.then(res => {
 // 		let data = JSON.parse(res)
 // 		console.log("Get Withdrawal: ", data)
 // 	});
-
 // client.getUserTrades()
 // 	.then(res => {
 // 		let data = JSON.parse(res)
 // 		console.log("Get User Trades: ", data)
 // 	});
+// client.createOrder('btc-eur', 'buy', 1, 'market', 1);
+// client.createOrder('btc-eur', 'buy', 1, 'market', 1);
+// client.createOrder('btc-eur', 'sell', 1, 'market', 1);
+// client.getAllOrders ()
+// 	.then(res => {
+// 		let data = JSON.parse(res)
+// 		console.log("Get Orders: ", data)
+// 	});
 
-// client.connectPublicSocket('trades');
+
+// connect to Public websocket
+client.connectPublicSocket('trades');
 // client.connectPublicSocket('data', 'btc-eur');
 // client.connectPublicSocket('orderbook', 'btc-eur');
-// client.checkConnection();
+client.checkConnection();
 
-//connect to private socket
-client.connectPrivateSocket('update');
-
-
-// client.connectChartSocket();
+// client.connectRealTimeSocket();
 // client.socketChartData();
-// client.socketRealTimeTrades
-// client.socketRealTimeOrderbook();
+// client.socketRealTimeTrades();
 // client.socketRealTimeTicker();
-
-
 // client.getLiveTrades('btc-eur');
 // client.getLiveOrderbooks('btc-eur');
 	// .then(res => {
@@ -60,12 +61,6 @@ client.connectPrivateSocket('update');
 	// 	console.log("Get User Trades: ", data)
 	// });
 
-// client.createOrder('btc-eur', 'buy', 1, 'market', 1);
-// client.createOrder('btc-eur', 'buy', 1, 'market', 1);
-// client.createOrder('btc-eur', 'sell', 1, 'market', 1);
-//
-// client.getAllOrders ()
-// 	.then(res => {
-// 		let data = JSON.parse(res)
-// 		console.log("Get Orders: ", data)
-// 	});
+
+//connect to Private socket
+client.connectPrivateSocket('wallet');
