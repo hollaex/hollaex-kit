7573b5c3e028ea8d35d33657fb3d65ccc0aed430
abeikverdi <ali@alireza.be>
2018-09-02 01:27:10 +0900

    major recoding

--- example/hollaex.js
+++ example/hollaex.js
@@ -1,117 +1,29 @@
 const HollaEx = require('../index');
 
-require('dotenv').load();
-
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN || '';
 var client = new HollaEx({accessToken : ACCESS_TOKEN});
-
-
-const formatAPI = (nestArr) => {
-	const dataArr = nestArr.map((datum) =>{
-		if(typeof datum==='string'){
-			const data = JSON.parse(datum);
-			return data
-		} else {
-			return formatAPI(datum);
-		}
-	});
-	return dataArr;
-}
-
-//get all API
-
-client.getAPI(['ticker:btc-eur', 'trades:eth-btc', 'privateUser', 'privateBalance'])
-	.then(res => {
-			const data = formatAPI(res);
-			// console.log(data);
-			console.log(JSON.stringify(data, null, 4));
-	});
-
-// client.getPrivateAPI(['user', 'balance'])
-// 	.then(res => {
-// 		res.map(datum=>{
-// 			const data=JSON.parse(datum);
-// 			console.log(data);
-// 		})
-// 	});
-
-
-// client.orderAction(getAllOrders, cancelAllOrders)
-// 	.then(res => {
-// 		let data = JSON.parse(res)
-// 		console.log("Get Ticker: ", data)
-// 	});
-
-
-
-// const data = client.allRequest([['publicAPI', 'ticker:btc-eur', 'trades:eth-btc', 'orderbooks:btc-eur'], ['privateAPI','user','balance']])
-// 	.then(res => {
-// 		res.map(array=>{
-// 			array.map(datum => {
-// 				const data=JSON.parse(datum);
-// 				console.log(data);
-// 			})
-// 		})
-// 	});
-//
-// 	data.on('trades', (data) => {
-// 		console.log(data)
-// 	})
-
-
-// client.getTickers('btc-eur')
-// 	.then(res => {
-// 		let data = JSON.parse(res)
-// 		console.log("Get Ticker: ", data)
-// 	});
-//
-// //Private rest API
-// client.getUser()
-// 	.then(res => {
-// 		let data = JSON.parse(res)
-// 		console.log("Get User: ", data)
-// 	});
-
-// client.getDeposits()
-// 	.then(res => {
-// 		let data = JSON.parse(res)
-// 		console.log("Get Deposit: ", data)
-// 	});
-// client.getWithdrawals()
-// 	.then(res => {
-// 		let data = JSON.parse(res)
-// 		console.log("Get Withdrawal: ", data)
-// 	});
-// client.getUserTrades()
-// 	.then(res => {
-// 		let data = JSON.parse(res)
-// 		console.log("Get User Trades: ", data)
-// 	});
-// client.createOrder('btc-eur', 'buy', 1, 'market', 1);
-// client.createOrder('btc-eur', 'buy', 1, 'market', 1);
-// client.createOrder('btc-eur', 'sell', 1, 'market', 1);
-// client.getAllOrders ()
-// 	.then(res => {
-// 		let data = JSON.parse(res)
-// 		console.log("Get Orders: ", data)
-// 	});
 
 
 // connect to websocket
 /*
-	events : trades, orderbook, ticker,chartData, chartTicker,privateUser, privateWallet, privateOrders
+	events : trades, orderbook, ticker, chart, user
 
 	symbols : btc-eur, eth-btc, eth-eur
 */
 
-// const socket = client.connectSocket(['trades:btc-eur', 'chartData:eth-btc', 'privateUser']);
+const socket = client.connectSocket('all');
+// const socket2 = client.connectSocket('ticker:eth-btc');
+
 // client.checkConnection();
-// socket.on('trades', (data) => {
-// 	console.log(data)
-// })
-// socket.on('user', (data) => {
-// 	console.log(data)
-// })
-// socket.on('data', (data) => {
-// 	console.log(data)
-// })
+socket.on('ticker', (data) => {
+	console.log(data)
+})
+socket.on('userOrder', (data) => {
+	console.log(data)
+})
+socket.on('chart', (data) => {
+	console.log(data)
+})
+socket.on('orderbook', (data) => {
+	console.log(data)
+})
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Promise = require('bluebird');
 var io = require('socket.io-client');
 const EventEmitter = require('events');
 
-class MyEmitter extends EventEmitter {}
+class Emitter extends EventEmitter {}
 
 const  { createRequest } = require('./utils');
 
@@ -23,109 +23,91 @@ class HollaEx  {
 		}
 	}
 
-	/* Make any request */
-	/* types: publicAPI, privateAPI, socket */
-
-	// allRequest(allEvents){
-	// 	const promises = allEvents.map(([type, ...events])=>{
-	// 		if(type === 'publicAPI'){
-	// 			this.getPublicAPI(events);
-	// 		} else if (type === 'privateAPI'){
-	// 			this.getPrivateAPI(events);
-	// 		} else if (type === 'socket'){
-	// 			this.connectSocket(events);
-	// 		}
-	// 	})
-	// 	return Promise.all(promises);
-	// }
-
-
 	/* Public */
 
  /* events: ticker, orderbooks, trades */
 
-	getAPI(events){
-		const privateArr = [];
-		const publicArr = [];
-		events.map(oneEvent=>{
-			if(oneEvent.includes('private')){
-				privateArr.push(oneEvent.slice(7).toLowerCase());
-			} else {
-				publicArr.push(oneEvent.split(":"));
-			}
-		 });
-		const promises = publicArr.map(([event, symbol])=>{
-			console.log('getting', symbol, event);
-			return createRequest('GET', `${this._url}/${event}?symbol=${symbol}`, this._headers);
-		});
-		promises.push(this.getPrivateAPI(privateArr));
-		return Promise.all(promises);
-	}
+	// getAPI(events){
+	// 	const privateArr = [];
+	// 	const publicArr = [];
+	// 	events.map(oneEvent=>{
+	// 		if(oneEvent.includes('private')){
+	// 			privateArr.push(oneEvent.slice(7).toLowerCase());
+	// 		} else {
+	// 			publicArr.push(oneEvent.split(":"));
+	// 		}
+	// 	 });
+	// 	const promises = publicArr.map(([event, symbol])=>{
+	// 		console.log('getting', symbol, event);
+	// 		return createRequest('GET', `${this._url}/${event}?symbol=${symbol}`, this._headers);
+	// 	});
+	// 	promises.push(this.getPrivateAPI(privateArr));
+	// 	return Promise.all(promises);
+	// }
 
 	// Ticker
 	getTickers(symbol) {
 		return createRequest('GET', `${this._url}/ticker?symbol=${symbol}`, this._headers);
 	}
-	//
-	// // Orderbook
-	// getOrderbooks(symbol) {
-	// 	return createRequest ('GET' , `${this._url}/orderbooks?symbol=${symbol}` , this._headers);
-	// }
-	//
-	// // Trades
-	// getTrades(symbol) {
-	// 	return createRequest ('GET', `${this._url}/trades?symbol=${symbol}` , this._headers )
-	// }
+	
+	// Orderbook
+	getOrderbooks(symbol) {
+		return createRequest ('GET' , `${this._url}/orderbooks?symbol=${symbol}` , this._headers);
+	}
+	
+	// Trades
+	getTrades(symbol) {
+		return createRequest ('GET', `${this._url}/trades?symbol=${symbol}` , this._headers )
+	}
 
 	/*********************************************************************************************************
 
 	/* Private */
 
 	/* events: user,balance,deposits,withdrawals,trades */
-	getPrivateAPI(events){
-		const promises = events.map(event=>{
-			console.log('getting', event);
-			if (event === 'user'){
-				return createRequest('GET', `${this._url}/user`, this._headers);
-			}	else {
-				return createRequest('GET', `${this._url}/user/${event}`, this._headers);
-			}
-		});
-		return Promise.all(promises);
+	// getPrivateAPI(events){
+	// 	const promises = events.map(event=>{
+	// 		console.log('getting', event);
+	// 		if (event === 'user'){
+	// 			return createRequest('GET', `${this._url}/user`, this._headers);
+	// 		}	else {
+	// 			return createRequest('GET', `${this._url}/user/${event}`, this._headers);
+	// 		}
+	// 	});
+	// 	return Promise.all(promises);
+	// }
+
+	// User
+	getUser() {
+		return createRequest('GET', `${this._url}/user`, this._headers);
+	}
+	
+	// Balance
+	getBalance() {
+		return createRequest('GET',`${this._url}/user/balance` , this._headers);
+	}
+	
+	// Deposits
+	getDeposit() {
+		return createRequest('GET',`${this._url}/user/deposits` , this._headers);
+	}
+	
+	// Withdrawal
+	getWithdrawal() {
+		return createRequest('GET',`${this._url}/user/withdrawals` , this._headers);
+	}
+	
+	// Trades
+	getUserTrade() {
+		return createRequest('GET',`${this._url}/user/trades` , this._headers);
 	}
 
-	// // User
-	// getUser() {
-	// 	return createRequest('GET', `${this._url}/user`, this._headers);
-	// }
-	//
-	// // Balance
-	// getBalance() {
-	// 	return createRequest('GET',`${this._url}/user/balance` , this._headers);
-	// }
-	//
-	// // Deposits
-	// getDeposits() {
-	// 	return createRequest('GET',`${this._url}/user/deposits` , this._headers);
-	// }
-	//
-	// // Withdrawal
-	// getWithdrawals() {
-	// 	return createRequest('GET',`${this._url}/user/withdrawals` , this._headers);
-	// }
-	//
-	// // Trades
-	// getUserTrades() {
-	// 	return createRequest('GET',`${this._url}/user/trades` , this._headers);
-	// }
-
-
-	// Orders
+	/****** Orders ******/
 	getOrder(orderId) {
 		return createRequest('GET',`${this._url}/user/orders/${orderId}` , this._headers);
 	}
 
-	getAllOrders(symbol='') {
+	getAllOrder(symbol='') {
 		return createRequest('GET',`${this._url}/user/orders?symbol=${symbol}` , this._headers);
 	}
 
@@ -138,165 +120,123 @@ class HollaEx  {
 		return createRequest('DELETE',`${this._url}/user/orders/${orderId}` , this._headers);
 	}
 
-	cancelAllOrders(symbol='') {
+	cancelAllOrder(symbol='') {
 		let data = {symbol};
 		return createRequest('DELETE',`${this._url}/user/orders?symbol=${symbol}` , this._headers);
 	}
-	/*********************************************************************************************************
 
-	//Websocket
-	/* Public */
-
-	connectSocket(events){
-		const realtime = ['trades', 'orderbook', 'ticker'];
-		const chart = ['chartData', 'chartTicker'];
-		const privateUser = ['privateUser', 'privateWallet', 'privateOrders', 'privateTrades', 'privateUpdate'];
-		const myEmitter = new MyEmitter();
-
-		const eventArr = events.map(oneEvent=>{
-			 return oneEvent.split(":");
-		 });
-
-		eventArr.map(([event,symbol])=>{
-			if(realtime.includes(event)) {
-				if(symbol){
-					this.publicSocket = io(`${this._wsUrl}/realtime`, {
-						// if you dont pass anything it will return all symbols
-						query: { symbol }
-					});
-				} else {
-					this.publicSocket = io(`${this._wsUrl}/realtime`);
-				}
-				this.publicSocket.on(event, (data) => {
-					myEmitter.emit(event, data)
-				});
-				console.log(`connecting to real time ${event} for ${symbol?symbol:'all symbols'}`);
-			}
-			if(chart.includes(event)){
-				event = event.slice(5).toLowerCase();
-				if(symbol){
-					this.publicSocket = io(`${this._wsUrl}/chart`, {
-						// if you dont pass anything it will return all symbols
-						query: { symbol }
-					});
-				} else {
-					this.publicSocket = io(`${this._wsUrl}/chart`);
-				}
-				this.publicSocket.on(event, (data) => {
-					myEmitter.emit(event, data)
-				});
-				console.log(`connecting to chart ${event} for ${symbol?symbol:'all symbols'}`);
-			}
-
-			if(privateUser.includes(event)){
-				event = event.slice(7).toLowerCase();
-				this.privateSocket = io(`${this._wsUrl}/user`, {
-					query: {
-						token: `Bearer ${this._accessToken}`
-					}
-				});
-				this.privateSocket.on(event, (data) => {
-					myEmitter.emit(event, data)
-				});
-				console.log(`connecting to private ${event}`);
-			}
-		})
-		return myEmitter;
+	// connect to websocket
+	connectSocket(events) {
+		return new Socket(events, this._wsUrl, this._accessToken);
 	}
+}
 
 
-	// // Real Time Connect
-	// connectRealTimeSocket(symbol) {
-	// 	if(symbol){
-	// 		this.publicSocket = io(`${this._wsUrl}/realtime`, {
-	// 			// if you dont pass anything it will return all symbols
-	// 			query: { symbol }
-	// 		});
-	// 		console.log('connecting realtime', symbol);
-	// 	} else {
-	// 		this.publicSocket = io(`${this._wsUrl}/realtime`);
-	// 		console.log('connecting realtime all symbols');
-	// 	}
-	// }
-	//
-	// // Real Time Trades
-	// socketRealTimeTrades(){
-	// 	this.publicSocket.on('trades', (data) => {
-	// 		console.log(data)
-	// 	});
-	// 	console.log('getting real time trades');
-	// }
-	//
-	// // Real Time Orderbook
-	// socketRealTimeOrderbook(){
-	// 	this.publicSocket.on('orderbook', (data) => {
-	// 		console.log(data)
-	// 	});
-	// 	console.log('getting real time orderbooks')
-	// }
-	//
-	// // Real Time Ticker
-	// socketRealTimeTicker(){
-	// 	this.publicSocket.on('ticker', (data) => {
-	// 		console.log(data)
-	// 	});
-	// 	console.log('getting real time ticker')
-	// }
-	//
-	// // Chart Connect
-	// connectChartSocket(symbol) {
-	// 	if(symbol){
-	// 		this.publicSocket = io(`${this._wsUrl}/chart`, {
-	// 			// if you dont pass anything it will return all symbols
-	// 			query: { symbol }
-	// 		});
-	// 		console.log('connecting chart', symbol);
-	// 	} else {
-	// 		this.publicSocket = io(`${this._wsUrl}/chart`);
-	// 		console.log('connecting all symbols');
-	// 	}
-	// }
-	//
-	// // Chart data
-	// socketChartData(){
-	// 	this.publicSocket.on('data', (data) => {
-	// 		console.log(data)
-	// 	});
-	// 	console.log('getting chart data');
-	// }
+/*******************
+Websocket
+*******************/
+class Socket extends EventEmitter {
+	constructor(events='', url, accessToken) {
+		super();
+		if (!Array.isArray(events)) {
+			let listeners = [];
+			let ioLink;
+			events = events.split(":");
+			let [event, symbol] = events
+			switch (event) {
+				case 'orderbook':
+				case 'trades':
+				case 'ticker':
+					if (symbol) {
+						ioLink = io(`${url}/realtime`, {query: { symbol }});
+					} else {
+						ioLink = io(`${url}/realtime`);
+					}
+					listeners.push(ioLink);				
+					listeners[listeners.length-1].on(event, (data) => {
+						this.emit(event, data)
+					});
+				break;
+				case 'chart':
+					if (symbol) {
+						ioLink = io(`${url}/chart`, {query: { symbol }});
+					} else {
+						ioLink = io(`${url}/chart`);
+					}
+					listeners.push(ioLink);		
+					listeners[listeners.length-1].on("data", (data) => {
+						this.emit(event, data)
+					});
+					listeners[listeners.length-1].on("ticker", (data) => {
+						this.emit(event, data)
+					});
+				break;
+				case 'user':
+					ioLink = io(`${url}/user`, {query: {token: `Bearer ${accessToken}`}});
 
+					listeners.push(ioLink);
+					listeners[listeners.length-1].on("user", (data) => {
+						this.emit("userInfo", data)
+					});
+					listeners[listeners.length-1].on("wallet", (data) => {
+						this.emit("userWallet", data)
+					});
+					listeners[listeners.length-1].on("orders", (data) => {
+						this.emit("userOrder", data)
+					});
+					listeners[listeners.length-1].on("trades", (data) => {
+						this.emit("userTrades", data)
+					});
+					listeners[listeners.length-1].on("update", (data) => {
+						this.emit("userUpdate", data)
+					});
+				break;
+				case 'all':
+					ioLink = io(`${url}/realtime`);
+					
+					listeners.push(ioLink);				
+					listeners[listeners.length-1].on("orderbook", (data) => {
+						this.emit("orderbook", data)
+					});
+					listeners[listeners.length-1].on("ticker", (data) => {
+						this.emit("ticker", data)
+					});
+					listeners[listeners.length-1].on("trades", (data) => {
+						this.emit("trades", data)
+					});
 
-	/*********************************************************************************************************
-	/* Private */
+					ioLink = io(`${url}/chart`);
+					
+					listeners.push(ioLink);		
+					listeners[listeners.length-1].on("data", (data) => {
+						this.emit(event, data)
+					});
+					listeners[listeners.length-1].on("ticker", (data) => {
+						this.emit(event, data)
+					});
 
-	// connectPrivateSocket(event){
-	// 	this.privateSocket = io(`${this._wsUrl}/user`, {
-	// 		query: {
-	// 			token: `Bearer ${this._accessToken}`
-	// 		}
-	// 	});
-	//
-	// 	this.privateSocket.on(event, (data) => {
-	// 		console.log(data);
-	// 	});
-	// 	console.log(`connecting to ${event}`)
-	//
-	// }
+					ioLink = io(`${url}/user`, {query: {token: `Bearer ${accessToken}`}});
 
-
-		// Check current socket connection
-		checkConnection(){
-			if(this.publicSocket|| this.privateSocket){
-				console.log(this.publicSocket || this.privateSocket);
-				this.publicSocket ? console.log(`connected to public socket ${this.publicSocket['nsp']}`)
-					: console.log(`connected to private socket ${this.privateSocket['nsp']}`);
-			} else {
-				console.log('no socket connecting established');
+					listeners.push(ioLink);
+					listeners[listeners.length-1].on("user", (data) => {
+						this.emit("userInfo", data)
+					});
+					listeners[listeners.length-1].on("wallet", (data) => {
+						this.emit("userWallet", data)
+					});
+					listeners[listeners.length-1].on("orders", (data) => {
+						this.emit("userOrder", data)
+					});
+					listeners[listeners.length-1].on("trades", (data) => {
+						this.emit("userTrade", data)
+					});
+					listeners[listeners.length-1].on("update", (data) => {
+						this.emit("userUpdate", data)
+					});
+				break;
 			}
 		}
-
-
-	/********************************************************************* TO BE ADDED MORE... */
+	}
 }
 
 module.exports = HollaEx;
