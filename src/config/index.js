fcd727a5799fa7d24909ec68541658927a6b2b11
jsmitrah <ram@mitrahsoft.com>
2018-09-04 20:19:18 +0530

    scss import fixed.

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const PRODUCTION_ENDPOINT =
 	process.env.REACT_APP_SERVER_ENDPOINT || 'https://api.hollaex.com';
-const LOCALHOST_ENDPOINT = 'http://localhost';
+const LOCALHOST_ENDPOINT = 'https://api.hollaex.com';
 
 const API_PATH_V0 = '/v0';
 
