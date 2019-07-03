5f486c45e4db51f8c05acee3fb9f18784e7fbcac
jsmitrah <ram@mitrahsoft.com>
2019-07-03 20:49:49 +0530

    Changes for donut highlight issue

--- src/components/Chart/Charts/DonutChart.js
+++ src/components/Chart/Charts/DonutChart.js
@@ -48,12 +48,11 @@ class DonutChart extends Component {
     checkData = (data = []) => {
         let largerValue = 0;
         let largerId = '';
-        data.map((value, index) => {
-            if (value.balance > largerValue) {
-                largerId = `slice-${index}`;
-                largerValue = value.balance;
+        data.map((value) => {
+            if (parseFloat(value.balancePercentage) > largerValue) {
+                largerId = value.symbol;
+                largerValue = parseFloat(value.balancePercentage);
             }
-            return null;
         });
         this.setState({ higherId: largerId, hoverId: largerId });
 
@@ -61,8 +60,8 @@ class DonutChart extends Component {
         return !!checkFilter.length;
     };
 
-    handleHover = id => {
-        this.setState({ hoverId: `slice-${id}` });
+    handleHover = symbol => {
+        this.setState({ hoverId: symbol });
     };
 
     handleOut = () => {
@@ -188,10 +187,10 @@ class DonutChart extends Component {
                 <g key={i}>
                     <path
                         d={arcj(value)}
-                        className={classnames(`chart_${data.symbol}`, { 'slice_active': this.state.hoverId === `slice-${i}` })}
-                        onMouseOver={() => this.handleHover(i)}
+                        className={classnames(`chart_${data.symbol}`, { 'slice_active': this.state.hoverId === data.symbol })}
+                        onMouseOver={() => this.handleHover(data.symbol)}
                         onMouseOut={this.handleOut} />
-                    {this.state.hoverId === `slice-${i}`
+                    {this.state.hoverId === data.symbol
                         ? <Fragment>
                             <text transform={translate(valX, valY)}
                                 dy="20px"
