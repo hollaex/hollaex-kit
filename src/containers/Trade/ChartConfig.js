8be4765ac1d64d9608a0c2e1d8ab3971f21c2135
jsmitrah <ram@mitrahsoft.com>
2019-08-02 19:05:29 +0530

    Hex theme change for trading view chart and others

--- src/containers/Trade/ChartConfig.js
+++ src/containers/Trade/ChartConfig.js
@@ -13,11 +13,34 @@ export const WHITE_THEME = {
 	"mainSeriesProperties.candleStyle.downColor": "#000000",
 	"mainSeriesProperties.candleStyle.drawWick": true,
 	"mainSeriesProperties.candleStyle.drawBorder": true,
-	"mainSeriesProperties.candleStyle.borderUpColor": "#000000",
+	"mainSeriesProperties.candleStyle.borderUpColor": "#d1d4dc",
 	"mainSeriesProperties.candleStyle.borderDownColor": "#000000",
-	"mainSeriesProperties.candleStyle.wickUpColor": '#000000',
+	"mainSeriesProperties.candleStyle.wickUpColor": '#d1d4dc',
 	"mainSeriesProperties.candleStyle.wickDownColor": '#000000',
-	"mainSeriesProperties.candleStyle.barColorsOnPrevClose": false,
+	"mainSeriesProperties.candleStyle.barColorsOnPrevClose": true,
+
+	//  Hollow Candles styles
+	"mainSeriesProperties.hollowCandleStyle.upColor": "#d1d4dc",
+	"mainSeriesProperties.hollowCandleStyle.downColor": "#000000",
+	"mainSeriesProperties.hollowCandleStyle.drawWick": true,
+	"mainSeriesProperties.hollowCandleStyle.drawBorder": true,
+	"mainSeriesProperties.hollowCandleStyle.borderColor": "#000000",
+	"mainSeriesProperties.hollowCandleStyle.borderUpColor": "#d1d4dc",
+	"mainSeriesProperties.hollowCandleStyle.borderDownColor": "#000000",
+	"mainSeriesProperties.hollowCandleStyle.wickUpColor": "#d1d4dc",
+	"mainSeriesProperties.hollowCandleStyle.wickDownColor": "#000000",
+
+	//  Heiken Ashi styles
+	"mainSeriesProperties.haStyle.upColor": "#d1d4dc",
+	"mainSeriesProperties.haStyle.downColor": "#000000",
+	"mainSeriesProperties.haStyle.drawWick": true,
+	"mainSeriesProperties.haStyle.drawBorder": true,
+	"mainSeriesProperties.haStyle.borderColor": "#000000",
+	"mainSeriesProperties.haStyle.borderUpColor": "#000000",
+	"mainSeriesProperties.haStyle.borderDownColor": "#000000",
+	"mainSeriesProperties.haStyle.wickColor": "#000000",
+	"mainSeriesProperties.haStyle.barColorsOnPrevClose": false,
+
 	//	Bars styles
 	"mainSeriesProperties.barStyle.upColor": "#d1d4dc",
 	"mainSeriesProperties.barStyle.downColor": "#000000",
@@ -31,7 +54,21 @@ export const WHITE_THEME = {
 	"mainSeriesProperties.areaStyle.linecolor": "#000000",
 	// "mainSeriesProperties.areaStyle.linestyle": CanvasEx.LINESTYLE_SOLID,
 	"mainSeriesProperties.areaStyle.linewidth": 1,
-	"mainSeriesProperties.areaStyle.priceSource": "close"
+	"mainSeriesProperties.areaStyle.priceSource": "close",
+
+	//  Baseline styles
+	"mainSeriesProperties.baselineStyle.baselineColor": "#d1d4dc",
+	"mainSeriesProperties.baselineStyle.topFillColor1": "#d1d4dc",
+	"mainSeriesProperties.baselineStyle.topFillColor2": "#d1d4dc",
+	"mainSeriesProperties.baselineStyle.bottomFillColor1": "#000000",
+	"mainSeriesProperties.baselineStyle.bottomFillColor2": "#000000",
+	"mainSeriesProperties.baselineStyle.topLineColor": "#d1d4dc",
+	"mainSeriesProperties.baselineStyle.bottomLineColor": "#000000",
+	"mainSeriesProperties.baselineStyle.topLineWidth": 1,
+	"mainSeriesProperties.baselineStyle.bottomLineWidth": 1,
+	"mainSeriesProperties.baselineStyle.priceSource": "close",
+	"mainSeriesProperties.baselineStyle.transparency": 50,
+	"mainSeriesProperties.baselineStyle.baseLevelPercentage": 50
 }
 
 export const DARK_THEME = {
@@ -53,7 +90,32 @@ export const DARK_THEME = {
 	"mainSeriesProperties.candleStyle.borderDownColor": "#FF9800",
 	"mainSeriesProperties.candleStyle.wickUpColor": '#6D9EEB',
 	"mainSeriesProperties.candleStyle.wickDownColor": '#FF9800',
-	"mainSeriesProperties.candleStyle.barColorsOnPrevClose": false,
+	"mainSeriesProperties.candleStyle.barColorsOnPrevClose": true,
+
+	//  Hollow Candles styles
+	"mainSeriesProperties.hollowCandleStyle.upColor": "#6D9EEB",
+	"mainSeriesProperties.hollowCandleStyle.downColor": "#FF9800",
+	"mainSeriesProperties.hollowCandleStyle.drawWick": true,
+	"mainSeriesProperties.hollowCandleStyle.drawBorder": true,
+	"mainSeriesProperties.hollowCandleStyle.borderColor": "#FF9800",
+	"mainSeriesProperties.hollowCandleStyle.borderUpColor": "#6D9EEB",
+	"mainSeriesProperties.hollowCandleStyle.borderDownColor": "#FF9800",
+	"mainSeriesProperties.hollowCandleStyle.wickUpColor": "#6D9EEB",
+	"mainSeriesProperties.hollowCandleStyle.wickDownColor": "#FF9800",
+
+	//  Heiken Ashi styles
+	"mainSeriesProperties.haStyle.upColor": "#6D9EEB",
+	"mainSeriesProperties.haStyle.downColor": "#FF9800",
+	"mainSeriesProperties.haStyle.drawWick": true,
+	"mainSeriesProperties.haStyle.drawBorder": true,
+	"mainSeriesProperties.haStyle.borderColor": "#FF9800",
+	"mainSeriesProperties.haStyle.borderUpColor": "#6D9EEB",
+	"mainSeriesProperties.haStyle.borderDownColor": "#FF9800",
+	"mainSeriesProperties.haStyle.wickColor": "#FF9800",
+	"mainSeriesProperties.haStyle.barColorsOnPrevClose": false,
+	"mainSeriesProperties.haStyle.wickUpColor": "#6D9EEB",
+	"mainSeriesProperties.haStyle.wickDownColor": "#FF9800",
+	
 	//	Bars styles
 	"mainSeriesProperties.barStyle.upColor": "#6D9EEB",
 	"mainSeriesProperties.barStyle.downColor": "#FF9800",
@@ -66,5 +128,19 @@ export const DARK_THEME = {
 	"mainSeriesProperties.areaStyle.linecolor": "#6D9EEB",
 	// "mainSeriesProperties.areaStyle.linestyle": CanvasEx.LINESTYLE_SOLID,
 	"mainSeriesProperties.areaStyle.linewidth": 1,
-	"mainSeriesProperties.areaStyle.priceSource": "close"
+	"mainSeriesProperties.areaStyle.priceSource": "close",
+
+	//  Baseline styles
+	"mainSeriesProperties.baselineStyle.baselineColor": "#6D9EEB",
+	"mainSeriesProperties.baselineStyle.topFillColor1": "#6D9EEB",
+	"mainSeriesProperties.baselineStyle.topFillColor2": "#6D9EEB",
+	"mainSeriesProperties.baselineStyle.bottomFillColor1": "#FF9800",
+	"mainSeriesProperties.baselineStyle.bottomFillColor2": "#FF9800",
+	"mainSeriesProperties.baselineStyle.topLineColor": "#6D9EEB",
+	"mainSeriesProperties.baselineStyle.bottomLineColor": "#FF9800",
+	"mainSeriesProperties.baselineStyle.topLineWidth": 1,
+	"mainSeriesProperties.baselineStyle.bottomLineWidth": 1,
+	"mainSeriesProperties.baselineStyle.priceSource": "close",
+	"mainSeriesProperties.baselineStyle.transparency": 50,
+	"mainSeriesProperties.baselineStyle.baseLevelPercentage": 50
 }
