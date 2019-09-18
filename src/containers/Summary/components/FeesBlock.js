1012663c6e93c5aab4347fa05026b7656d9fd155
jkoon90 <jkoon90@gmail.com>
2019-09-18 16:34:51 +0900

    resolve conflict issues

--- src/containers/Summary/components/FeesBlock.js
+++ src/containers/Summary/components/FeesBlock.js
@@ -5,25 +5,33 @@ import STRINGS from '../../../config/localizedStrings';
 import { formatPercentage } from '../../../utils/currency';
 
 const getMakerRow = (pairs, coins, pair, level, index) => {
-    const { pair_base, pair_2, maker_fees } = pairs[pair];
-    const feeData = maker_fees ? maker_fees[level] : 0;
-    const pairBase = coins[pair_base] || { symbol: '' };
-    const pairTwo = coins[pair_2] || {};
-    return (
-        <tr key={index}>
-            <td className="account-limits-coin" rowSpan={2}>
-                <div className='d-flex align-items-center'>
-                    <CurrencyBall name={pairBase.symbol.toUpperCase()} symbol={pair_base} size='m' />
-                    <div className="ml-2">
-                        {`${pairBase.fullname} / ${pairTwo.fullname}`}
-                    </div>
-                </div>
-            </td>
-            <td className="account-limits-maker account-limits-status">{STRINGS.SUMMARY.MAKER}:</td>
-            <td className="account-limits-maker account-limits-value">{formatPercentage(feeData)}</td>
-        </tr>
-    );
-}
+	const { pair_base, pair_2, maker_fees } = pairs[pair];
+	const feeData = maker_fees ? maker_fees[level] : 0;
+	const pairBase = coins[pair_base] || { symbol: '' };
+	const pairTwo = coins[pair_2] || {};
+	return (
+		<tr key={index}>
+			<td className="account-limits-coin" rowSpan={2}>
+				<div className="d-flex align-items-center">
+					<CurrencyBall
+						name={pairBase.symbol.toUpperCase()}
+						symbol={pair_base}
+						size="m"
+					/>
+					<div className="ml-2">
+						{`${pairBase.fullname} / ${pairTwo.fullname}`}
+					</div>
+				</div>
+			</td>
+			<td className="account-limits-maker account-limits-status">
+				{STRINGS.SUMMARY.MAKER}:
+			</td>
+			<td className="account-limits-maker account-limits-value">
+				{formatPercentage(feeData)}
+			</td>
+		</tr>
+	);
+};
 
 const getTakerRow = (pairs, pair, level, index) => {
 	const { taker_fees } = pairs[pair];
@@ -40,35 +48,6 @@ const getTakerRow = (pairs, pair, level, index) => {
 	);
 };
 
-<<<<<<< HEAD
-const getRows = (pairs, coins, level) => {
-    const rowData = [];
-    Object.keys(pairs).map((pair, index) => {
-        rowData.push(getMakerRow(pairs, coins, pair, level, index));
-        rowData.push(getTakerRow(pairs, pair, level, index));
-        return '';
-    });
-    return rowData;
-};
-
-const FeesBlock = ({ pairs, coins, level }) => {
-    return (
-        <div>
-            <table className="account-limits">
-                <thead>
-                    <tr>
-                        <th className="limit-head-currency">{STRINGS.CURRENCY}</th>
-                        <th></th>
-                        <th className="limit-head">{STRINGS.FEES}</th>
-                    </tr>
-                </thead>
-                <tbody className="account-limits-content font-weight-bold">
-                    {getRows(pairs, coins, level)}
-                </tbody>
-            </table>
-        </div>
-    );
-=======
 const getRows = (pairs, level) => {
 	const rowData = [];
 	Object.keys(pairs).map((pair, index) => {
@@ -96,7 +75,6 @@ const FeesBlock = ({ pairs, level }) => {
 			</table>
 		</div>
 	);
->>>>>>> cleanup
 };
 
 export default FeesBlock;
--- src/containers/Summary/components/TradingVolume.js
+++ src/containers/Summary/components/TradingVolume.js
@@ -37,75 +37,18 @@ class TradingVolume extends Component {
 		}
 	}
 
-    constructData = (tradeValues, coins) => {
-        const { pairs, prices, activeTheme } = this.props;
-        const chartData = [];
-        let totalVolume = 0;
-        let peakVolume = this.state.peakVolume;
-        const { min } = coins[BASE_CURRENCY] || {};
-        const currentMonth = moment().month();
-        let chartMonths = [...CHART_MONTHS];
-        for (let i = 0; i <= currentMonth; i++) {
-            let temp = chartMonths.shift();
-            chartMonths = [...chartMonths, temp];
-        }
-
-        if (Object.keys(tradeValues).length) {
-            chartMonths.map((obj, key) => {
-                let trade = tradeValues[obj.key];
-                let data = {
-                    key: obj.key,
-                    month: obj.value
-                }
-                if (trade) {
-                    let total = 0;
-                    let pairWisePrice = {};
-                    let pairVolume = {};
-                    Object.keys(trade).map((pair) => {
-                        let pairValue = pairs[pair] || {};
-                        let volumeObj = trade[pair] || {};
-                        let pairPrice = calculatePrice(volumeObj.volume, prices[pairValue.pair_base]);
-                        pairWisePrice[pairValue.pair_base] = pairPrice;
-                        pairVolume[pairValue.pair_base] = volumeObj.volume;
-                        total += pairPrice;
-                        return total;
-                    });
-                    data.pairVolume = pairVolume;
-                    data.pairWisePrice = pairWisePrice;
-                    data.total = total;
-                } else {
-                    data.pairVolume = {};
-                    data.pairWisePrice = {};
-                    data.total = 0;
-                }
-                if (peakVolume < data.total) {
-                    peakVolume = data.total;
-                }
-                totalVolume += data.total;
-                chartData.push(data);
-                return chartData;
-            });
-            const limitContent = [];
-            TRADING_VOLUME_CHART_LIMITS.map((_, index) => {
-                if (index === 0) {
-                    limitContent.push({
-                        icon: SUMMMARY_ICON.KRAKEN,
-                        text: STRINGS.SUMMARY.PRO_TRADER_ACCOUNT_ELIGIBLITY
-                    });
-                } else if (index === 1 && activeTheme === 'dark') {
-                    limitContent.push({
-                        icon: SUMMMARY_ICON.LEVIATHAN_DARK,
-                        text: STRINGS.SUMMARY.VIP_TRADER_ACCOUNT_ELIGIBLITY
-                    });
-                } else {
-                    limitContent.push({
-                        icon: SUMMMARY_ICON.LEVIATHAN,
-                        text: STRINGS.SUMMARY.VIP_TRADER_ACCOUNT_ELIGIBLITY
-                    });
-                }
-                return index;
-            });
-            peakVolume += (peakVolume * 0.1);
+	constructData = (tradeValues, coins) => {
+		const { pairs, prices, activeTheme } = this.props;
+		const chartData = [];
+		let totalVolume = 0;
+		let peakVolume = this.state.peakVolume;
+		const { min } = coins[BASE_CURRENCY] || {};
+		const currentMonth = moment().month();
+		let chartMonths = [...CHART_MONTHS];
+		for (let i = 0; i <= currentMonth; i++) {
+			let temp = chartMonths.shift();
+			chartMonths = [...chartMonths, temp];
+		}
 
 		if (Object.keys(tradeValues).length) {
 			chartMonths.map((obj, key) => {
@@ -167,45 +110,75 @@ class TradingVolume extends Component {
 			});
 			peakVolume += peakVolume * 0.1;
 
-<<<<<<< HEAD
-    render() {
-        const { chartData, limits, limitContent, peakVolume } = this.state;
-        const { tradeVolumes, coins } = this.props;
-        const { fullname } = coins[BASE_CURRENCY] || {};
-        return (
-            <div className="summary-section_2">
-                <div className="w-100 h-100">
-                    <div className="summary-content-txt">
-                        <div>
-                            {STRINGS.formatString(
-                                STRINGS.SUMMARY.TRADING_VOLUME_TXT_1,
-                                fullname
-                            )}
-                        </div>
-                        <div>{STRINGS.SUMMARY.TRADING_VOLUME_TXT_2}</div>
-                    </div>
-                    <BarChart
-                        loading={tradeVolumes.fetching && !tradeVolumes.fetched}
-                        chartData={chartData}
-                        peakVolume={peakVolume}
-                        yAxisLimits={limits}
-                        limitContent={limitContent}
-                        activeTheme={this.props.activeTheme} />
-                </div>
-            </div>
-        );
-    }
-};
-=======
-			this.setState({
-				chartData,
-				limitContent,
-				peakVolume,
-				totalVolume: formatToCurrency(totalVolume, min)
-			});
+			if (Object.keys(tradeValues).length) {
+				chartMonths.map((obj, key) => {
+					let trade = tradeValues[obj.key];
+					let data = {
+						key: obj.key,
+						month: obj.value
+					};
+					if (trade) {
+						let total = 0;
+						let pairWisePrice = {};
+						let pairVolume = {};
+						Object.keys(trade).map((pair) => {
+							let pairValue = pairs[pair] || {};
+							let volumeObj = trade[pair] || {};
+							let pairPrice = calculatePrice(
+								volumeObj.volume,
+								prices[pairValue.pair_base]
+							);
+							pairWisePrice[pairValue.pair_base] = pairPrice;
+							pairVolume[pairValue.pair_base] = volumeObj.volume;
+							total += pairPrice;
+							return total;
+						});
+						data.pairVolume = pairVolume;
+						data.pairWisePrice = pairWisePrice;
+						data.total = total;
+					} else {
+						data.pairVolume = {};
+						data.pairWisePrice = {};
+						data.total = 0;
+					}
+					if (peakVolume < data.total) {
+						peakVolume = data.total;
+					}
+					totalVolume += data.total;
+					chartData.push(data);
+					return chartData;
+				});
+				const limitContent = [];
+				TRADING_VOLUME_CHART_LIMITS.map((_, index) => {
+					if (index === 0) {
+						limitContent.push({
+							icon: SUMMMARY_ICON.KRAKEN,
+							text: STRINGS.SUMMARY.PRO_TRADER_ACCOUNT_ELIGIBLITY
+						});
+					} else if (index === 1 && activeTheme === 'dark') {
+						limitContent.push({
+							icon: SUMMMARY_ICON.LEVIATHAN_DARK,
+							text: STRINGS.SUMMARY.VIP_TRADER_ACCOUNT_ELIGIBLITY
+						});
+					} else {
+						limitContent.push({
+							icon: SUMMMARY_ICON.LEVIATHAN,
+							text: STRINGS.SUMMARY.VIP_TRADER_ACCOUNT_ELIGIBLITY
+						});
+					}
+					return index;
+				});
+				peakVolume += peakVolume * 0.1;
+
+				this.setState({
+					chartData,
+					limitContent,
+					peakVolume,
+					totalVolume: formatToCurrency(totalVolume, min)
+				});
+			}
 		}
 	};
->>>>>>> cleanup
 
 	render() {
 		const { chartData, limits, limitContent, peakVolume } = this.state;
