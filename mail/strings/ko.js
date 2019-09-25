f8ae03478dc798989921efacac21baed71846b12
Brandon Kim <bkim2490@gmail.com>
2019-09-25 18:14:02 +0900

    mail change for exchange name, support email, coins, domain

--- mail/strings/ko.js
+++ mail/strings/ko.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const { API_NAME, SUPPORT_EMAIL } = require('../../constants');
+
 const COMMON = {
 	GREETING: (name) => `${name}님`,
 	CLOSING: {
 		1: '이용해 주셔서 감사합니다.',
-		2: 'HollaEx팀'
+		2: `${API_NAME} 팀`
 	},
 	IP_ADDRESS: (ip) => `IP 주소: ${ip}`,
 	IP_REQUEST_FROM: (ip) => `요청하신곳: ${ip}`,
@@ -19,19 +21,12 @@ const COMMON = {
 		'만약 이 요청을 원하지 않는다면, 회원님의 계정에는 변경사항이 없으니 본 메일을 무시해 주시기 바랍니다.',
 	EXPLORER: '회원님이 요청하신 거래 상황을 이곳에서 확인하실 수 있습니다.',
 	DEPOSIT: '입금',
-	WITHDRAWAL: '출금',
-	EUR: '유로',
-	BTC: '비트코인',
-	ETH: '이더리움',
-	BCH: '비트코인캐시',
-	XRP: '리플'
+	WITHDRAWAL: '출금'
 };
 
 const FOOTER = {
 	FOLLOW_US: 'Follow us on',
 	NEED_HELP: '도움이 필요하시다면, 이 이메일에 회신해 주시기 바랍니다.',
-	EXIR: 'HollaEx',
-	BITHOLLA: '비트홀라',
 	PRIVACY_POLICY: '개인정보 처리방침',
 	TERMS: '이용약관',
 	INVITE_YOUR_FRIENDS: '친구 초대:',
@@ -42,9 +37,9 @@ const SIGNUP = {
 	TITLE: '회원가입',
 	GREETING: (name) => COMMON.GREETING(name),
 	BODY: {
-		1: `안녕하세요. HollaEx에 가입해주셔서 감사합니다.
+		1: `안녕하세요. ${API_NAME}에 가입해주셔서 감사합니다.
 		해당 부서에서 최대한 빠르게 회원님의 신청서를 검토 한 후 연락드리겠습니다.
-		문의사항은 <a href="mailto:support@bitholla.com">support@bitholla.com</a> 로 연락주시기 바랍니다.`,
+		문의사항은 <a href="mailto:${SUPPORT_EMAIL}">${SUPPORT_EMAIL}</a> 로 연락주시기 바랍니다.`,
 		2: '아래 버튼을 클릭하여 등록 절차를 진행하시기 바랍니다.',
 		3: '나의 계정 활성화'
 	},
@@ -55,13 +50,13 @@ const WELCOME = {
 	TITLE: '환영합니다',
 	GREETING: (name) => COMMON.GREETING(name),
 	BODY: {
-		1: '누구나 빠르고 쉬운 거래가 가능한 가상화폐거래소 HollaEx를 이용해주셔서 감사합니다.',
+		1: `누구나 빠르고 쉬운 거래가 가능한 가상화폐거래소 ${API_NAME}를 이용해주셔서 감사합니다.`,
 		2: (account, deposit) => `
 			거래를 시작하기 위해선 먼저, 비트코인 또는 현금을 계좌에 입금하여야 합니다.
 			${account} 페이지로 이동하여 ${deposit} 페이지를 방문해주시기 바랍니다.`,
 		3: '계정',
 		4: '입금',
-		5: '문의사항은 support@bitholla.com 로 문의해주시기 바랍니다.'
+		5: `문의사항은 ${SUPPORT_EMAIL} 로 문의해주시기 바랍니다.`
 	},
 	CLOSING: COMMON.CLOSING
 };
@@ -94,20 +89,20 @@ const RESETPASSWORD = {
 };
 
 const DEPOSIT = {
-	TITLE: (currency) => `${COMMON[currency.toUpperCase()]} ${COMMON.DEPOSIT}`,
+	TITLE: (currency) => `${currency.toUpperCase()} ${COMMON.DEPOSIT}`,
 	GREETING: (name) => COMMON.GREETING(name),
 	BODY: {
 		PENDING: {
 			1: (amount, confirmation = 1, currency) =>
-				`회원님의 HollaEx 지갑으로 ${amount} ${currency} 입금이 진행 중입니다. 거래가 승인되고 지갑에 자금이 입금될 때까지 기다려주십시오. 회원님의 거래에는 비트코인 블록체인 상 ${confirmation} 개의 승인이 요구됩니다.`,
+				`회원님의 ${API_NAME} 지갑으로 ${amount} ${currency} 입금이 진행 중입니다. 거래가 승인되고 지갑에 자금이 입금될 때까지 기다려주십시오. 회원님의 거래에는 비트코인 블록체인 상 ${confirmation} 개의 승인이 요구됩니다.`,
 			2: (amount, currency) => `수량: ${amount} ${currency}`,
 			3: '입금 상태: 비트코인 거래 승인 대기중입니다.',
 			4: (txid) => COMMON.TXID(txid),
 			5: COMMON.EXPLORER
 		},
 		COMPLETED: (amount, currency) =>
-			`회원님의 ${amount} BTC 입금이 완료되었습니다. 회원님의 ${
-				COMMON[currency.toUpperCase()]
+			`회원님의 ${amount} ${currency.toUpperCase()} 입금이 완료되었습니다. 회원님의 ${
+				currency.toUpperCase()
 			} 지갑에서 확인 및 이용하실 수 있습니다.`
 	},
 	CLOSING: COMMON.CLOSING
@@ -136,7 +131,7 @@ const ACCOUNTUPGRADE = {
 
 const DEPOSITCANCEL = {
 	TITLE: (currency, type) =>
-		`${COMMON[currency.toUpperCase()]} ${
+		`${currency.toUpperCase()} ${
 			COMMON[type.toUpperCase()]
 		} rejected`,
 	GREETING: (name) => COMMON.GREETING(name),
@@ -144,7 +139,7 @@ const DEPOSITCANCEL = {
 		DEPOSIT: (currency, date, amount) =>
 			`회원님이 ${date}에 ${amount}의 ${currency} 을 입금하신 내역을 찾을 수 없거나 처리 할 수 없습니다. 해당 거래는 시스템에 의해 거부되었습니다.`,
 		WITHDRAWAL: (currency, date, amount) =>
-			`회원님이 ${date}에 ${amount}의 ${currency} 을 출금하신 내역을 찾을 수 없거나 처리 할 수 없습니다. 해당 거래는 시스템에 의해 거부되었으며, 보류중인 회원님의 출금금액이 HollaEx 지갑으로 환불됩니다.`,
+			`회원님이 ${date}에 ${amount}의 ${currency} 을 출금하신 내역을 찾을 수 없거나 처리 할 수 없습니다. 해당 거래는 시스템에 의해 거부되었으며, 보류중인 회원님의 출금금액이 ${API_NAME} 지갑으로 환불됩니다.`,
 		1: '추가 문의 사항이 있으시다면 이 이메일에 회신해주시기 바랍니다.',
 		2: (txid) => COMMON.TXID(txid),
 		3: (amount) => COMMON.AMOUNT(amount),
@@ -155,7 +150,7 @@ const DEPOSITCANCEL = {
 
 const WITHDRAWAL = {
 	TITLE: (currency) =>
-		`${COMMON[currency.toUpperCase()]} ${COMMON.WITHDRAWAL}`,
+		`${currency.toUpperCase()} ${COMMON.WITHDRAWAL}`,
 	GREETING: (name) => COMMON.GREETING(name),
 	BODY: {
 		FIAT: {
@@ -179,7 +174,7 @@ const WITHDRAWAL = {
 
 const WITHDRAWALREQUEST = {
 	TITLE: (currency) =>
-		`${COMMON[currency.toUpperCase()]} ${COMMON.WITHDRAWAL} 요청`,
+		`${currency.toUpperCase()} ${COMMON.WITHDRAWAL} 요청`,
 	GREETING: (name) => COMMON.GREETING(name),
 	BODY: {
 		1: (currency, amount, address) =>
