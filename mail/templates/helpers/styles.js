f8cae4fa94b3c9f4e7989037cffadd788814c48a
Brandon Kim <bkim2490@gmail.com>
2019-09-25 15:31:25 +0900

    mail folder added

---
+++ mail/templates/helpers/styles.js
@@ -0,0 +1,24 @@
+module.exports = {
+	body:
+		'background-color: #f3f3f3; padding: 1rem 0; font-family: arial; color: #000 !important',
+	logo: 'text-align: center; margin-bottom: 1.5rem;',
+	wrapper: 'align-items:center; max-width: 45rem; margin: 0 auto;',
+	header:
+		'padding-top: 3rem; padding-bottom: 1rem; background-color: #333333; text-align: center',
+	header_icon: 'width: 60px',
+	header_icon_wrapper: 'text-align: center; margin-bottom:0.5rem;',
+	header_icon_title: 'color: #FFF; text-align: center; font-size: 1.3rem',
+	container:
+		'background-color: #fff; color: #000 !important; padding: 5rem; padding-top: 30px; padding-bottom: 15px;',
+	box_shadow:
+		'-webkit-box-shadow: 0px 4px 10px 2px rgba(204,204,204,1) !important; -moz-box-shadow: 0px 4px 10px 2px rgba(204,204,204,1) !important; box-shadow: 0px 4px 10px 2px rgba(204,204,204,1) !important;',
+	title: 'padding-top: 20px; padding-bottom: 20px;',
+	link: 'text-decoration: none; color: #000;',
+	link_blue: 'text-decoration: none; color: #35b2e8 !important;',
+	buttonWrapper: 'padding-top: 10px; margin-bottom: 10px;',
+	button:
+		'cursor: pinter; background-color: #333333; color: white; border: none; padding: 1rem; text-transform: uppercase; cursor: pointer !important; font-size: 14px; min-width: 11rem;',
+	footer:
+		'padding-top: 1rem; padding-bottom: 3rem; background-color: transparent; color: #000 !important; font-size: 12px',
+	poweredby: 'margin-top: 0.5rem'
+};
