c08902f20fb66e4aa8cdf51e125c174e9905e903
jsmitrah <ram@mitrahsoft.com>
2019-09-27 17:37:16 +0530

    changes for admin/pairs  API update

--- web/src/containers/Admin/UserFees/actions.js
+++ web/src/containers/Admin/UserFees/actions.js
@@ -5,8 +5,8 @@ export const requestFees = () => requestAuthenticated('/admin/pairs');
 export const feeUpdate = (id, values) => {
 	const options = {
 		method: 'PUT',
-		body: JSON.stringify(values)
+		body: JSON.stringify({ ...values, pair: id })
 	};
 
-	return requestAuthenticated(`/admin/pairs/fees/${id}`, options);
+	return requestAuthenticated(`/admin/pairs`, options);
 };
