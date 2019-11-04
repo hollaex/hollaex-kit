45c3306d90f3581ca2ddc1523399d1cc5f53c981
Jkoon <jkoon90@gmail.com>
2019-11-04 10:28:20 +0900

    Fixed the currency + '0'  Issue

--- src/components/CurrencyBall/withPrice.js
+++ src/components/CurrencyBall/withPrice.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { CurrencyBall } from '../';
+import { formatToCurrency } from '../../utils/currency';
 import {
-	formatToCurrency
-} from '../../utils/currency';
-import { BASE_CURRENCY, DEFAULT_COIN_DATA } from '../../config/constants';
+	// BASE_CURRENCY,
+	DEFAULT_COIN_DATA
+} from '../../config/constants';
 
-const CurrencyBallWithPrice = ({ symbol, amount, price, size = 'm', coins = {} }) => {
+const CurrencyBallWithPrice = ({
+	symbol,
+	amount,
+	price,
+	size = 'm',
+	coins = {}
+}) => {
 	const { name, min, ...rest } = coins[symbol] || DEFAULT_COIN_DATA;
 	// const baseCoin = coins[BASE_CURRENCY] || DEFAULT_COIN_DATA;
-	const currencyShortName = rest.symbol
-		? rest.symbol.toUpperCase()
-		: name;
+	const currencyShortName = rest.symbol ? rest.symbol.toUpperCase() : name;
 	return (
 		<div className="with_price-block_amount d-flex direction_ltr">
 			<CurrencyBall name={currencyShortName} symbol={symbol} size={size} />
 			<div className="with_price-block_amount-value d-flex">
 				{`${formatToCurrency(amount, min)}`}
-				{symbol !== BASE_CURRENCY && price && (
-					<div className={`with_price-block_amount-value-${BASE_CURRENCY.toLowerCase()} d-flex align-items-end`}>
-						
-					</div>
-				)}
+				{/* {symbol !== BASE_CURRENCY && price && (
+					<div
+						className={`with_price-block_amount-value-${BASE_CURRENCY.toLowerCase()} d-flex align-items-end`}
+					/>
+				)} */}
 			</div>
 		</div>
 	);
