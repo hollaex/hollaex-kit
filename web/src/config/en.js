30358c99d4a76a9dafaf406b3bced1d0f4c0be4b
abeikverdi <ali@bitholla.com>
2019-12-17 00:15:19 +0900

    new announcement

--- web/src/config/en.js
+++ web/src/config/en.js
@@ -1260,7 +1260,9 @@ export default {
 	TRADE_POSTS: {
 		// new
 		ANNOUNCEMENT: 'Announcement',
-		ANNOUNCEMNT_TXT:
+		ANNOUNCEMNT_TXT_3:
+			'Public launch is rescheduled to January 1st 2020. All wallets except USDT are open for deposits and withdrawals now and you can signup using the referral link.',
+		ANNOUNCEMNT_TXT_2:
 			'Public wave auction is starting from December 18th, 2019 1pm UTC. You can deposit and start placing bids at anytime and purchase HEX.',
 		ANNOUNCEMNT_TXT_1:
 			'Earn HEX with HAP program by introducing your friends to hollaex.com. {0}.',
