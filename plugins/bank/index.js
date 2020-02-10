998b9ed820c941f35574de3521dcc8a8f33c2a21
Brandon Kim <bkim2490@gmail.com>
2020-02-10 12:27:55 +0900

    add logging to bank

--- plugins/bank/index.js
+++ plugins/bank/index.js
@@ -5,14 +5,28 @@ const { verifyToken, checkScopes } = require('../helpers/auth');
 const { findUser, getUserValuesById, getUserValuesByEmail } = require('../helpers/user');
 const { addBankAccount, adminAddUserBanks, approveBankAccount, rejectBankAccount, VERIFY_ATTR } = require('./helpers');
 const bodyParser = require('body-parser');
+const { logger } = require('../helpers/common');
+const { sendEmail } = require('../../mail');
+const { MAILTYPE } = require('../../mail/strings');
 
 app.post('/plugins/bank/user', [verifyToken, bodyParser.json()], (req, res) => {
 	const endpointScopes = ['user'];
 	const scopes = req.auth.scopes;
 	checkScopes(endpointScopes, scopes);
 
+	logger.verbose(
+		'POST /bank/user auth',
+		req.auth.sub
+	);
+
 	const email = req.auth.sub.email;
 	const bank_account = req.body;
+
+	logger.info(
+		'POST /bank/user bank',
+		email,
+		bank_account
+	);
 
 	findUser({
 		where: { email }
@@ -21,6 +35,7 @@ app.post('/plugins/bank/user', [verifyToken, bodyParser.json()], (req, res) => {
 		.then(() => getUserValuesByEmail(email))
 		.then((user) => res.json(user['bank_account']))
 		.catch((error) => {
+			logger.error('POST /bank/user error', error.message);
 			res.status(error.status || 400).json({ message: error.message })
 		})
 });
@@ -30,8 +45,19 @@ app.post('/plugins/bank/admin', [verifyToken, bodyParser.json()], (req, res) => 
 	const scopes = req.auth.scopes;
 	checkScopes(endpointScopes, scopes);
 
+	logger.verbose(
+		'POST /bank/admin auth',
+		req.auth.sub
+	);
+
 	const { bank_account } = req.body;
 	const id = req.query.user_id;
+
+	logger.info(
+		'POST /bank/admin bank',
+		id,
+		bank_account
+	);
 
 	findUser({
 		where: {
@@ -42,6 +68,7 @@ app.post('/plugins/bank/admin', [verifyToken, bodyParser.json()], (req, res) => 
 		.then(() => getUserValuesById(id))
 		.then((user) => res.json(user['bank_account']))
 		.catch((error) => {
+			logger.error('POST /bank/admin error', error.message);
 			res.status(error.status || 400).json({ message: error.message })
 		});
 });
@@ -51,7 +78,17 @@ app.post('/plugins/bank/verify', [verifyToken, bodyParser.json()], (req, res) =>
 	const scopes = req.auth.scopes;
 	checkScopes(endpointScopes, scopes);
 
+	logger.verbose(
+		'POST /bank/verify auth',
+		req.auth.sub
+	);
+
 	const { user_id, bank_id } = req.body;
+
+	logger.verbose(
+		'POST /bank/verify body',
+		req.body
+	);
 
 	findUser({
 		where: {
@@ -66,9 +103,14 @@ app.post('/plugins/bank/verify', [verifyToken, bodyParser.json()], (req, res) =>
 		.then((user) => {
 			const data = {};
 			data.bank_account = user.bank_account;
+			loggerAdmin.debug(
+				'POST /bank/verify data',
+				data
+			);
 			res.json(data);
 		})
 		.catch((err) => {
+			logger.error('POST /bank/verify error', err.message);
 			res.status(err.status || 400).json({ message: err.message });
 		});
 });
@@ -78,8 +120,18 @@ app.post('/plugins/bank/revoke', [verifyToken, bodyParser.json()], (req, res) =>
 	const scopes = req.auth.scopes;
 	checkScopes(endpointScopes, scopes);
 
+	logger.verbose(
+		'POST /bank/revoke auth',
+		req.auth.sub
+	);
+
 	const { user_id, bank_id } = req.body;
 	let { message } = req.body || DEFAULT_REJECTION_NOTE;
+
+	logger.verbose(
+		'POST /bank/revoke body',
+		req.body
+	);
 
 	findUser({
 		where: {
@@ -96,9 +148,20 @@ app.post('/plugins/bank/revoke', [verifyToken, bodyParser.json()], (req, res) =>
 			const emailData = { type: 'bank', message };
 			const data = {};
 			data.bank_account = bank_account;
+			sendEmail(
+				MAILTYPE.USER_VERIFICATION_REJECT,
+				email,
+				emailData,
+				user.settings
+			);
+			loggerAdmin.debug(
+				'POST /bank/revoke data',
+				data
+			);
 			res.json(data);
 		})
 		.catch((err) => {
+			logger.error('POST /bank/revoke error', err.message);
 			res.status(err.status || 400).json({ message: err.message });
 		});
 });
