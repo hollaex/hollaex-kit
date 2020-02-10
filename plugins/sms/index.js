305a58d7b53202361cb2fd6d41a4212ab5da4199
Brandon Kim <bkim2490@gmail.com>
2020-02-10 12:35:21 +0900

    sms loggert

--- plugins/sms/index.js
+++ plugins/sms/index.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const { createSMSCode, storeSMSCode, checkSMSCode, deleteSMSCode, sendSMS, updateUserPhoneNumber } = require('./helpers');
 const { DEFAULT_LANGUAGE } = require('../../constants');
 const { SMS } = require('../../mail/strings').languageFile(DEFAULT_LANGUAGE);
+const { logger } = require('../helpers/common');
 const {
 	SMS_INVALID_PHONE,
 	SMS_SUCCESS,
@@ -19,15 +20,31 @@ app.get('/plugins/sms/verify', verifyToken, (req, res) => {
 	const scopes = req.auth.scopes;
 	checkScopes(endpointScopes, scopes);
 
+	logger.verbose(
+		'GET /sms/verify auth',
+		req.auth.sub
+	);
+
 	const phoneNumber = new PhoneNumber(req.query.phone);
 	const { id } = req.auth.sub;
 
 	if (!phoneNumber.isValid()) {
+		logger.error(
+			'GET /sms/verify not valid phone number',
+			req.query.phone
+		);
 		return res.status(400).json({ message: SMS_INVALID_PHONE });
 	}
 
 	const phone = phoneNumber.getNumber();
 	const code = createSMSCode();
+
+	logger.verbose(
+		'GET /sms/verify body',
+		id,
+		req.query.phone,
+		code
+	);
 
 	sendSMS(phone, {
 		message: SMS.verificationCode(code)
@@ -36,9 +53,17 @@ app.get('/plugins/sms/verify', verifyToken, (req, res) => {
 			return storeSMSCode(id, phone, code);
 		})
 		.then((data) => {
+			logger.debug(
+				'GET /sms/verify data',
+				data
+			);
 			res.json({ message: SMS_SUCCESS });
 		})
 		.catch((error) => {
+			logger.error(
+				'GET /sms/verify error',
+				error.message
+			);
 			return res.status(error.statusCode || 400).json({ message: error.message });
 		});
 });
@@ -48,15 +73,31 @@ app.post('/plugins/sms/verify', [verifyToken, bodyParser.json()], (req, res) => 
 	const scopes = req.auth.scopes;
 	checkScopes(endpointScopes, scopes);
 
+	logger.verbose(
+		'POST /sms/verify auth',
+		req.auth.sub
+	);
+
 	const { id } = req.auth.sub;
 	const { code } = req.body;
 	const phoneNumber = new PhoneNumber(req.body.phone);
 
 	if (!phoneNumber.isValid()) {
+		logger.error(
+			'POST /sms/verify not valid phone number',
+			req.query.phone
+		);
 		return res.status(400).json({ message: SMS_INVALID_PHONE });
 	}
 
 	const phone = phoneNumber.getNumber();
+
+	logger.verbose(
+		'POST /sms/verify body',
+		id,
+		req.query.phone,
+		code
+	);
 
 	checkSMSCode(id, phone, code)
 		.then((data) => {
@@ -71,9 +112,17 @@ app.post('/plugins/sms/verify', [verifyToken, bodyParser.json()], (req, res) => 
 			return deleteSMSCode(id);
 		})
 		.then((data) => {
+			logger.debug(
+				'POST /sms/verify data',
+				data
+			);
 			res.json({ message: PHONE_VERIFIED });
 		})
 		.catch((error) => {
+			logger.error(
+				'POST /sms/verify error',
+				error.message
+			);
 			return res.status(error.statusCode || 400).json({ message: error.message });
 		});
 });
