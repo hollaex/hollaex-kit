1aca1a7dae3be792daac76533873ad3f1b4c8e01
Yechan Kim <kycfeel@gmail.com>
2020-02-17 12:23:32 +0900

    HollaEx Kit - CLI installer now supports dependencies download for macOS with Homebrew.

--- install.sh
+++ install.sh
@@ -88,6 +88,91 @@ if command apt-get -v > /dev/null 2>&1; then
 
     fi
 
+# Dependencies installer for macOS with Homebrew.
+elif command brew -v > /dev/null 2>&1; then
+
+    if ! command docker -v > /dev/null 2>&1; then
+
+        printf "\n\033[93mHollaEx CLI requires Docker to operate. Installing it now...\033[39m\n"
+
+        echo "Updating Homebrew list"
+        brew update
+        IS_HOMEBREW_UPDATED=true
+
+        echo "Installing Docker"
+        if command brew install docker; then
+
+            printf "\n\033[92mDocker has been successfully installed!\033[39m\n"
+            echo "Info: $(docker -v)"
+
+            echo -e "\nAdding current user to Docker usergroup"
+            if command sudo gpasswd -a $USER docker; then
+
+                    DOCKER_USERGROUP_ADDED=true
+
+            fi
+
+        else
+
+            printf "\n\033[91mFailed to install Docker.\033[39m\n"
+            echo "Please review the logs and try to manually install it. - 'brew install docker'."
+            exit 1;
+
+        fi
+
+    fi
+
+    if ! command docker-compose -v > /dev/null 2>&1; then
+
+        printf "\n\033[93mHollaEx CLI requires Docker-Compose to operate. Installing it now...\033[39m\n"
+
+        if [[ ! $IS_APT_UPDATED ]]; then
+
+            echo "Updating Homebrew list"
+            brew update
+        fi
+
+        if command brew install docker-compose; then
+
+            printf "\n\033[92mDocker-Compose has been successfully installed!\033[39m\n"
+
+            echo "Info: $(docker-compose -v)"
+
+        else
+
+            printf "\n\033[91mFailed to install Docker-Compose.\033[39m\n"
+            echo "Please review the logs and try to manually install it. - 'brew install docker-compose'."
+            exit 1;
+
+        fi
+
+    fi
+
+    if ! command jq --version > /dev/null 2>&1; then
+
+        printf "\n\033[93mHollaEx CLI requires jq to operate. Installing it now...\033[39m\n"
+
+        if [[ ! $IS_APT_UPDATED ]]; then
+
+            echo "Updating Homebrew list"
+            brew update
+        fi
+
+        if command brew install jq; then
+
+            printf "\n\033[92mjq has been successfully installed!\033[39m\n"
+
+            echo "Info: $(jq --version)"
+
+        else
+
+            printf "\n\033[91mFailed to install jq.\033[39m\n"
+            echo "Please review the logs and try to manually install it. - 'brew install jq'."
+
+        fi
+
+    fi
+
 fi
 
 if ! command docker -v > /dev/null 2>&1 || ! command docker-compose -v > /dev/null 2>&1 || ! command jq --version > /dev/null 2>&1; then
