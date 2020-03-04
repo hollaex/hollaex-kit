79e1fd04e7bbd18b67e946da744fe1f74c6f92d1
Brandon Kim <bkim2490@gmail.com>
2020-03-04 17:45:05 +0900

    remove redundant redis helper

--- plugins/helpers/common.js
+++ plugins/helpers/common.js
@@ -1,12 +1,8 @@
 'use strict';
 
-// Redis client
-const redis = require('../../db/redis');
-
 // Winston logger
 const logger = require('../../config/logger').loggerPlugin;
 
 module.exports = {
-	redis,
 	logger
 };
