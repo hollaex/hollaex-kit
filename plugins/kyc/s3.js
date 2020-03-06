9c0d38e3b74ae25fc7bcd1e481769e7662382167
Brandon Kim <bkim2490@gmail.com>
2020-03-06 11:35:11 +0900

    kyc plugin reading config  from constnats

--- plugins/kyc/s3.js
+++ plugins/kyc/s3.js
@@ -1,12 +1,7 @@
 'use strict';
 
 const aws = require('aws-sdk');
-const ID_DOCS_BUCKET = process.env.ID_DOCS_BUCKET || '';
-const S3_WRITE_ACCESSKEYID = process.env.S3_WRITE_ACCESSKEYID || '';
-const S3_WRITE_SECRETACCESSKEY = process.env.S3_WRITE_SECRETACCESSKEY || '';
-const S3_READ_ACCESSKEYID = process.env.S3_READ_ACCESSKEYID || '';
-const S3_READ_SECRETACCESSKEY = process.env.S3_READ_SECRETACCESSKEY || '';
-
+const { GET_CONFIGURATION, GET_SECRETS } = require('../../constants');
 
 const generateBuckets = (bucketsString = '') => {
 	const bucketsSplit = bucketsString
@@ -24,26 +19,28 @@ const generateBuckets = (bucketsString = '') => {
 	return buckets;
 };
 
-const credentials = {
-	write: {
-		accessKeyId: S3_WRITE_ACCESSKEYID,
-		secretAccessKey: S3_WRITE_SECRETACCESSKEY
-	},
-	read: {
-		accessKeyId: S3_READ_ACCESSKEYID,
-		secretAccessKey: S3_READ_SECRETACCESSKEY
-	},
-	buckets: generateBuckets(ID_DOCS_BUCKET)
+const credentials = () => {
+	return {
+		write: {
+			accessKeyId: GET_SECRETS().plugins.s3.key.write,
+			secretAccessKey: GET_SECRETS().plugins.s3.secret.write
+		},
+		read: {
+			accessKeyId: GET_SECRETS().plugins.s3.key.read,
+			secretAccessKey: GET_SECRETS().plugins.s3.secret.read
+		},
+		buckets: generateBuckets(GET_CONFIGURATION().constants.plugins.configuration.s3.id_docs_bucket)
+	}
 };
 
 const write = (bucketName) => {
-	aws.config.update(credentials.write);
-	return new aws.S3(credentials.buckets[bucketName]);
+	aws.config.update(credentials().write);
+	return new aws.S3(credentials().buckets[bucketName]);
 };
 
 const read = (bucketName) => {
-	aws.config.update(credentials.read);
-	return new aws.S3(credentials.buckets[bucketName]);
+	aws.config.update(credentials().read);
+	return new aws.S3(credentials().buckets[bucketName]);
 };
 
 module.exports = {
