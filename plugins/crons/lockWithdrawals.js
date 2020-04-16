dbb0dc1a4b4c3886fb2c91e8738a6a7af56fc54d
Brandon Kim <bkim2490@gmail.com>
2020-04-16 14:57:31 +0900

    lockWithdrawal address check and getting vault connected coins

--- plugins/crons/lockWithdrawals.js
+++ plugins/crons/lockWithdrawals.js
@@ -2,8 +2,19 @@
 
 const { Deposit } = require('../../db/models');
 const { all } = require('bluebird');
+const { checkAddress } = require('../vault/helpers');
 const { loggerDeposits } = require('../../config/logger');
+const { each } = require('lodash');
+const { GET_SECRETS } = require('../../constants');
+const { sendEmail } = require('../../mail');
+const { MAILTYPE } = require('../../mail/strings');
 
+const vaultCoins = [];
+each(GET_SECRETS().vault.connected_coins, (coin) => {
+	vaultCoins.push({
+		currency: coin
+	});
+});
 Deposit.findAll({
 	where: {
 		type: 'withdrawal',
@@ -11,7 +22,8 @@ Deposit.findAll({
 		dismissed: false,
 		rejected: false,
 		processing: false,
-		waiting: false
+		waiting: false,
+		$or: vaultCoins
 	}
 })
 	.then((withdrawals) => {
@@ -21,15 +33,27 @@ Deposit.findAll({
 		}
 		loggerDeposits.debug(`Locking ${withdrawals.length} withdrawals`);
 		return all(withdrawals.map((withdrawal) => {
-			return withdrawal.update({ processing: true }, { fields: ['processing'], returning: true })
-				.then((result) => {
-					loggerDeposits.info(`Withdrawal with ID ${withdrawal.dataValues.id} locked`);
-					return;
-				})
-				.catch((err) => {
-					loggerDeposits.error(`Error occured while locking ${withdrawal.dataValues.id}: ${err.message}`);
-					return;
-				});
+			if (checkAddress(withdrawal.dataValues.currency)) {
+				return withdrawal.update({ processing: true }, { fields: ['processing'], returning: true })
+					.then((result) => {
+						loggerDeposits.info(`Withdrawal with ID ${withdrawal.dataValues.id} locked`);
+						return;
+					})
+					.catch((err) => {
+						loggerDeposits.error(`Error occured while locking ${withdrawal.dataValues.id}: ${err.message}`);
+						return;
+					});
+			} else {
+				return withdrawal.update({ rejected: true }, { fields: ['rejected'], returning: true })
+					.then((result) => {
+						loggerDeposits.info(`Withdrawal with ID ${withdrawal.dataValues.id} rejected because of invalid address`);
+						return sendEmail()
+					})
+					.catch((err) => {
+						loggerDeposits.error(`Error occured while locking ${withdrawal.dataValues.id}: ${err.message}`);
+						return;
+					});
+			}
 		}));
 	})
 	.then(() => {
