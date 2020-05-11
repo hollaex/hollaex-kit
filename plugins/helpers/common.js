234f5644642c9c652dbc912e648cdc5b12a6dd92
Brandon Kim <bkim2490@gmail.com>
2020-05-11 16:38:54 +0900

    common add pagaination, ordering, etc

--- plugins/helpers/common.js
+++ plugins/helpers/common.js
@@ -58,8 +58,77 @@ const isUrl = (url) => {
 	return pattern.test(url);
 };
 
+const getPagination = (limit = { value: 50 }, page = { value: 1 }) => {
+	let _limit = 50;
+	let _page = 1;
+	logger.debug('helpers/common/getPagination', _limit, _page);
+	if (limit.value) {
+		if (limit.value > 50) {
+			_limit = 50;
+		} else if (limit.value <= 0) {
+			_limit = 1;
+		} else {
+			_limit = limit.value;
+		}
+	}
+
+	if (page.value && page.value >= 0) {
+		_page = page.value;
+	}
+	logger.debug('helpers/common/getPagination', _limit, _page);
+	return {
+		limit: _limit,
+		offset: _limit * (_page - 1)
+	};
+};
+
+const convertSequelizeCountAndRows = (data) => {
+	return {
+		count: data.count,
+		data: data.rows.map((row) => {
+			const item = Object.assign({}, row.dataValues);
+			// delete item.id;
+			return item;
+		})
+	};
+};
+
+const getTimeframe = (start_date = { value: undefined }, end_date = { value: undefined }) => {
+	logger.debug(
+		'helpers/common/getTimeframe',
+		'stat_date: ',
+		start_date.value,
+		'end_date: ',
+		end_date.value
+	);
+	let timestamp = {};
+	if (start_date.value) timestamp['$gte'] = start_date.value;
+	if (end_date.value) timestamp['$lte'] = end_date.value;
+	if (Object.entries(timestamp).length === 0) return undefined;
+	return timestamp;
+};
+
+const getOrdering = (order_by = { value: undefined }, order = { value: undefined }, attr = []) => {
+	logger.debug(
+		'helpers/common/getOrdering',
+		'order_by: ',
+		order_by.value,
+		'order: ',
+		order.value
+	);
+	if (!order_by.value || !attr.includes(order_by.value)) {
+		return undefined;
+	} else {
+		return [order_by.value, order.value || 'desc'];
+	}
+};
+
 module.exports = {
 	logger,
 	updateConstants,
-	isUrl
+	isUrl,
+	getPagination,
+	getOrdering,
+	getTimeframe,
+	convertSequelizeCountAndRows
 };
