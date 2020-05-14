981ed3725138c3fcfdb7c5d3e5d65ae3481a7b1b
Brandon Kim <bkim2490@gmail.com>
2020-05-14 17:59:27 +0900

    add prettier

--- auth.js
+++ auth.js
@@ -2,16 +2,27 @@
 
 const jwt = require('jsonwebtoken');
 const { intersection } = require('lodash');
-const { ACCESS_DENIED, NOT_AUTHORIZED, TOKEN_EXPIRED, INVALID_TOKEN, MISSING_HEADER, DEACTIVATED_USER } = require('./messages');
+const {
+	ACCESS_DENIED,
+	NOT_AUTHORIZED,
+	TOKEN_EXPIRED,
+	INVALID_TOKEN,
+	MISSING_HEADER,
+	DEACTIVATED_USER
+} = require('./messages');
 
 /**
-	* Express middleware function that checks validity of bearer token.
-	* @param {string} secret - Secret used to generate token.
-	* @param {string} issuer - Issuer of valid tokens.
-	* @param {array} frozenUsers - (OPTIONAL) User ids that are deactivated.
-	* @param {array} endpointScopes - (OPTIONAL) Valid user scopes for the endpoint.
-*/
-const verifyBearerToken = (secret, issuer, frozenUsers, endpointScopes) => (req, res, next) => {
+ * Express middleware function that checks validity of bearer token.
+ * @param {string} secret - Secret used to generate token.
+ * @param {string} issuer - Issuer of valid tokens.
+ * @param {array} frozenUsers - (OPTIONAL) User ids that are deactivated.
+ * @param {array} endpointScopes - (OPTIONAL) Valid user scopes for the endpoint.
+ */
+const verifyBearerToken = (secret, issuer, frozenUsers, endpointScopes) => (
+	req,
+	res,
+	next
+) => {
 	const sendError = (msg) => {
 		return req.res.status(403).json({ message: ACCESS_DENIED(msg) });
 	};
@@ -23,7 +34,6 @@ const verifyBearerToken = (secret, issuer, frozenUsers, endpointScopes) => (req,
 
 		jwt.verify(tokenString, secret, (verificationError, decodedToken) => {
 			if (!verificationError && decodedToken) {
-
 				const issuerMatch = decodedToken.iss == issuer;
 
 				if (!issuerMatch) {
@@ -34,7 +44,10 @@ const verifyBearerToken = (secret, issuer, frozenUsers, endpointScopes) => (req,
 					return sendError(DEACTIVATED_USER);
 				}
 
-				if (endpointScopes && intersection(endpointScopes, decodedToken.scopes).length === 0) {
+				if (
+					endpointScopes &&
+					intersection(endpointScopes, decodedToken.scopes).length === 0
+				) {
 					return sendError(NOT_AUTHORIZED);
 				}
 
@@ -50,10 +63,10 @@ const verifyBearerToken = (secret, issuer, frozenUsers, endpointScopes) => (req,
 };
 
 /**
-	* Function that checks to see if user's scope is valid for the endpoint. Will throw Error if user doesn't have valid scope.
-	* @param {array} endpointScopes - Authorized scopes for the endpoint.
-	* @param {array} userScopes - Scopes of the user.
-*/
+ * Function that checks to see if user's scope is valid for the endpoint. Will throw Error if user doesn't have valid scope.
+ * @param {array} endpointScopes - Authorized scopes for the endpoint.
+ * @param {array} userScopes - Scopes of the user.
+ */
 const checkScopes = (endpointScopes, userScopes) => {
 	if (intersection(endpointScopes, userScopes).length === 0) {
 		throw new Error(NOT_AUTHORIZED);
@@ -61,10 +74,10 @@ const checkScopes = (endpointScopes, userScopes) => {
 };
 
 /**
-	* Function that checks to see if user's account is frozen. Will throw Error if user account is frozen.
-	* @param {array} frozenUsers - Ids of frozen users.
-	* @param {array} userId - Id of user.
-*/
+ * Function that checks to see if user's account is frozen. Will throw Error if user account is frozen.
+ * @param {array} frozenUsers - Ids of frozen users.
+ * @param {array} userId - Id of user.
+ */
 const checkFrozenUser = (frozenUsers, userId) => {
 	if (frozenUsers[userId]) {
 		throw new Error(DEACTIVATED_USER);
