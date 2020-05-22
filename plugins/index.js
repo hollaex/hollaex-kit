0bb2288410a45a0f50208940cd181323a2b3a1d3
Brandon Kim <bkim2490@gmail.com>
2020-05-22 16:39:55 +0900

    add available plugins as constant

--- plugins/index.js
+++ plugins/index.js
@@ -3,25 +3,20 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { PLUGIN_PORT } = require('./constants');
-const { DOMAIN, GET_CONFIGURATION } = require('../constants');
-const { readdirSync } = require('fs');
+const { PLUGIN_PORT, AVAILABLE_PLUGINS } = require('./constants');
+const { DOMAIN, GET_CONFIGURATION, GET_SECRETS } = require('../constants');
 
 const PLUGINS = () => GET_CONFIGURATION().constants.plugins.enabled || process.env.PLUGINS || '';
-const CORS_WHITELIST = [DOMAIN, 'http://localhost:8080', 'http://localhost:3000'];
+const ALLOWED_DOMAINS = () => GET_SECRETS().allowed_domains || [DOMAIN, 'http://localhost:8080', 'http://localhost:3000'];
 
 const PORT = PLUGIN_PORT;
 
 const enabledPlugins = () => PLUGINS().split(',');
 
-const availablePlugins = readdirSync(__dirname, { withFileTypes: true })
-	.filter(dirent => dirent.isDirectory() && dirent.name !== 'helpers' && dirent.name !== 'node_modules' & dirent.name !== 'crons' && dirent.name !== 'jobs')
-	.map(dirent => dirent.name);
-
 app.get('/plugins', (req, res) => {
 	res.json({
 		enabled: enabledPlugins(),
-		available: availablePlugins
+		available: AVAILABLE_PLUGINS
 	});
 });
 
@@ -29,7 +24,7 @@ app.listen(PORT);
 
 const corsOptions = {
 	origin: function (origin, callback) {
-		if (CORS_WHITELIST.indexOf(origin) !== -1) {
+		if (ALLOWED_DOMAINS().indexOf(origin) !== -1) {
 			callback (null, true);
 		} else {
 			callback(new Error('Not allowed by CORS'));
@@ -37,13 +32,17 @@ const corsOptions = {
 	}
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 module.exports = app;
 
 // Require every plugin for now
-availablePlugins.forEach((plugin) => {
+AVAILABLE_PLUGINS.forEach((plugin) => {
 	if (plugin) {
-		require('./' + plugin);
+		try {
+			require('./' + plugin);
+		} catch (err)  {
+			console.log(`${plugin} directory does not exist`);
+		}
 	}
 });
