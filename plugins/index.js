3c65c6c299b31a114b1f8e59d25f9436edc470f2
Brandon Kim <bkim2490@gmail.com>
2020-05-22 16:58:16 +0900

    removed allowed_domains from plugins/index

--- plugins/index.js
+++ plugins/index.js
@@ -4,10 +4,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { PLUGIN_PORT, AVAILABLE_PLUGINS } = require('./constants');
-const { DOMAIN, GET_CONFIGURATION, GET_SECRETS } = require('../constants');
+const { GET_CONFIGURATION } = require('../constants');
 
 const PLUGINS = () => GET_CONFIGURATION().constants.plugins.enabled || process.env.PLUGINS || '';
-const ALLOWED_DOMAINS = () => GET_SECRETS().allowed_domains || [DOMAIN, 'http://localhost:8080', 'http://localhost:3000'];
 
 const PORT = PLUGIN_PORT;
 
@@ -22,17 +21,7 @@ app.get('/plugins', (req, res) => {
 
 app.listen(PORT);
 
-const corsOptions = {
-	origin: function (origin, callback) {
-		if (ALLOWED_DOMAINS().indexOf(origin) !== -1) {
-			callback (null, true);
-		} else {
-			callback(new Error('Not allowed by CORS'));
-		}
-	}
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 module.exports = app;
 
