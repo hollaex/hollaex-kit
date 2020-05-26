38253157a39f59db003f58b2b070b1d45aacf4bf
Brandon Kim <bkim2490@gmail.com>
2020-05-26 16:33:16 +0900

    checkAddress fix for vault

--- plugins/vault/crons/lockWithdrawals.js
+++ plugins/vault/crons/lockWithdrawals.js
@@ -2,83 +2,95 @@
 
 const { Deposit, User } = require('../../../db/models');
 const { all } = require('bluebird');
-const { checkAddress } = require('../helpers');
+const WAValidator = require('multicoin-address-validator');
 const { loggerDeposits } = require('../../../config/logger');
 const { each } = require('lodash');
 const { GET_SECRETS } = require('../../../constants');
 const { sendEmail } = require('../../../mail');
 const { MAILTYPE } = require('../../../mail/strings');
 
+const checkAddress = (address, symbol, network = 'prod') => {
+	if (symbol === 'btc' || symbol === 'bch' || symbol === 'xrp' || symbol === 'xmr') {
+		return WAValidator.validate(address, symbol, network);
+	} else {
+		return WAValidator.validate(address, 'eth', network);
+	}
+};
+
 const vaultCoins = [];
 
 const lockWithdrawals = () => {
-	each(GET_SECRETS().vault.connected_coins, (coin) => {
-		vaultCoins.push({
-			currency: coin
+	return new Promise((resolve, reject) => {
+		each(GET_SECRETS().vault.connected_coins, (coin) => {
+			vaultCoins.push({
+				currency: coin
+			});
 		});
-	});
-	Deposit.findAll({
-		where: {
-			type: 'withdrawal',
-			status: false,
-			dismissed: false,
-			rejected: false,
-			processing: false,
-			waiting: false,
-			$or: vaultCoins
-		},
-		include: [
-			{
-				model: User,
-				attributes: ['email', 'settings']
-			}
-		]
-	})
-		.then((withdrawals) => {
-			if (withdrawals.length === 0) {
-				loggerDeposits.info('No withdrawals need locking');
-				return;
-			}
-			loggerDeposits.debug(`Locking ${withdrawals.length} withdrawals`);
-			return all(withdrawals.map((withdrawal) => {
-				if (checkAddress(withdrawal.address, withdrawal.currency)) {
-					return withdrawal.update({ processing: true }, { fields: ['processing'], returning: true })
-						.then((result) => {
-							loggerDeposits.info(`Withdrawal with ID ${withdrawal.id} locked`);
-							return;
-						})
-						.catch((err) => {
-							loggerDeposits.error(`Error occured while locking ${withdrawal.id}: ${err.message}`);
-							return;
-						});
-				} else {
-					return withdrawal.update({ rejected: true }, { fields: ['rejected'], returning: true })
-						.then((result) => {
-							loggerDeposits.warn(`Withdrawal with ID ${withdrawal.id} rejected because of an invalid address`);
-							return sendEmail(
-								MAILTYPE.INVALID_ADDRESS,
-								withdrawal.User.email,
-								{
-									currency: withdrawal.currency,
-									amount: withdrawal.amount,
-									address: withdrawal.address
-								},
-								withdrawal.User.settings
-							);
-						})
-						.catch((err) => {
-							loggerDeposits.error(`Error occured while locking ${withdrawal.id}: ${err.message}`);
-							return;
-						});
+		Deposit.findAll({
+			where: {
+				type: 'withdrawal',
+				status: false,
+				dismissed: false,
+				rejected: false,
+				processing: false,
+				waiting: false,
+				$or: vaultCoins
+			},
+			include: [
+				{
+					model: User,
+					attributes: ['email', 'settings']
 				}
-			}));
+			]
 		})
-		.then(() => {
-			loggerDeposits.info('lockWithdrawals finished');
-		})
-		.catch((err) => {
-			loggerDeposits.error(err.message);
-		});
+			.then((withdrawals) => {
+				if (withdrawals.length === 0) {
+					loggerDeposits.info('No withdrawals need locking');
+					resolve();
+				}
+				loggerDeposits.debug(`Locking ${withdrawals.length} withdrawals`);
+				return all(withdrawals.map((withdrawal) => {
+					if (checkAddress(withdrawal.address, withdrawal.currency)) {
+						return withdrawal.update({ processing: true }, { fields: ['processing'], returning: true })
+							.then((result) => {
+								loggerDeposits.info(`Withdrawal with ID ${withdrawal.id} locked`);
+								return;
+							})
+							.catch((err) => {
+								loggerDeposits.error(`Error occured while locking ${withdrawal.id}: ${err.message}`);
+								return;
+							});
+					} else {
+						return withdrawal.update({ rejected: true }, { fields: ['rejected'], returning: true })
+							.then((result) => {
+								loggerDeposits.warn(`Withdrawal with ID ${withdrawal.id} rejected because of an invalid address`);
+								return sendEmail(
+									MAILTYPE.INVALID_ADDRESS,
+									withdrawal.User.email,
+									{
+										currency: withdrawal.currency,
+										amount: withdrawal.amount,
+										address: withdrawal.address
+									},
+									withdrawal.User.settings
+								);
+							})
+							.catch((err) => {
+								loggerDeposits.error(`Error occured while locking ${withdrawal.id}: ${err.message}`);
+								return;
+							});
+					}
+				}));
+			})
+			.then(() => {
+				loggerDeposits.info('lockWithdrawals finished');
+				resolve();
+			})
+			.catch((err) => {
+				loggerDeposits.error('plugins/vault/crons/lockWithdrawal catch', err.message);
+				reject(err);
+			});
+	});
 };
 
 module.exports = {
