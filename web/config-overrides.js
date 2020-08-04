8d88e0173e56a75605ecd35a0d8d2dc547dc1124
Amir Hossein Salar <ah.salar@gmail.com>
2020-08-04 11:56:00 +0430

    add config-overrides to integrate SMP to CRA

---
+++ web/config-overrides.js
@@ -0,0 +1,9 @@
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const smp = new SpeedMeasurePlugin({ outputFormat: "human", outputTarget: './measure.txt' });
+
+module.exports = function (config, env) {
+	config = smp.wrap({
+		...config,
+	});
+	return config
+};
