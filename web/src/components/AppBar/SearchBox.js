81a3a2c413695a6f14ba2fa3a42c88a544fabd10
Amir Hossein Salar <ah.salar@gmail.com>
2020-08-11 10:48:11 +0430

    absolutify SearchBox component

--- web/src/components/AppBar/SearchBox.js
+++ web/src/components/AppBar/SearchBox.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import classnames from 'classnames';
 
-import { ICONS } from '../../config/constants';
-import renderFields from '../../components/Form/factoryFields';
+import { ICONS } from 'config/constants';
+import renderFields from 'components/Form/factoryFields';
 
-class SearchBox extends React.Component {
+class SearchBox extends Component {
 	render() {
 		const {
 			handleSearch,
