35b8c3aa2cca9518875f9458ae3854f59663dd91
user <ali@alireza.be>
2020-08-25 16:39:24 +0900

    exchange kit initialization with activation code

--- server/init.js
+++ server/init.js
@@ -128,18 +128,18 @@ const checkStatus = () => {
 				]);
 			}
 		})
-		.then(([activation, status]) => {
-			loggerGeneral.info('init/checkStatus/activation', activation.name, activation.active);
+		.then(([exchange, status]) => {
+			loggerGeneral.info('init/checkStatus/activation', exchange.name, exchange.active);
 			setConfiguration({
 				info: {
-					name: activation.name,
-					active: activation.active,
-					url: activation.url,
-					is_trial: activation.is_trial,
-					created_at: activation.created_at,
-					expiry: activation.expiry,
-					coins: activation.coins,
-					pairs: activation.pairs,
+					name: exchange.name,
+					active: exchange.active,
+					url: exchange.url,
+					is_trial: exchange.is_trial,
+					created_at: exchange.created_at,
+					expiry: exchange.expiry,
+					coins: exchange.coins,
+					pairs: exchange.pairs,
 					status: true
 				}
 			});
@@ -148,7 +148,8 @@ const checkStatus = () => {
 				baseURL: HE_NETWORK_BASE_URL,
 				apiKey: status.api_key,
 				apiSecret: status.secret,
-				exchange_id: activation.id
+				exchange_id: exchange.id,
+				activation_code: exchange.activation_code
 			});
 
 			return User.findAll({
