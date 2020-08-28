3454eac38756297bb4a7217ef967d72ad286f985
Brandon Kim <brandonkimmmm@Brandons-MacBook-Pro-2.local>
2020-08-28 18:02:52 +0900

    get user role and get all user

--- tools/users.js
+++ tools/users.js
@@ -10,70 +10,24 @@ const { INIT_CHANNEL } = require(`${SERVER_PATH}/constant`);
 const { sendEmail } = require(`${SERVER_PATH}/mail`);
 const { MAILTYPE } = require(`${SERVER_PATH}/mail/strings`);
 
-const getUserByEmail = (email, sequelize = false) => {
-	if (!email || !isEmail(email)) {
-		throw new Error('Please provide a valid email address');
-	}
-	return dbQuery.findOne('user', {
-		where: { email }
-	})
-		.then((user) => {
-			if (!user) {
-				throw new Error(`User with email ${email} does not exist`);
-			} else {
-				if (sequelize) {
-					return user;
-				} else {
-					return user.dataValues;
-				}
-			}
-		});
+const getAllUsers = () => {
+	return dbQuery.findAndCountAllWithRows('user');
 };
 
-const getUserByKitId = (id, sequelize = false) => {
-	if (!id) {
-		throw new Error('Please provide an id');
+const getUserByCryptoAddress = (currency, address) => {
+	if (!currency || !address) {
+		throw new Error('Please provide the user\'s currency and crypto address');
 	}
 	return dbQuery.findOne('user', {
-		where: { id }
-	})
-		.then((user) => {
-			if (!user) {
-				throw new Error(`User with kit id ${id} does not exist`);
-			} else {
-				if (sequelize) {
-					return user;
-				} else {
-					return user.dataValues;
-				}
-			}
-		});
+		where: { crypto_wallet: { [currency]: address } }
+	});
 };
 
-const getUserByNetworkId = (network_id, sequelize = false) => {
-	if (!network_id) {
-		throw new Error('Please provide an id');
-	}
-	return dbQuery.findOne('user', {
-		where: { network_id }
-	})
-		.then((user) => {
-			if (!user) {
-				throw new Error(`User with network id ${network_id} does not exist`);
-			} else {
-				if (sequelize) {
-					return user;
-				} else {
-					return user.dataValues;
-				}
-			}
-		});
-};
-
-const freezeUser = (opts = {}, sequelize = false) => {
+const getUser = (opts = {}, sequelize = false) => {
 	if (!opts.email && !opts.kit_id && !opts.network_id) {
 		throw new Error('Please provide the user\'s kit id, network id, or email');
 	}
+
 	const where = {};
 	if (opts.email) {
 		where.email = opts.email;
@@ -82,13 +36,44 @@ const freezeUser = (opts = {}, sequelize = false) => {
 	} else {
 		where.network_id = opts.network_id;
 	}
+
 	return dbQuery.findOne('user', {
 		where
 	})
 		.then((user) => {
 			if (!user) {
 				throw new Error('User does not exist');
-			} else if (!user.activated) {
+			} else {
+				return sequelize ? user : user.dataValues;
+			}
+		});
+};
+
+const getUserByEmail = (email, sequelize = false) => {
+	if (!email || !isEmail(email)) {
+		throw new Error('Please provide a valid email address');
+	}
+	return getUser({ email }, sequelize);
+};
+
+const getUserByKitId = (kit_id, sequelize = false) => {
+	if (!kit_id) {
+		throw new Error('Please provide a kit id');
+	}
+	return getUser({ kit_id }, sequelize);
+};
+
+const getUserByNetworkId = (network_id, sequelize = false) => {
+	if (!network_id) {
+		throw new Error('Please provide a network id');
+	}
+	return getUser({ network_id }, sequelize);
+};
+
+const freezeUser = (opts = {}, sequelize = false) => {
+	return getUser(opts, true)
+		.then((user) => {
+			if (!user.activated) {
 				throw new Error('User account is already frozen');
 			}
 			return user.update({ activated: false }, { fields: ['activated'], returning: true });
@@ -103,33 +88,14 @@ const freezeUser = (opts = {}, sequelize = false) => {
 				},
 				user.settings
 			);
-			if (sequelize) {
-				return user;
-			} else {
-				return user.dataValues;
-			}
+			return sequelize ? user : user.dataValues;
 		});
 };
 
 const unfreezeUser = (opts = {}, sequelize = false) => {
-	if (!opts.email && !opts.kit_id && !opts.network_id) {
-		throw new Error('Please provide the user\'s kit id, network id, or email');
-	}
-	const where = {};
-	if (opts.email) {
-		where.email = opts.email;
-	} else if (opts.kit_id) {
-		where.id = opts.kit_id;
-	} else {
-		where.network_id = opts.network_id;
-	}
-	return dbQuery.findOne('user', {
-		where
-	})
+	return getUser(opts, true)
 		.then((user) => {
-			if (!user) {
-				throw new Error('User does not exist');
-			} else if (user.activated) {
+			if (user.activated) {
 				throw new Error('User account is not frozen');
 			}
 			return user.update({ activated: true }, { fields: ['activated'], returning: true });
@@ -144,10 +110,25 @@ const unfreezeUser = (opts = {}, sequelize = false) => {
 				},
 				user.settings
 			);
-			if (sequelize) {
-				return user;
+			return sequelize ? user : user.dataValues;
+		});
+};
+
+const getUserRole = (opts = {}) => {
+	return getUser(opts, true)
+		.then((user) => {
+			if (user.is_admin) {
+				return 'admin';
+			} else if (user.is_supervisor) {
+				return 'supervisor';
+			} else if (user.is_support) {
+				return 'support';
+			} else if (user.is_kyc) {
+				return 'kyc';
+			} else if (user.is_tech) {
+				return 'tech';
 			} else {
-				return user.dataValues;
+				return 'user';
 			}
 		});
 };
@@ -156,7 +137,10 @@ module.exports = {
 	getUserByEmail,
 	getUserByKitId,
 	getUserByNetworkId,
+	getUserByCryptoAddress,
 	getFrozenUsers,
 	freezeUser,
-	unfreezeUser
+	unfreezeUser,
+	getAllUsers,
+	getUserRole
 };
