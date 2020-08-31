91928be2835e0c395ba045dfefae2d6facc47959
Brandon Kim <brandonkimmmm@Brandons-MacBook-Pro-2.local>
2020-08-31 12:16:45 +0900

    omit user fields when getiing all and update user settings added

--- constant.js
+++ constant.js
@@ -3,3 +3,22 @@
 const path = require('path');
 
 exports.SERVER_PATH = path.resolve(__dirname, '../');
+
+exports.SETTING_KEYS = [
+	'language',
+	'notification',
+	'interface',
+	'audio',
+	'risk',
+	'chat'
+];
+
+exports.OMITTED_USER_FIELDS = [
+	'password',
+	'is_admin',
+	'is_support',
+	'is_supervisor',
+	'is_kyc',
+	'is_tech',
+	'flagged'
+];
--- tools/users.js
+++ tools/users.js
@@ -2,16 +2,31 @@
 
 // const model = require('./database').model;
 const dbQuery = require('./database').query;
+const { has, omit } = require('lodash');
 const { isEmail } = require('validator');
-const { SERVER_PATH } = require('../constant');
+const { SERVER_PATH, SETTING_KEYS, OMITTED_USER_FIELDS } = require('../constant');
 const { getFrozenUsers } = require(`${SERVER_PATH}/init`);
 const { publisher } = require('./database/redis');
 const { INIT_CHANNEL } = require(`${SERVER_PATH}/constant`);
 const { sendEmail } = require(`${SERVER_PATH}/mail`);
 const { MAILTYPE } = require(`${SERVER_PATH}/mail/strings`);
+const { getKit, getSecrets, getCoins, getPairs } = require(`${SERVER_PATH}/init`);
+
+/**
+ *
+ * @param {object} user - User object
+ * @return {object}
+ */
+const omitUserFields = (user) => {
+	return omit(user, OMITTED_USER_FIELDS);
+};
 
 const getAllUsers = () => {
-	return dbQuery.findAndCountAllWithRows('user');
+	return dbQuery.findAndCountAllWithRows('user', {
+		attributes: {
+			exclude: OMITTED_USER_FIELDS
+		}
+	});
 };
 
 const getUserByCryptoAddress = (currency, address) => {
@@ -23,7 +38,7 @@ const getUserByCryptoAddress = (currency, address) => {
 	});
 };
 
-const getUser = (opts = {}, sequelize = false) => {
+const getUser = (opts = {}, rawData = true) => {
 	if (!opts.email && !opts.kit_id && !opts.network_id) {
 		throw new Error('Please provide the user\'s kit id, network id, or email');
 	}
@@ -36,6 +51,7 @@ const getUser = (opts = {}, sequelize = false) => {
 	} else {
 		where.network_id = opts.network_id;
 	}
+	where.raw = rawData;
 
 	return dbQuery.findOne('user', {
 		where
@@ -44,39 +60,39 @@ const getUser = (opts = {}, sequelize = false) => {
 			if (!user) {
 				throw new Error('User does not exist');
 			} else {
-				return sequelize ? user : user.dataValues;
+				return user;
 			}
 		});
 };
 
-const getUserByEmail = (email, sequelize = false) => {
+const getUserByEmail = (email, rawData = true) => {
 	if (!email || !isEmail(email)) {
 		throw new Error('Please provide a valid email address');
 	}
-	return getUser({ email }, sequelize);
+	return getUser({ email }, rawData);
 };
 
-const getUserByKitId = (kit_id, sequelize = false) => {
+const getUserByKitId = (kit_id, rawData = true) => {
 	if (!kit_id) {
 		throw new Error('Please provide a kit id');
 	}
-	return getUser({ kit_id }, sequelize);
+	return getUser({ kit_id }, rawData);
 };
 
-const getUserByNetworkId = (network_id, sequelize = false) => {
+const getUserByNetworkId = (network_id, rawData = true) => {
 	if (!network_id) {
 		throw new Error('Please provide a network id');
 	}
-	return getUser({ network_id }, sequelize);
+	return getUser({ network_id }, rawData);
 };
 
-const freezeUser = (opts = {}, sequelize = false) => {
-	return getUser(opts, true)
+const freezeUser = (opts = {}, rawData = true) => {
+	return getUser(opts, false)
 		.then((user) => {
 			if (!user.activated) {
 				throw new Error('User account is already frozen');
 			}
-			return user.update({ activated: false }, { fields: ['activated'], returning: true });
+			return user.update({ activated: false }, { fields: ['activated'], returning: true, raw: rawData });
 		})
 		.then((user) => {
 			publisher.publish(INIT_CHANNEL, JSON.stringify({type: 'freezeUser', data: user.id }));
@@ -88,17 +104,17 @@ const freezeUser = (opts = {}, sequelize = false) => {
 				},
 				user.settings
 			);
-			return sequelize ? user : user.dataValues;
+			return user;
 		});
 };
 
-const unfreezeUser = (opts = {}, sequelize = false) => {
-	return getUser(opts, true)
+const unfreezeUser = (opts = {}, rawData = true) => {
+	return getUser(opts, false)
 		.then((user) => {
 			if (user.activated) {
 				throw new Error('User account is not frozen');
 			}
-			return user.update({ activated: true }, { fields: ['activated'], returning: true });
+			return user.update({ activated: true }, { fields: ['activated'], returning: true, raw: rawData });
 		})
 		.then((user) => {
 			publisher.publish(INIT_CHANNEL, JSON.stringify({type: 'unfreezeUser', data: user.id }));
@@ -110,7 +126,7 @@ const unfreezeUser = (opts = {}, sequelize = false) => {
 				},
 				user.settings
 			);
-			return sequelize ? user : user.dataValues;
+			return user;
 		});
 };
 
@@ -133,6 +149,44 @@ const getUserRole = (opts = {}) => {
 		});
 };
 
+const DEFAULT_SETTINGS = {
+	language: getKit().defaults.language,
+	orderConfirmationPopup: true
+};
+
+const joinSettings = (userSettings = {}, newSettings = {}) => {
+	const joinedSettings = {};
+	SETTING_KEYS.forEach((key) => {
+		if (has(newSettings, key)) {
+			joinedSettings[key] = newSettings[key];
+		} else if (has(userSettings, key)) {
+			joinedSettings[key] = userSettings[key];
+		} else {
+			joinedSettings[key] = DEFAULT_SETTINGS[key];
+		}
+	});
+	return joinedSettings;
+};
+
+const updateUserSettings = (opts = {}, settings = {}, rawData = true) => {
+	return getUser(opts, false)
+		.then((user) => {
+			if (Object.keys(settings).length > 0) {
+				settings = joinSettings(user.dataValues.settings, settings);
+			}
+			return user.update({ settings }, {
+				fields: [
+					'settings'
+				],
+				returning: true,
+				raw: rawData
+			});
+		})
+		.then((user) => {
+			return user;
+		});
+};
+
 module.exports = {
 	getUserByEmail,
 	getUserByKitId,
@@ -142,5 +196,7 @@ module.exports = {
 	freezeUser,
 	unfreezeUser,
 	getAllUsers,
-	getUserRole
+	getUserRole,
+	updateUserSettings,
+	omitUserFields
 };
