6c740942910e9d39e10f00f328e25a0446e61b44
Amir Hossein Salar <ah.salar@gmail.com>
2020-09-03 01:30:36 +0430

    improve confirm trade style

--- web/src/components/QuickTrade/ReviewBlock.js
+++ web/src/components/QuickTrade/ReviewBlock.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { oneOfType, string, number, bool } from 'prop-types';
-import classnames from 'classnames';
 import { CurrencyBallWithPrice } from 'components';
 
 const ReviewBlock = ({ text, amount, symbol }) => {
 
 	return (
-		<div className={classnames('review-block-wrapper', 'd-flex', 'flex-column')}>
+		<div className="review-block-wrapper d-flex flex-column">
 			{ text && (
 				<div className="input_block-title text-left">
           {text}
