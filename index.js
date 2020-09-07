3688c93db22e9d6fd577ffff3d0afbd47c92be02
Brandon Kim <brandonkimmmm@Brandons-MacBook-Pro-2.local>
2020-09-07 16:18:49 +0900

    export library directly

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-module.exports = (opts = {}) => {
-	if (opts.apiKey && opts.apiSecret && opts.exchangeId) {
-		process.env.API_KEY = opts.apiKey;
-		process.env.API_SECRET = opts.apiSecret;
-		process.env.EXCHANGE_ID = opts.exchangeId;
-		process.env.NETWORK_URL = opts.apiURL;
-		process.env.NETWORK_BASE_URL = opts.baseURL;
-	}
-	return require('./tools');
-};
+module.exports = require('./tools');
