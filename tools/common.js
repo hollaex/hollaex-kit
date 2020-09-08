2bf94603c70b6d6d08ce167179bf5858a8308fa4
Brandon Kim <bkim2490@gmail.com>
2020-09-08 12:57:06 +0900

    add mask secrets

--- tools/common.js
+++ tools/common.js
@@ -2,6 +2,8 @@
 
 const { SERVER_PATH } = require('../constants');
 const { getKit, getSecrets, getCoins, getPairs } = require(`${SERVER_PATH}/init`);
+const { SECRET_MASK } = require(`${SERVER_PATH}/constants`);
+const { each } = require('lodash');
 
 /**
  * Checks if url given is a valid url.
@@ -29,10 +31,35 @@ const getKitPairs = () => {
 	return getPairs();
 };
 
+const maskSecrets = (secrets) => {
+	each(secrets, (secret, secretKey) => {
+		if (secretKey === 'captcha') {
+			secret.secret_key = SECRET_MASK;
+		} else if (secretKey === 'smtp') {
+			secret.password = SECRET_MASK;
+		} else if (secretKey === 'plugins') {
+			each(secret, (plugin, pluginKey) => {
+				if (pluginKey === 's3') {
+					plugin.secret =  SECRET_MASK;
+				} else if (pluginKey === 'sns') {
+					plugin.secret = SECRET_MASK;
+				} else if (pluginKey === 'freshdesk') {
+					plugin.key = SECRET_MASK;
+					plugin.auth = SECRET_MASK;
+				} else if (pluginKey === 'zendesk') {
+					plugin.key = SECRET_MASK;
+				}
+			});
+		}
+	});
+	return secrets;
+};
+
 module.exports = {
 	isUrl,
 	getKitConfig,
 	getKitSecrets,
 	getKitCoins,
-	getKitPairs
+	getKitPairs,
+	maskSecrets
 };
