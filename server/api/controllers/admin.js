9655d5326afb61c41dc22ef2013b493425af932d
Brandon Kim <bkim2490@gmail.com>
2020-09-09 13:09:16 +0900

    /admin/deposits withdrawals

--- server/api/controllers/admin.js
+++ server/api/controllers/admin.js
@@ -382,6 +382,72 @@ const getUserAudits = (req, res) => {
 		});
 };
 
+const getDeposits = (req, res) => {
+	loggerAdmin.verbose(
+		req.uuid,
+		'controllers/admin/getDeposits/auth',
+		req.auth
+	);
+
+	const { user_id, currency, limit, page, order_by, order, start_date, end_date, format } = req.swagger.params;
+
+	toolsLib.users.getUserDepositsByKitId(user_id.value, currency.value, limit.value, page.value, order_by.value, order.value, start_date.value, end_date.value, format.value)
+		.then((data) => {
+			if (format.value) {
+				if (data.data.length === 0) {
+					throw new Error('No data found');
+				}
+				const csv = parse(data.data, Object.keys(data.data[0]));
+				res.setHeader('Content-disposition', `attachment; filename=${toolsLib.getKitConfig().api_name}-users-deposits.csv`);
+				res.set('Content-Type', 'text/csv');
+				return res.status(202).send(csv);
+			} else {
+				return res.json(data);
+			}
+		})
+		.catch((err) => {
+			loggerAdmin.error(
+				req.uuid,
+				'controllers/admin/getDeposits',
+				err.message
+			);
+			return res.status(err.status || 400).json({ message: err.message });
+		});
+};
+
+const getWithdrawals = (req, res) => {
+	loggerAdmin.verbose(
+		req.uuid,
+		'controllers/admin/getWithdrawals/auth',
+		req.auth
+	);
+
+	const { user_id, currency, limit, page, order_by, order, start_date, end_date, format } = req.swagger.params;
+
+	toolsLib.users.getUserWithdrawalsByKitId(user_id.value, currency.value, limit.value, page.value, order_by.value, order.value, start_date.value, end_date.value, format.value)
+		.then((data) => {
+			if (format.value) {
+				if (data.data.length === 0) {
+					throw new Error('No data found');
+				}
+				const csv = parse(data.data, Object.keys(data.data[0]));
+				res.setHeader('Content-disposition', `attachment; filename=${toolsLib.getKitConfig().api_name}-users-deposits.csv`);
+				res.set('Content-Type', 'text/csv');
+				return res.status(202).send(csv);
+			} else {
+				return res.json(data);
+			}
+		})
+		.catch((err) => {
+			loggerAdmin.error(
+				req.uuid,
+				'controllers/admin/getWithdrawals',
+				err.message
+			);
+			return res.status(err.status || 400).json({ message: err.message });
+		});
+};
+
 module.exports = {
 	getAdminKit,
 	putAdminKit,
@@ -398,5 +464,7 @@ module.exports = {
 	deactivateOtpAdmin,
 	flagUser,
 	getAdminUserLogins,
-	getUserAudits
+	getUserAudits,
+	getDeposits,
+	getWithdrawals
 };
