7a5549635662cea09014284d27b3bc46a2097035
Brandon Kim <bkim2490@gmail.com>
2020-09-14 16:12:42 +0900

    add queries to deposits,withdrawals

--- server/api/controllers/admin.js
+++ server/api/controllers/admin.js
@@ -382,9 +382,9 @@ const getDeposits = (req, res) => {
 		req.auth
 	);
 
-	const { user_id, currency, limit, page, order_by, order, start_date, end_date, format } = req.swagger.params;
+	const { user_id, currency, limit, page, order_by, order, start_date, end_date, status, dismissed, rejected, processing, waiting, format } = req.swagger.params;
 
-	toolsLib.users.getUserDepositsByKitId(user_id.value, currency.value, limit.value, page.value, order_by.value, order.value, start_date.value, end_date.value, format.value)
+	toolsLib.users.getUserDepositsByKitId(user_id.value, currency.value, status.value, dismissed.value, rejected.value, processing.value, waiting.value, limit.value, page.value, order_by.value, order.value, start_date.value, end_date.value, format.value)
 		.then((data) => {
 			if (format.value) {
 				if (data.data.length === 0) {
@@ -414,9 +414,9 @@ const getWithdrawals = (req, res) => {
 		req.auth
 	);
 
-	const { user_id, currency, limit, page, order_by, order, start_date, end_date, format } = req.swagger.params;
+	const { user_id, currency, limit, page, order_by, order, start_date, end_date, status, dismissed, rejected, processing, waiting, format } = req.swagger.params;
 
-	toolsLib.users.getUserWithdrawalsByKitId(user_id.value, currency.value, limit.value, page.value, order_by.value, order.value, start_date.value, end_date.value, format.value)
+	toolsLib.users.getUserWithdrawalsByKitId(user_id.value, currency.value, status.value, dismissed.value, rejected.value, processing.value, waiting.value, limit.value, page.value, order_by.value, order.value, start_date.value, end_date.value, format.value)
 		.then((data) => {
 			if (format.value) {
 				if (data.data.length === 0) {
