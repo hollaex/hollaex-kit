ed3b7545f20dd271e822ce6e2eb9ec75ec7838c6
Brandon Kim <bkim2490@gmail.com>
2020-09-18 15:26:42 +0900

    /trades

--- server/api/controllers/engine.js
+++ server/api/controllers/engine.js
@@ -1,10 +1,21 @@
 'use strict';
 
 const { loggerOrderbook } = require('../../config/logger');
+const toolsLib = require('hollaex-tools-lib');
 const { getNodeLib } = require('../../init');
 
 const getTopOrderbooks = (req, res) => {
 	const symbol = req.swagger.params.symbol.value;
+
+	if (symbol && !toolsLib.subscribedToPair(symbol)) {
+		loggerOrderbook.error(
+			req.uuid,
+			'controller/engine/getTopOrderbooks',
+			'Invalid symbol'
+		);
+		return res.status(400).json({ message: 'Invalid symbol' });
+	}
+
 	getNodeLib().getOrderbooksEngine(symbol)
 		.then((data) => {
 			return res.json(data);
@@ -19,6 +30,33 @@ const getTopOrderbooks = (req, res) => {
 		});
 };
 
+const getTrades = (req, res) => {
+	const symbol = req.swagger.params.symbol.value;
+
+	if (symbol && !toolsLib.subscribedToPair(symbol)) {
+		loggerOrderbook.error(
+			req.uuid,
+			'controller/engine/getTopOrderbooks',
+			'Invalid symbol'
+		);
+		return res.status(400).json({ message: 'Invalid symbol' });
+	}
+
+	getNodeLib().getTradesEngine(symbol)
+		.then((data) => {
+			return res.json(data);
+		})
+		.catch((err) => {
+			loggerOrderbook.error(
+				req.uuid,
+				'controller/engine/getTrades',
+				err.message
+			);
+			return res.status(err.status || 400).json({ message: err.message });
+		});
+};
+
 module.exports = {
-	getTopOrderbooks
+	getTopOrderbooks,
+	getTrades
 };
