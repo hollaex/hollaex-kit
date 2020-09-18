5c34de0c19dda8cbab073e97995816d150afc564
Brandon Kim <bkim2490@gmail.com>
2020-09-18 15:38:44 +0900

    /ticker, /ticker/all

--- server/api/controllers/engine.js
+++ server/api/controllers/engine.js
@@ -56,7 +56,50 @@ const getTrades = (req, res) => {
 		});
 };
 
+const getTicker = (req, res) => {
+	const symbol = req.swagger.params.symbol.value;
+
+	if (!toolsLib.subscribedToPair(symbol)) {
+		loggerOrderbook.error(
+			req.uuid,
+			'controller/engine/getTopOrderbooks',
+			'Invalid symbol'
+		);
+		return res.status(400).json({ message: 'Invalid symbol' });
+	}
+
+	getNodeLib().getTickerEngine(symbol)
+		.then((data) => {
+			return res.json(data);
+		})
+		.catch((err) => {
+			loggerOrderbook.error(
+				req.uuid,
+				'controller/engine/getTicker',
+				err.message
+			);
+			return res.status(err.status || 400).json({ message: err.message });
+		});
+};
+
+const getAllTicker = (req, res) => {
+	getNodeLib().getAllTickersEngine()
+		.then((data) => {
+			return res.json(data);
+		})
+		.catch((err) => {
+			loggerOrderbook.error(
+				req.uuid,
+				'controller/engine/getAllTicker',
+				err.message
+			);
+			return res.status(err.status || 400).json({ message: err.message });
+		});
+};
+
 module.exports = {
 	getTopOrderbooks,
-	getTrades
+	getTrades,
+	getTicker,
+	getAllTicker
 };
