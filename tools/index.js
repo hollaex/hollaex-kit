b4454844ab81d2af95d432eb7cfdd4c5d273071d
Brandon Kim <bkim2490@gmail.com>
2020-09-24 12:40:55 +0900

    plugins => plugin

--- tools/index.js
+++ tools/index.js
@@ -2,20 +2,9 @@
 
 module.exports = {
 	...require('./common'),
-
-	// auth
 	auth: require('./auth'),
-
-	// database
 	database: require('./database'),
-
 	order: require('./order'),
-
-	// logger
-	logger: require('./logger'),
-
-	// plugins
-	plugins: require('./plugins'),
-
+	plugin: require('./plugin'),
 	user: require('./user')
 };
