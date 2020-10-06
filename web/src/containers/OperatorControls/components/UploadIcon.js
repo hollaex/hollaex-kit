568b420dac2307d614247fbde55106745420f9db
Amir Hossein Salar <ah.salar@gmail.com>
2020-10-06 14:21:00 +0330

    UploadIcon component initial commit

---
+++ web/src/containers/OperatorControls/components/UploadIcon.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import Modal from 'components/Dialog/DesktopDialog';
+import { bool, func, array } from 'prop-types';
+import { Button } from 'antd';
+
+class UploadIcon extends Component {
+
+  state = {
+    selectedFiles: {},
+  }
+
+  onFileChange = ({ target: { name, files } }) => {
+    this.setState(prevState => ({
+      ...prevState,
+      selectedFiles: {
+        ...prevState.selectedFiles,
+        [name]: files[0],
+      }
+    }))
+  };
+
+  handleSave = () => {
+    const { onSave } = this.props;
+    const { selectedFiles } = this.state;
+
+    const path = '/somewhere';
+    const icons = {};
+
+    Object.entries(selectedFiles).forEach(([key, file]) => {
+      if (file) {
+        // Upload and then
+        icons[key] = path;
+      }
+    })
+
+    onSave(icons);
+  }
+
+  render() {
+    const { isOpen, onCloseDialog, editId } = this.props;
+
+    return (
+      <Modal
+        isOpen={isOpen}
+        label="operator-controls-modal"
+        className="operator-controls__modal"
+        disableTheme={true}
+        onCloseDialog={onCloseDialog}
+        shouldCloseOnOverlayClick={true}
+        showCloseText={true}
+        bodyOpenClassName="operator-controls__modal-open"
+      >
+        <div className="operator-controls__all-strings-header">
+          <div className="operator-controls__modal-title">
+            Upload Icon
+          </div>
+        </div>
+        <div className="pt-4">
+          {editId.map((id) => (
+            <div key={id} className="pb-1">
+              <input
+                name={id}
+                type="file"
+                accept="image/*"
+                style={{ width: '232px' }}
+                onChange={this.onFileChange}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="d-flex justify-content-end pt-3 mt-3">
+          <Button
+            block
+            type="primary"
+            className="operator-controls__save-button"
+            onClick={this.handleSave}
+          >
+            Save
+          </Button>
+        </div>
+      </Modal>
+    );
+  }
+}
+
+UploadIcon.propTypes = {
+  editId: array.isRequired,
+  isOpen: bool.isRequired,
+  onCloseDialog: func.isRequired,
+  onSave: func.isRequired,
+}
+
+export default UploadIcon;
