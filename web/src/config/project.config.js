6d9980f25b20a70e0c2c8c031a51782b791a55f2
Amir Hossein Salar <ah.salar@gmail.com>
2020-10-08 07:40:21 +0330

    add icons to project configs

--- web/src/config/project.config.js
+++ web/src/config/project.config.js
@@ -1,7 +1,9 @@
 import React from "react";
+import icons from 'config/icons';
 
 const config = {
   DEFAULT_LANGUAGE: process.env.REACT_APP_DEFAULT_LANGUAGE || 'en',
+  icons,
 }
 
 const ProjectConfig = React.createContext("appConfig")
