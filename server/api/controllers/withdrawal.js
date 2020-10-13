c47f6067f4e9f630bf7f0868e1ed49b239e4250a
Brandon Kim <bkim2490@gmail.com>
2020-10-13 11:58:19 +0900

    withdrawal csv fix

--- server/api/controllers/withdrawal.js
+++ server/api/controllers/withdrawal.js
@@ -114,9 +114,6 @@ const getAdminWithdrawals = (req, res) => {
 	toolsLib.user.getUserWithdrawalsByKitId(user_id.value, currency.value, status.value, dismissed.value, rejected.value, processing.value, waiting.value, limit.value, page.value, order_by.value, order.value, start_date.value, end_date.value, format.value)
 		.then((data) => {
 			if (format.value) {
-				if (data.data.length === 0) {
-					throw new Error('No data found');
-				}
 				res.setHeader('Content-disposition', `attachment; filename=${toolsLib.getKitConfig().api_name}-users-deposits.csv`);
 				res.set('Content-Type', 'text/csv');
 				return res.status(202).send(data);
@@ -147,9 +144,6 @@ const getUserWithdrawals = (req, res) => {
 	toolsLib.user.getUserWithdrawalsByKitId(user_id, currency, limit.value, page.value, order_by.value, order.value, start_date.value, end_date.value, format.value)
 		.then((data) => {
 			if (format.value) {
-				if (data.data.length === 0) {
-					throw new Error('No data found');
-				}
 				res.setHeader('Content-disposition', `attachment; filename=${toolsLib.getKitConfig().api_name}-withdrawals.csv`);
 				res.set('Content-Type', 'text/csv');
 				return res.status(202).send(data);
