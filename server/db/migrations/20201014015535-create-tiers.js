254f41909328749fd993ed70dce79123fb18568d
user <ali@alireza.be>
2020-10-14 15:40:27 +0900

    tier model

---
+++ server/db/migrations/20201014015535-create-tiers.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable(
+			'Tiers',
+			{
+				id: {
+					allowNull: false,
+					primaryKey: true,
+					type: Sequelize.INTEGER
+				},
+				name: {
+					type: Sequelize.STRING,
+					allowNull: false
+				},
+				description: {
+					type: Sequelize.STRING,
+					allowNull: false
+				},
+				deposit_limit: {
+					type: Sequelize.DOUBLE,
+					defaultValue: 0
+				},
+				withdrawal_limit: {
+					type: Sequelize.DOUBLE,
+					defaultValue: 0
+				},
+				fees: {
+					type: Sequelize.JSONB,
+					defaultValue: {}
+				},
+				created_at: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal('NOW()')
+				},
+				updated_at: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal('NOW()')
+				},
+
+			},
+			{
+				timestamps: true,
+				underscored: true
+			}
+		);
+	},
+	down: (queryInterface) => queryInterface.dropTable('Tiers')
+};
