3d4d85223fb5770234392a12e7d97fde7af3971c
Amir Hossein Salar <ah.salar@gmail.com>
2020-10-16 21:37:39 +0330

    make IdentityVerification and IdentityVerificationHome pages editable

--- web/src/containers/Verification/IdentityVerificationHome.js
+++ web/src/containers/Verification/IdentityVerificationHome.js
@@ -4,6 +4,7 @@ import { Button, PanelInformationRow } from '../../components';
 import { getCountry } from './utils';
 import STRINGS from '../../config/localizedStrings';
 import { getFormatTimestamp } from '../../utils/utils';
+import { EditWrapper } from 'components';
 
 const formatBirthday = {
     en: 'DD, MMMM, YYYY',
@@ -15,6 +16,7 @@ const IdentityVerificationHome = ({ user, activeLanguage, setActivePageContent, 
     if (!address.country) {
         return (
             <div>
+                <EditWrapper stringId="USER_VERIFICATION.START_IDENTITY_VERIFICATION" />
                 <Button label={STRINGS["USER_VERIFICATION.START_IDENTITY_VERIFICATION"]} onClick={() => setActivePageContent('kyc')} />
             </div>
         );
@@ -28,15 +30,18 @@ const IdentityVerificationHome = ({ user, activeLanguage, setActivePageContent, 
                             {STRINGS["USER_VERIFICATION.DOCUMENT_SUBMISSION"]}
                         </span>)
                     }
+                    <EditWrapper stringId="USER_VERIFICATION.BANK_VERIFICATION_HELP_TEXT,USER_VERIFICATION.DOCUMENT_SUBMISSION" />
                 </div>
                 <div className="my-3">
                     <PanelInformationRow
+                        stringId="USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.FULL_NAME_LABEL"
                         label={STRINGS["USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.FULL_NAME_LABEL"]}
                         information={user.full_name}
                         className="title-font"
                         disable />
                     <div className="d-flex">
                         <PanelInformationRow
+                            stringId="USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.GENDER_LABEL,USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.GENDER_OPTIONS.MAN,USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.GENDER_OPTIONS.WOMAN"
                             label={STRINGS["USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.GENDER_LABEL"]}
                             information={
                                 user.gender === false
@@ -46,23 +51,27 @@ const IdentityVerificationHome = ({ user, activeLanguage, setActivePageContent, 
                             className="title-font divider"
                             disable />
                         <PanelInformationRow
+                            stringId="USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.DOB_LABEL"
                             label={STRINGS["USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.DOB_LABEL"]}
                             information={getFormatTimestamp(user.dob, formatBirthday[activeLanguage])}
                             className="title-font"
                             disable />
                     </div>
                     <PanelInformationRow
+                        stringId="USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.NATIONALITY_LABEL"
                         label={STRINGS["USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.NATIONALITY_LABEL"]}
                         information={getCountry(user.nationality).name}
                         className="title-font"
                         disable />
                     <div className="d-flex">
                         <PanelInformationRow
+                            stringId="USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.COUNTRY_LABEL"
                             label={STRINGS["USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.COUNTRY_LABEL"]}
                             information={getCountry(address.country).name}
                             className="title-font divider"
                             disable />
                         <PanelInformationRow
+                            stringId="USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.CITY_LABEL"
                             label={STRINGS["USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.CITY_LABEL"]}
                             information={address.city}
                             className="title-font"
@@ -71,12 +80,14 @@ const IdentityVerificationHome = ({ user, activeLanguage, setActivePageContent, 
                     <div className="d-flex">
                         <div className="w-75">
                             <PanelInformationRow
+                                stringId="USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.ADDRESS_LABEL"
                                 label={STRINGS["USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.ADDRESS_LABEL"]}
                                 information={address.address}
                                 className="title-font divider"
                                 disable />
                         </div>
                         <PanelInformationRow
+                            stringId="USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.POSTAL_CODE_LABEL"
                             label={STRINGS["USER_VERIFICATION.USER_DOCUMENTATION_FORM.FORM_FIELDS.POSTAL_CODE_LABEL"]}
                             information={address.postal_code}
                             className="title-font"
@@ -85,9 +96,15 @@ const IdentityVerificationHome = ({ user, activeLanguage, setActivePageContent, 
                 </div>
                 {id_data.status === 3
                     ? null
-                    :<Button
-                        label={STRINGS["USER_VERIFICATION.REVIEW_IDENTITY_VERIFICATION"]}
-                        onClick={() => setActivePageContent('kyc')} />
+                    : (
+                      <div>
+                          <EditWrapper stringId="USER_VERIFICATION.REVIEW_IDENTITY_VERIFICATION" />
+                          <Button
+                            label={STRINGS["USER_VERIFICATION.REVIEW_IDENTITY_VERIFICATION"]}
+                            onClick={() => setActivePageContent('kyc')}
+                          />
+                      </div>
+                  )
                 }
             </div>
         );
