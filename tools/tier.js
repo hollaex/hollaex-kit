bcee20d9e3af4e6784c120d08d37f5680124ac94
Brandon Kim <bkim2490@gmail.com>
2020-10-20 17:34:41 +0900

    updateTier added

--- tools/tier.js
+++ tools/tier.js
@@ -56,6 +56,95 @@ const createTier = (level, name, description, deposit_limit, withdrawal_limit, f
 		});
 };
 
+const updateTier = (level, updateData) => {
+	const existingTiers = getKitTiers();
+
+	if (!existingTiers[level]) {
+		return reject(new Error('Tier does not exist'));
+	}
+
+	if (
+		updateData.fees
+		&& updateData.fees.maker
+		&& Object.keys(updateData.fees.maker).length > 0
+		&& difference(Object.keys(updateData.fees.maker), getKitPairs()).length > 0
+	) {
+		return reject(new Error('Fees includes a symbol that you are not subscribed to'));
+	}
+
+	if (
+		updateData.fees
+		&& updateData.fees.taker
+		&& Object.keys(updateData.fees.taker).length > 0
+		&& difference(Object.keys(updateData.fees.taker), getKitPairs()).length > 0
+	) {
+		return reject(new Error('Fees includes a symbol that you are not subscribed to'));
+	}
+
+	return dbQuery.findOne('tier', {
+		where: {
+			id: level
+		}
+	})
+		.then((tier) => {
+			const newData = {};
+
+			let updatedMakerFee = tier.fees.maker;
+			if (
+				updateData.fees
+				&& updateData.fees.maker
+			) {
+				updatedMakerFee = { ...updatedMakerFee, ...updateData.fees.maker };
+			}
+
+			let updatedTakerFee = tier.fees.taker;
+			if (
+				updateData.fees
+				&& updateData.fees.taker
+			) {
+				updatedTakerFee = { ...updatedTakerFee, ...updateData.fees.taker };
+			}
+
+			if (updateData.name) {
+				newData.name = updateData.name;
+			}
+
+			if (updateData.description) {
+				newData.description = updateData.description;
+			}
+
+			if (updateData.deposit_limit) {
+				newData.deposit_limit = updateData.deposit_limit;
+			}
+
+			if (updateData.withdrawal_limit) {
+				newData.withdrawal_limit = updateData.deposit_limit;
+			}
+
+			newData.fees = {
+				maker: updatedMakerFee,
+				taker: updatedTakerFee
+			};
+
+			return tier.update(newData, { returning: true });
+		})
+		.then((tier) => {
+			publisher.publish(
+				CONFIGURATION_CHANNEL,
+				JSON.stringify({
+					type: 'update',
+					data: {
+						tiers: {
+							[tier.id]: tier
+						}
+					}
+				})
+			);
+			return tier;
+		});
+};
+
 module.exports = {
-	createTier
+	createTier,
+	updateTier
 };
