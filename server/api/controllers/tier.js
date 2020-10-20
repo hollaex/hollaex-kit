995100ab845d2c9089fce5b742f2b7ee4046e247
Brandon Kim <bkim2490@gmail.com>
2020-10-20 17:47:59 +0900

    post, put tier

--- server/api/controllers/tier.js
+++ server/api/controllers/tier.js
@@ -14,6 +14,46 @@ const getTiers = (req, res) => {
 	}
 };
 
+const postTier = (req, res) => {
+	loggerTier.verbose(req.uuid, 'controllers/tier/postTier auth', req.auth);
+
+	const { level, name, description, deposit_limit, withdrawal_limit, fees } = req.swagger.params.data.value;
+
+	toolsLib.tier.createTier(level, name, description, deposit_limit, withdrawal_limit, fees)
+		.then((tier) => {
+			return res.json(tier);
+		})
+		.catch((err) => {
+			loggerTier.error(req.uuid, 'controllers/tier/postTier err', err.message);
+			return res.status(err.status || 400).json({ message: err.message });
+		});
+};
+
+const putTier = (req, res) => {
+	loggerTier.verbose(req.uuid, 'controllers/tier/putTier auth', req.auth);
+
+	const { level, name, description, deposit_limit, withdrawal_limit, fees } = req.swagger.params.data.value;
+
+	const updateData = {
+		name,
+		description,
+		deposit_limit,
+		withdrawal_limit,
+		fees
+	};
+
+	toolsLib.tier.updateTier(level, updateData)
+		.then((tier) => {
+			return res.json(tier);
+		})
+		.catch((err) => {
+			loggerTier.error(req.uuid, 'controllers/tier/postTier err', err.message);
+			return res.status(err.status || 400).json({ message: err.message });
+		});
+};
+
 module.exports = {
-	getTiers
+	getTiers,
+	postTier,
+	putTier
 };
