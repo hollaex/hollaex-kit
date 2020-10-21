09fe3eaf2727da15b5070b7abad95f36fb8ada4c
Brandon Kim <bkim2490@gmail.com>
2020-10-21 14:49:27 +0900

    check wtihdrawal limit for request withdrawal

--- tools/transaction.js
+++ tools/transaction.js
@@ -9,15 +9,20 @@ const { subscribedToCoin, getKitCoin, getKitSecrets } = require('./common');
 const {
 	INVALID_OTP_CODE,
 	INVALID_WITHDRAWAL_TOKEN,
-	EXPIRED_WITHDRAWAL_TOKEN
+	EXPIRED_WITHDRAWAL_TOKEN,
+	INVALID_COIN,
+	INVALID_AMOUNT,
+	WITHDRAWAL_DISABLED_FOR_COIN,
+	UPGRADE_VERIFICATION_LEVEL
 } = require('../messages');
 const { getUserByKitId } = require('./user');
+const math = require('mathjs');
+const { findTier, estimateNativeCurrencyPrice } = require('./tier');
 const { client } = require('./database/redis');
 const crypto = require('crypto');
 const uuid = require('uuid/v4');
 const { all, reject } = require('bluebird');
 const { getNodeLib } = require(`${SERVER_PATH}/init`);
-const { INVALID_COIN, INVALID_AMOUNT, WITHDRAWAL_DISABLED_FOR_COIN, UPGRADE_VERIFICATION_LEVEL } = require('../messages');
 
 // const validateWithdraw = (currency, address, amount) => {
 
@@ -47,6 +52,25 @@ const sendRequestWithdrawalEmail = (id, address, amount, currency, otpCode, capt
 		.then((user) => {
 			if (user.verification_level < 1) {
 				throw new Error(UPGRADE_VERIFICATION_LEVEL(1));
+			}
+			return all([
+				user,
+				findTier(user.verification_level)
+			]);
+		})
+		.then(async ([ user, tier ]) => {
+			const limit = tier.withdrawal_limit;
+			if (limit === -1) {
+				throw new Error('Withdrawals are disabled for this coin');
+			} else if (limit > 0) {
+				const coinConvertedToNativeCurrency = await estimateNativeCurrencyPrice(currency);
+
+				if (coinConvertedToNativeCurrency > 0) {
+					const convertedAmount = math.number(math.multiply(math.bignumber(amount), math.bignumber(coinConvertedToNativeCurrency)));
+					if (convertedAmount > limit) {
+						throw new Error('Amount exceeds withdrawal limit');
+					}
+				}
 			}
 			return withdrawRequestEmail(
 				user,
