4dd9e7833058674a87624ab2d01b11378339e89c
Brandon Kim <bkim2490@gmail.com>
2020-10-23 15:02:06 +0900

    icon added

--- server/api/controllers/tier.js
+++ server/api/controllers/tier.js
@@ -17,11 +17,11 @@ const getTiers = (req, res) => {
 const postTier = (req, res) => {
 	loggerTier.verbose(req.uuid, 'controllers/tier/postTier auth', req.auth);
 
-	const { level, name, description, deposit_limit, withdrawal_limit, fees } = req.swagger.params.data.value;
+	const { level, name, icon, description, deposit_limit, withdrawal_limit, fees } = req.swagger.params.data.value;
 
 	loggerTier.info(req.uuid, 'controllers/tier/postTier new tier', level, name, description);
 
-	toolsLib.tier.createTier(level, name, description, deposit_limit, withdrawal_limit, fees)
+	toolsLib.tier.createTier(level, name, icon, description, deposit_limit, withdrawal_limit, fees)
 		.then((tier) => {
 			loggerTier.verbose(req.uuid, 'controllers/tier/postTier new tier created', level);
 			return res.json(tier);
@@ -35,10 +35,11 @@ const postTier = (req, res) => {
 const putTier = (req, res) => {
 	loggerTier.verbose(req.uuid, 'controllers/tier/putTier auth', req.auth);
 
-	const { level, name, description, deposit_limit, withdrawal_limit } = req.swagger.params.data.value;
+	const { level, name, icon, description, deposit_limit, withdrawal_limit } = req.swagger.params.data.value;
 
 	const updateData = {
 		name,
+		icon,
 		description,
 		deposit_limit,
 		withdrawal_limit
