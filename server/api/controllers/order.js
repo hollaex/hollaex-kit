fd55a16072b4c78cb50bafa1fa5ae7eb7639101a
Brandon Kim <bkim2490@gmail.com>
2020-10-28 13:10:39 +0900

    status and open

--- server/api/controllers/order.js
+++ server/api/controllers/order.js
@@ -75,13 +75,14 @@ const cancelUserOrder = (req, res) => {
 const getAllUserOrders = (req, res) => {
 	loggerOrders.verbose(req.uuid, 'controllers/order/getAllUserOrders auth', req.auth);
 	const user_id = req.auth.sub.id;
-	const { symbol, side, type, limit, page, order_by, order, start_date, end_date } = req.swagger.params;
+	const { symbol, side, status, open, limit, page, order_by, order, start_date, end_date } = req.swagger.params;
 
 	toolsLib.order.getAllUserOrdersByKitId(
 		user_id,
 		symbol.value,
 		side.value,
-		type.value,
+		status.value,
+		open.value,
 		limit.value,
 		page.value,
 		order_by.value,
@@ -115,7 +116,7 @@ const cancelAllUserOrders = (req, res) => {
 
 const getAdminUserOrders = (req, res) => {
 	loggerOrders.verbose(req.uuid, 'controllers/order/getAdminUserOrders/auth', req.auth);
-	const { user_id, symbol, side, type, limit, page, order_by, order, start_date, end_date } = req.swagger.params;
+	const { user_id, symbol, side, status, open, limit, page, order_by, order, start_date, end_date } = req.swagger.params;
 
 	let promiseQuery;
 
@@ -124,7 +125,8 @@ const getAdminUserOrders = (req, res) => {
 			user_id.value,
 			symbol.value,
 			side.value,
-			type.value,
+			status.value,
+			open.value,
 			limit.value,
 			page.value,
 			order_by.value,
@@ -136,7 +138,8 @@ const getAdminUserOrders = (req, res) => {
 		promiseQuery = toolsLib.order.getAllExchangeOrders(
 			symbol.value,
 			side.value,
-			type.value,
+			status.value,
+			open.value,
 			limit.value,
 			page.value,
 			order_by.value,
