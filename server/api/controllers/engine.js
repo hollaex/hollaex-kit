f2a1498a42246e7c44b72f704c8ec44809e55a83
Brandon Kim <bkim2490@gmail.com>
2020-11-02 10:41:57 +0900

    get oracle price/prices added

--- server/api/controllers/engine.js
+++ server/api/controllers/engine.js
@@ -207,6 +207,48 @@ const getSymbols = (req, res) => {
 		});
 };
 
+const getAssetPrice = (req, res) => {
+	loggerEngine.verbose(req.uuid, 'controllers/engine/getAssetPrice auth', req.auth);
+
+	const { asset, quote, amount } = req.swagger.params;
+
+	loggerEngine.info(req.uuid, 'controllers/engine/getAssetPrice asset', asset.value, 'quote', quote.value, 'amount', amount.value);
+
+	toolsLib.getAssetPrice(asset.value, quote.value, amount.value)
+		.then((data) => {
+			return res.json(data);
+		})
+		.catch((err) => {
+			loggerEngine.error(
+				req.uuid,
+				'controller/engine/getAssetPrice',
+				err.message
+			);
+			return res.status(err.status || 400).json({ message: err.message });
+		});
+};
+
+const getAssetsPrices = (req, res) => {
+	loggerEngine.verbose(req.uuid, 'controllers/engine/getAssetsPrices auth', req.auth);
+
+	const { assets, quote, amount } = req.swagger.params;
+
+	loggerEngine.info(req.uuid, 'controllers/engine/getAssetsPrices assets', assets.value, 'quote', quote.value, 'amount', amount.value);
+
+	toolsLib.getAssetsPrices(assets.value, quote.value, amount.value)
+		.then((data) => {
+			return res.json(data);
+		})
+		.catch((err) => {
+			loggerEngine.error(
+				req.uuid,
+				'controller/engine/getAssetsPrices',
+				err.message
+			);
+			return res.status(err.status || 400).json({ message: err.message });
+		});
+};
+
 module.exports = {
 	getTopOrderbooks,
 	getTrades,
@@ -216,5 +258,7 @@ module.exports = {
 	getCharts,
 	getConfig,
 	getHistory,
-	getSymbols
+	getSymbols,
+	getAssetPrice,
+	getAssetsPrices
 };
