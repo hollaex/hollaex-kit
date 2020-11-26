c31beb3cdec792a7ec0f81f00abca5b60e229f2f
Ali Beikverdi <ali@bitholla.com>
2020-11-26 19:40:31 +0900

    plugins table migration

---
+++ server/db/migrations/20201126102745-create-plugins.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const TABLE_NAME = 'Plugins';
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable(
+			TABLE_NAME,
+			{
+				id: {
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+					type: Sequelize.INTEGER
+				},
+				version: {
+					type: Sequelize.INTEGER,
+					defaultValue: 1
+				},
+				name: {
+					type: Sequelize.STRING,
+					allowNull: false
+				},
+				description: {
+					type: Sequelize.STRING,
+				},
+				author: {
+					type: Sequelize.STRING,
+				},
+				url: {
+					type: Sequelize.STRING,
+				},
+				logo: {
+					type: Sequelize.STRING,
+				},
+				script: {
+					type: Sequelize.TEXT,
+					allowNull: false
+				},
+				created_at: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal('NOW()')
+				},
+				updated_at: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal('NOW()')
+				}
+			},
+			{
+				timestamps: true,
+				underscored: true
+			}
+		);
+	},
+	down: (queryInterface) => queryInterface.dropTable(TABLE_NAME)
+};
