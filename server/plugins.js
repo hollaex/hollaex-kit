7516fb7ed253e59235079349959befd480354ab7
Brandon Kim <bkim2490@gmail.com>
2020-12-01 15:10:59 +0900

    post Plugin

--- server/plugins.js
+++ server/plugins.js
@@ -1,11 +1,5 @@
 'use strict';
 
-// run server (express)
-// populate plugin DB scripts using library
-
-
-// possible challenge (new libaries not included in package.json)
-
 var app = require('express')();
 const _eval = require('eval');
 const lodash = require('lodash');
@@ -13,10 +7,19 @@ const PORT = process.env.PLUGIN_PORT || 10090;
 const toolsLib = require('hollaex-tools-lib');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
-const { loggerPlugin } = require('./config/logger');
+const { checkSchema } = expressValidator;
+const morgan = require('morgan');
+const { logEntryRequest, stream, loggerPlugin } = require('./config/logger');
+const { domainMiddleware, helmetMiddleware } = require('./config/middleware');
+const morganType = process.env.NODE_ENV === 'development' ? 'dev' : 'combined';
 
+app.use(morgan(morganType, { stream }));
 app.listen(PORT);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(logEntryRequest);
+app.use(domainMiddleware);
+helmetMiddleware(app);
 
 app.get('/plugins', (req, res) => {
 	res.json({ message: 'Welcome to HollaEx Plugins' });
@@ -24,19 +27,79 @@ app.get('/plugins', (req, res) => {
 
 app.post('/plugins', [
 	toolsLib.security.verifyBearerTokenExpressMiddleware(['admin']),
-	expressValidator.body('name').isString().isLength({ min: 1 }).withMessage('must be a string with at least 1 character'),
-	expressValidator.body('script').isString().isLength({ min: 5 }).withMessage('must be a string with at least 5 characters'),
-	expressValidator.body('version').isNumeric()
+	checkSchema({
+		name: {
+			in: ['body'],
+			errorMessage: 'must be a string',
+			isString: true,
+			isLength: {
+				errorMessage: 'must be minimum length of 1',
+				options: { min: 1 }
+			},
+			optional: false
+		},
+		script: {
+			in: ['body'],
+			errorMessage: 'must be a string',
+			isString: true,
+			isLength: {
+				errorMessage: 'must be minimum length of 5',
+				options: { min: 5 }
+			},
+			optional: false
+		},
+		version: {
+			in: ['body'],
+			errorMessage: 'must be a number',
+			isNumeric: true,
+			optional: false
+		},
+		description: {
+			in: ['body'],
+			errorMessage: 'must be a string',
+			isString: true,
+			optional: true
+		},
+		author: {
+			in: ['body'],
+			errorMessage: 'must be a string',
+			isString: true,
+			optional: true
+		},
+		url: {
+			in: ['body'],
+			errorMessage: 'must be a string',
+			isString: true,
+			optional: true
+		},
+		logo: {
+			in: ['body'],
+			errorMessage: 'must be a string',
+			isString: true,
+			optional: true
+		},
+		enabled: {
+			in: ['body'],
+			errorMessage: 'must be a boolean',
+			isBoolean: true,
+			optional: true
+		}
+	})
 ], (req, res) => {
 	const errors = expressValidator.validationResult(req);
 	if (!errors.isEmpty()) {
-		loggerPlugin.error('POST /plugins validation error');
 		return res.status(400).json({ errors: errors.array() });
 	}
 
+	loggerPlugin.verbose(
+		req.uuid,
+		'GET /plugins auth',
+		req.auth.sub
+	);
+
 	const { name, script, version, description, author, url, logo, enabled } = req.body;
 
-	loggerPlugin.verbose('POST /plugins name', name, 'version', version);
+	loggerPlugin.info(req.uuid, 'POST /plugins name', name, 'version', version);
 
 	toolsLib.database.findOne('plugin', {
 		where: { name, version }
@@ -57,15 +120,15 @@ app.post('/plugins', [
 			});
 		})
 		.then((plugin) => {
-			loggerPlugin.info('POST /plugins installed', name);
+			loggerPlugin.info(req.uuid, 'POST /plugins installed', name);
 			if (plugin.enabled) {
-				loggerPlugin.info('POST /plugins enabled', name);
+				loggerPlugin.info(req.uuid, 'POST /plugins enabled', name);
 				_eval(plugin.script, plugin.name, { app, toolsLib, lodash, expressValidator }, true);
 			}
 			return res.json(plugin);
 		})
 		.catch((err) => {
-			loggerPlugin.error('POST /plugins err', err.name);
+			loggerPlugin.error(req.uuid, 'POST /plugins err', err.name);
 			return res.status(err.status || 400).json({ message: err.message });
 		});
 });
@@ -76,6 +139,7 @@ toolsLib.database.findAll('plugin', {
 })
 	.then((plugins) => {
 		lodash.each(plugins, (plugin) => {
+			loggerPlugin.verbose('plugin', plugin.name, 'enabled');
 			_eval(plugin.script, plugin.name, { app, toolsLib, lodash, expressValidator, loggerPlugin }, true);
 		});
 	});
