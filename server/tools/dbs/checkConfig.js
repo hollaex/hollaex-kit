7bb3a291a4351dc851623751a94cf9b875e9c42f
Brandon Kim <bkim2490@gmail.com>
2020-12-02 15:29:18 +0900

    logo_path => logo_image

--- server/tools/dbs/checkConfig.js
+++ server/tools/dbs/checkConfig.js
@@ -35,8 +35,7 @@ Status.findOne()
 			},
 			setup_completed: isBoolean(existingKitConfigurations.setup_completed) ? existingKitConfigurations.setup_completed : false,
 			native_currency: existingKitConfigurations.native_currency || process.env.NATIVE_CURRENCY,
-			logo_path: existingKitConfigurations.logo_path || process.env.LOGO_PATH,
-			logo_black_path: existingKitConfigurations.logo_black_path || process.env.LOGO_BLACK_PATH,
+			logo_image: existingKitConfigurations.logo_image || existingKitConfigurations.logo_path || process.env.LOGO_IMAGE,
 			valid_languages: existingKitConfigurations.valid_languages || process.env.VALID_LANGUAGES || (process.env.NEW_USER_DEFAULT_LANGUAGE ? process.env.NEW_USER_DEFAULT_LANGUAGE.split(',') : 'en'),
 			new_user_is_activated: existingKitConfigurations.new_user_is_activated || (process.env.NEW_USER_IS_ACTIVATED && process.env.NEW_USER_IS_ACTIVATED === 'true') || false,
 			captcha: {
