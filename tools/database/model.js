2e563d736978ea51c6521daaebd96d9c316b2a6e
Brandon Kim <bkim2490@gmail.com>
2020-12-07 12:42:42 +0900

    model and query update

--- tools/database/model.js
+++ tools/database/model.js
@@ -3,6 +3,7 @@
 const { SERVER_PATH } = require('../../constants');
 const models = require(`${SERVER_PATH}/db/models`);
 const { PROVIDE_TABLE_NAME } = require(`${SERVER_PATH}/messages`);
+const { capitalize, camelCase } = require('lodash');
 
 /**
  * Get sequelize model of table.
@@ -36,7 +37,57 @@ const update = (table, query = {}, options = {}) => {
 	return getModel(table).update(query, options);
 };
 
+const createModel = (
+	name,
+	properties = {},
+	options = {
+		timestamps: true,
+		underscored: true
+	}
+) => {
+	const result =  models.sequelize.import(name, (sequelize, DataTypes) => {{
+		const modelProperties = {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: DataTypes.INTEGER
+			}
+		};
+
+		for (let prop in properties) {
+			if (!properties[prop].type) {
+				throw new Error('Type not given for property ' + prop);
+			}
+			properties[prop].type = DataTypes[properties[prop].type.toUpperCase()];
+			modelProperties[prop] = properties[prop];
+		}
+		const model = models.sequelize.define(
+			name.split(' ').map((word) => `${capitalize(word)}`).join(''),
+			modelProperties,
+			{
+				timestamps: true,
+				underscored: true,
+				...options
+			}
+		);
+		return model;
+	}});
+
+	return result;
+};
+
+const associateModel = (model, association, associatedModel, options = {}) => {
+	model.associate = (models) => {
+		model[camelCase(association)](models[associatedModel.split(' ').map((word) => `${capitalize(word)}`).join('')], options);
+	};
+
+	model.associate(models);
+};
+
 module.exports = {
+	createModel,
+	associateModel,
 	getModel,
 	create,
 	destroy,
