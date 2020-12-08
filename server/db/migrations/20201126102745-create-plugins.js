57848c353cb3c389a6228ceb7d69ec2b52b10714
Brandon Kim <bkim2490@gmail.com>
2020-12-08 13:40:28 +0900

    plugin migration update

--- server/db/migrations/20201126102745-create-plugins.js
+++ server/db/migrations/20201126102745-create-plugins.js
@@ -4,78 +4,89 @@ const TABLE_NAME = 'Plugins';
 
 module.exports = {
 	up: (queryInterface, Sequelize) => {
-		return queryInterface.createTable(
-			TABLE_NAME,
-			{
-				id: {
-					allowNull: false,
-					autoIncrement: true,
-					primaryKey: true,
-					type: Sequelize.INTEGER
-				},
-				version: {
-					type: Sequelize.INTEGER,
-					defaultValue: 1
-				},
-				name: {
-					type: Sequelize.STRING,
-					allowNull: false,
-					unique: true
-				},
-				description: {
-					type: Sequelize.STRING,
-				},
-				author: {
-					type: Sequelize.STRING,
-				},
-				url: {
-					type: Sequelize.STRING,
-				},
-				logo: {
-					type: Sequelize.STRING,
-				},
-				script: {
-					type: Sequelize.TEXT,
-					allowNull: false
-				},
-				enabled: {
-					type: Sequelize.BOOLEAN,
-					allowNull: false,
-					defaultValue: true
-				},
-				prescript: {
-					type: Sequelize.JSONB,
-					defaultValue: {
-						install: [],
-						run: null
-					}
-				},
-				postscript: {
-					type: Sequelize.JSONB,
-					defaultValue: {
-						run: null
-					}
-				},
-				meta: {
-					type: Sequelize.JSONB,
-					defaultValue: {}
-				},
-				created_at: {
-					allowNull: false,
-					type: Sequelize.DATE,
-					defaultValue: Sequelize.literal('NOW()')
-				},
-				updated_at: {
-					allowNull: false,
-					type: Sequelize.DATE,
-					defaultValue: Sequelize.literal('NOW()')
+		return queryInterface.createTable(TABLE_NAME, {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			version: {
+				type: Sequelize.INTEGER,
+				defaultValue: 1
+			},
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				unique: true
+			},
+			enabled: {
+				type: Sequelize.BOOLEAN,
+				defaultValue: true
+			},
+			author: {
+				type: Sequelize.STRING
+			},
+			bio: {
+				type: Sequelize.STRING
+			},
+			description: {
+				type: Sequelize.STRING
+			},
+			documentation: {
+				type: Sequelize.STRING
+			},
+			logo: {
+				type: Sequelize.STRING
+			},
+			icon: {
+				type: Sequelize.STRING
+			},
+			url: {
+				type: Sequelize.STRING
+			},
+			meta: {
+				type: Sequelize.JSONB,
+				defaultValue: {}
+			},
+			prescript: {
+				type: Sequelize.JSONB,
+				defaultValue: {
+					install: [],
+					run: null
 				}
 			},
-			{
-				timestamps: true,
-				underscored: true
+			postscript: {
+				type: Sequelize.JSONB,
+				defaultValue: {
+					run: null
+				}
+			},
+			script: {
+				type: Sequelize.TEXT,
+				allowNull: false
+			},
+			admin_view: {
+				type: Sequelize.TEXT
+			},
+			web_view: {
+				type: Sequelize.TEXT
+			},
+			created_at: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal('NOW()')
+			},
+			updated_at: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal('NOW()')
 			}
-		);
+		},
+		{
+			timestamps: true,
+			underscored: true
+		});
 	},
 	down: (queryInterface) => queryInterface.dropTable(TABLE_NAME)
 };
