b4b8baffa1ae2ac50716131ead09a143c1b23876
Brandon Kim <bkim2490@gmail.com>
2020-12-08 13:40:35 +0900

    model update

--- server/db/models/plugin.js
+++ server/db/models/plugin.js
@@ -1,60 +1,70 @@
-module.exports = function(sequelize, DataTypes) {
-	const Plugin = sequelize.define(
-		'Plugin',
-		{
-			version: {
-				type: DataTypes.INTEGER,
-				defaultValue: 1
-			},
-			name: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				unique: true
-			},
-			description: {
-				type: DataTypes.STRING,
-			},
-			author: {
-				type: DataTypes.STRING,
-			},
-			url: {
-				type: DataTypes.STRING,
-			},
-			logo: {
-				type: DataTypes.STRING,
-			},
-			script: {
-				type: DataTypes.TEXT,
-				allowNull: false
-			},
-			enabled: {
-				type: DataTypes.BOOLEAN,
-				allowNull: false,
-				defaultValue: true
-			},
-			prescript: {
-				type: DataTypes.JSONB,
-				defaultValue: {
-					install: [],
-					run: null
-				}
-			},
-			postscript: {
-				type: DataTypes.JSONB,
-				defaultValue: {
-					run: null
-				}
-			},
-			meta: {
-				type: DataTypes.JSONB,
-				defaultValue: {}
-			},
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+	const Plugin = sequelize.define('Plugin', {
+		version: {
+			type: DataTypes.INTEGER,
+			defaultValue:1
 		},
-		{
-			timestamps: true,
-			underscored: true
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		enabled: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: true
+		},
+		author: {
+			type: DataTypes.STRING
+		},
+		bio: {
+			type: DataTypes.STRING
+		},
+		description: {
+			type: DataTypes.STRING
+		},
+		documentation: {
+			type: DataTypes.STRING
+		},
+		logo: {
+			type: DataTypes.STRING
+		},
+		icon: {
+			type: DataTypes.STRING
+		},
+		url: {
+			type: DataTypes.STRING
+		},
+		meta: {
+			type: DataTypes.JSONB,
+			defaultValue: {}
+		},
+		prescript: {
+			type: DataTypes.JSONB,
+			defaultValue: {
+				install: [],
+				run: null
+			}
+		},
+		postscript: {
+			type: DataTypes.JSONB,
+			defaultValue: {
+				run: null
+			}
+		},
+		script: {
+			type: DataTypes.TEXT,
+			allowNull: false
+		},
+		admin_view: {
+			type: DataTypes.TEXT
+		},
+		web_view: {
+			type: DataTypes.TEXT
 		}
-	);
+	}, {
+		timestamps: true,
+		underscored: true
+	});
 
 	return Plugin;
 };
