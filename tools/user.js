e4bbcd3f6e862216311c9f6c226d4b143759ca65
Brandon Kim <bkim2490@gmail.com>
2020-12-14 17:50:39 +0900

    user get fix

--- tools/user.js
+++ tools/user.js
@@ -415,7 +415,7 @@ const getAllUsersAdmin = (opts = {
 	};
 	if (opts.id || opts.search) {
 		query.attributes = {
-			exclude: ['balance', 'password', 'updated_at']
+			exclude: ['password', 'is_admin', 'is_support', 'is_supervisor', 'is_kyc', 'is_communicator', 'updated_at']
 		};
 		if (opts.id) {
 			query.where.id = opts.id;
