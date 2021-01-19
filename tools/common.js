8f96527ae13da53201649972e441c88ac2f33da1
Brandon Kim <bkim2490@gmail.com>
2021-01-19 18:00:44 +0900

    update kit fix

--- tools/common.js
+++ tools/common.js
@@ -118,19 +118,6 @@ const maskSecrets = (secrets) => {
 			secret.secret_key = SECRET_MASK;
 		} else if (secretKey === 'smtp') {
 			secret.password = SECRET_MASK;
-		} else if (secretKey === 'plugins') {
-			each(secret, (plugin, pluginKey) => {
-				if (pluginKey === 's3') {
-					plugin.secret =  SECRET_MASK;
-				} else if (pluginKey === 'sns') {
-					plugin.secret = SECRET_MASK;
-				} else if (pluginKey === 'freshdesk') {
-					plugin.key = SECRET_MASK;
-					plugin.auth = SECRET_MASK;
-				} else if (pluginKey === 'zendesk') {
-					plugin.key = SECRET_MASK;
-				}
-			});
 		}
 	});
 	return secrets;
