b319418f27755dd9a44e8718c1573315f9afd0fd
Amir Hossein Salar <ah.salar@gmail.com>
2021-01-26 11:15:17 +0330

    add upstream theme logic

--- web/src/containers/OperatorControls/components/AddTheme.js
+++ web/src/containers/OperatorControls/components/AddTheme.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Modal from 'components/Dialog/DesktopDialog';
 import { bool, object, func, string } from 'prop-types';
 import { Input, Button, Radio, Divider, Collapse, Tooltip } from 'antd';
@@ -38,6 +38,8 @@ class AddTheme extends Component {
 
 		this.state = {
 			isEditTheme,
+			upstreamSelected: isEditTheme,
+			upstreamTheme: 'white',
 			themeKey,
 			theme: filteredTheme,
 			isSingleBase,
@@ -179,6 +181,24 @@ class AddTheme extends Component {
 		return Object.keys(systemThemes).includes(themeKey);
 	};
 
+	handleUpstreamTheme = ({ target: { value } }) => {
+		this.setState({
+			upstreamTheme: value,
+		});
+	};
+
+	next = () => {
+		const { upstreamTheme } = this.state;
+
+		if (upstreamTheme) {
+			const upstreamThemeObject = systemThemes[upstreamTheme];
+			this.setState({
+				theme: upstreamThemeObject,
+				upstreamSelected: true,
+			});
+		}
+	};
+
 	render() {
 		const { isOpen, onCloseDialog } = this.props;
 		const {
@@ -188,6 +208,8 @@ class AddTheme extends Component {
 			isSingleBase,
 			baseRatios,
 			// isDarken,
+			upstreamTheme,
+			upstreamSelected,
 		} = this.state;
 
 		return (
@@ -206,79 +228,186 @@ class AddTheme extends Component {
 						{`${isEditTheme ? 'Edit' : 'Add'} theme`}
 					</div>
 				</div>
-				<div className="my-4 d-flex align-center">
-					<div className="bold mr-4">Theme:</div>
-					<Input
-						disabled={isEditTheme}
-						type="text"
-						name="theme-key"
-						placeholder="Please enter a theme name"
-						className="operator-controls__input mr-2"
-						value={themeKey}
-						onChange={this.handleThemeKey}
-					/>
-					<Button
-						ghost
-						shape="circle"
-						size="small"
-						className="operator-controls__all-strings-settings-button"
-						disabled={!this.isSystemDefined()}
-						onClick={this.resetTheme}
-						icon={<UndoOutlined />}
-					/>
-				</div>
-				<div className="mb-5">
-					<Group onChange={this.handleBaseMode} value={isSingleBase}>
-						<Radio value={false}>
-							<Tooltip
-								overlayStyle={{ zIndex: 10001 }}
-								title="Edit multiple colors in the theme separately"
-							>
-								Use separated base
-							</Tooltip>
-						</Radio>
-						<Radio value={true}>
-							<Tooltip
-								overlayStyle={{ zIndex: 10001 }}
-								title="Easily edit the whole theme through a single base background color"
-							>
-								Use single base
-							</Tooltip>
-						</Radio>
-					</Group>
-					{/*{isSingleBase && (*/}
-					{/*<div className="pl-5">*/}
-					{/*<Group onChange={this.handleColorMode} value={isDarken}>*/}
-					{/*<Radio value={true}>Darken</Radio>*/}
-					{/*<Radio value={false}>Lighten</Radio>*/}
-					{/*</Group>*/}
-					{/*</div>*/}
-					{/*)}*/}
-				</div>
-				<div>
-					{Object.entries(nestedStructure)
-						.filter(
-							([clusterKey, clusterObj]) =>
-								!isSingleBase || clusterKey === 'base'
-						)
-						.map(([clusterKey, clusterObj]) => {
-							const renderCollapse = clusterKey === 'base' && isSingleBase;
+				{!upstreamSelected && (
+					<Fragment>
+						<div className="my-4">
+							Select the base color type. This can be directly edited later in
+							the separated base color settings.
+						</div>
+						<div className="mb-4">
+							<Group onChange={this.handleUpstreamTheme} value={upstreamTheme}>
+								<Radio value="white">Light based theme</Radio>
+								<Radio value="dark">Dark based theme</Radio>
+							</Group>
+						</div>
+					</Fragment>
+				)}
+				{upstreamSelected && (
+					<Fragment>
+						<div className="my-4 d-flex align-center">
+							<div className="bold mr-4">Theme:</div>
+							<Input
+								disabled={isEditTheme}
+								type="text"
+								name="theme-key"
+								placeholder="Please enter a theme name"
+								className="operator-controls__input mr-2"
+								value={themeKey}
+								onChange={this.handleThemeKey}
+							/>
+							<Button
+								ghost
+								shape="circle"
+								size="small"
+								className="operator-controls__all-strings-settings-button"
+								disabled={!this.isSystemDefined()}
+								onClick={this.resetTheme}
+								icon={<UndoOutlined />}
+							/>
+						</div>
+						<div className="mb-5">
+							<Group onChange={this.handleBaseMode} value={isSingleBase}>
+								<Radio value={false}>
+									<Tooltip
+										overlayStyle={{ zIndex: 10001 }}
+										title="Edit multiple colors in the theme separately"
+									>
+										Use separated base
+									</Tooltip>
+								</Radio>
+								<Radio value={true}>
+									<Tooltip
+										overlayStyle={{ zIndex: 10001 }}
+										title="Easily edit the whole theme through a single base background color"
+									>
+										Use single base
+									</Tooltip>
+								</Radio>
+							</Group>
+							{/*{isSingleBase && (*/}
+							{/*<div className="pl-5">*/}
+							{/*<Group onChange={this.handleColorMode} value={isDarken}>*/}
+							{/*<Radio value={true}>Darken</Radio>*/}
+							{/*<Radio value={false}>Lighten</Radio>*/}
+							{/*</Group>*/}
+							{/*</div>*/}
+							{/*)}*/}
+						</div>
+						<div>
+							{Object.entries(nestedStructure)
+								.filter(
+									([clusterKey, clusterObj]) =>
+										!isSingleBase || clusterKey === 'base'
+								)
+								.map(([clusterKey, clusterObj]) => {
+									const renderCollapse = clusterKey === 'base' && isSingleBase;
 
-							return (
-								<div className="pb-4" key={clusterKey}>
-									<Divider orientation="left">
-										<span className="caps">
-											<BgColorsOutlined /> {clusterKey}
-										</span>
-									</Divider>
-									{renderCollapse ? (
-										<Collapse defaultActiveKey={['1']} bordered={false} ghost>
-											<Collapse.Panel showArrow={false} key="1" disabled={true}>
-												{Object.keys(clusterObj)
-													.filter(
-														(localColorKey) => localColorKey === 'background'
-													)
-													.map((localColorKey) => {
+									return (
+										<div className="pb-4" key={clusterKey}>
+											<Divider orientation="left">
+												<span className="caps">
+													<BgColorsOutlined /> {clusterKey}
+												</span>
+											</Divider>
+											{renderCollapse ? (
+												<Collapse
+													defaultActiveKey={['1']}
+													bordered={false}
+													ghost
+												>
+													<Collapse.Panel
+														showArrow={false}
+														key="1"
+														disabled={true}
+													>
+														{Object.keys(clusterObj)
+															.filter(
+																(localColorKey) =>
+																	localColorKey === 'background'
+															)
+															.map((localColorKey) => {
+																const colorKey = `${clusterKey}_${localColorKey}`;
+																const isCalculated = this.isCalculated(
+																	colorKey
+																);
+																const colorValue = isCalculated
+																	? baseRatios[colorKey]
+																	: theme[colorKey];
+
+																return (
+																	<ColorInput
+																		colorKey={colorKey}
+																		isCalculated={isCalculated}
+																		colorValue={colorValue}
+																		pickerHandler={this.pickerHandler}
+																		onReset={this.onReset}
+																		validateColor={this.validateColor}
+																		onChange={this.handleInputChange}
+																	/>
+																);
+															})}
+													</Collapse.Panel>
+													<Collapse.Panel
+														showArrow={false}
+														header={
+															<span>
+																<CaretDownOutlined /> Modify theme breakdown
+															</span>
+														}
+														key="2"
+													>
+														{Object.keys(clusterObj)
+															.filter(
+																(localColorKey) =>
+																	localColorKey !== 'background'
+															)
+															.map((localColorKey) => {
+																const colorKey = `${clusterKey}_${localColorKey}`;
+																const isCalculated = this.isCalculated(
+																	colorKey
+																);
+																const colorValue = isCalculated
+																	? baseRatios[colorKey]
+																	: theme[colorKey];
+
+																return (
+																	<ColorInput
+																		colorKey={colorKey}
+																		isCalculated={isCalculated}
+																		colorValue={colorValue}
+																		pickerHandler={this.pickerHandler}
+																		onReset={this.onReset}
+																		validateColor={this.validateColor}
+																		onChange={this.handleInputChange}
+																	/>
+																);
+															})}
+														{isSingleBase && (
+															<div className="pt-3">
+																<span>
+																	To edit more colors directly try switching to
+																	the
+																</span>
+																<Button
+																	type="link"
+																	value={false}
+																	className="operator-button_underline"
+																	onClick={() =>
+																		this.setState({
+																			isSingleBase: false,
+																		})
+																	}
+																>
+																	separated base color
+																</Button>
+																<span>configuration</span>
+															</div>
+														)}
+													</Collapse.Panel>
+												</Collapse>
+											) : (
+												<div className="pt-2">
+													{Object.keys(clusterObj).map((localColorKey) => {
 														const colorKey = `${clusterKey}_${localColorKey}`;
 														const isCalculated = this.isCalculated(colorKey);
 														const colorValue = isCalculated
@@ -297,99 +426,38 @@ class AddTheme extends Component {
 															/>
 														);
 													})}
-											</Collapse.Panel>
-											<Collapse.Panel
-												showArrow={false}
-												header={
-													<span>
-														<CaretDownOutlined /> Modify theme breakdown
-													</span>
-												}
-												key="2"
-											>
-												{Object.keys(clusterObj)
-													.filter(
-														(localColorKey) => localColorKey !== 'background'
-													)
-													.map((localColorKey) => {
-														const colorKey = `${clusterKey}_${localColorKey}`;
-														const isCalculated = this.isCalculated(colorKey);
-														const colorValue = isCalculated
-															? baseRatios[colorKey]
-															: theme[colorKey];
-
-														return (
-															<ColorInput
-																colorKey={colorKey}
-																isCalculated={isCalculated}
-																colorValue={colorValue}
-																pickerHandler={this.pickerHandler}
-																onReset={this.onReset}
-																validateColor={this.validateColor}
-																onChange={this.handleInputChange}
-															/>
-														);
-													})}
-												{isSingleBase && (
-													<div className="pt-3">
-														<span>
-															To edit more colors directly try switching to the
-														</span>
-														<Button
-															type="link"
-															value={false}
-															className="operator-button_underline"
-															onClick={() =>
-																this.setState({
-																	isSingleBase: false,
-																})
-															}
-														>
-															separated base color
-														</Button>
-														<span>configuration</span>
-													</div>
-												)}
-											</Collapse.Panel>
-										</Collapse>
-									) : (
-										<div className="pt-2">
-											{Object.keys(clusterObj).map((localColorKey) => {
-												const colorKey = `${clusterKey}_${localColorKey}`;
-												const isCalculated = this.isCalculated(colorKey);
-												const colorValue = isCalculated
-													? baseRatios[colorKey]
-													: theme[colorKey];
-
-												return (
-													<ColorInput
-														colorKey={colorKey}
-														isCalculated={isCalculated}
-														colorValue={colorValue}
-														pickerHandler={this.pickerHandler}
-														onReset={this.onReset}
-														validateColor={this.validateColor}
-														onChange={this.handleInputChange}
-													/>
-												);
-											})}
+												</div>
+											)}
 										</div>
-									)}
-								</div>
-							);
-						})}
-				</div>
+									);
+								})}
+						</div>
+					</Fragment>
+				)}
 				<div className="pt-3">
-					<Button
-						block
-						type="primary"
-						size="large"
-						className="operator-controls__save-button"
-						disabled={this.isSaveDisabled()}
-						onClick={this.addTheme}
-					>
-						Save
-					</Button>
+					{upstreamSelected ? (
+						<Button
+							block
+							type="primary"
+							size="large"
+							className="operator-controls__save-button"
+							disabled={this.isSaveDisabled()}
+							onClick={this.addTheme}
+						>
+							Save
+						</Button>
+					) : (
+						<Button
+							block
+							type="primary"
+							size="large"
+							className="operator-controls__save-button"
+							disabled={!upstreamTheme}
+							onClick={this.next}
+						>
+							Next
+						</Button>
+					)}
 				</div>
 			</Modal>
 		);
