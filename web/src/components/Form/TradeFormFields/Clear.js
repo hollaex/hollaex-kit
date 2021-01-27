27425142afb8f46616b761af296f7d7fe08f5bca
Amir Hossein Salar <ah.salar@gmail.com>
2021-01-27 15:26:36 +0330

    changes for order entry and trade-block styles

--- web/src/components/Form/TradeFormFields/Clear.js
+++ web/src/components/Form/TradeFormFields/Clear.js
@@ -5,7 +5,7 @@ const Clear = (props) => {
 	const { onClick } = props;
 
 	return (
-		<div className="d-flex justify-content-end">
+		<div className="d-flex justify-content-end mb-0">
 			<span className="pointer text-uppercase blue-link" onClick={onClick}>
 				{STRINGS['CLEAR']}
 			</span>
