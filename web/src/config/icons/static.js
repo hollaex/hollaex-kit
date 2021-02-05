70ffeeecc91d1b89509d60468caebcb9e233fe59
Amir Hossein Salar <ah.salar@gmail.com>
2021-02-05 15:11:03 +0330

    add new static icons

--- web/src/config/icons/static.js
+++ web/src/config/icons/static.js
@@ -67,6 +67,9 @@ const icons = {
 	CANCEL_CROSS_ACTIVE: '/assets/images/cancel-cross-active.svg',
 	VERIFICATION_ICON: '/assets/images/verification-green-tick.svg',
 	CHAT_FEATURE_ICON: '/assets/images/chat-feature-icon.svg',
+	USER_EMAIL: '/assets/images/user-email.svg',
+	USER_EMAIL_VERIFIED: '/assets/images/user-email-verified.svg',
+	USER_EMAIL_UNVERIFIED: '/assets/images/user-email-unverified.svg',
 };
 
 export default icons;
