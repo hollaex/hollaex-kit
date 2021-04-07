e68835b11cb81ed645c790a810c538b7da90e9f1
brandonkimmmm <bkim2490@gmail.com>
2021-04-07 17:00:17 +0900

    put mint/burn update

--- tools/wallet.js
+++ tools/wallet.js
@@ -559,7 +559,7 @@ const getExchangeWithdrawals = (
 	});
 };
 
-const mintAssetByKitId = (kitId, currency, amount, description, transaction_id) => {
+const mintAssetByKitId = (kitId, currency, amount, description, transactionId, status) => {
 	return getUserByKitId(kitId)
 		.then((user) => {
 			if (!user) {
@@ -567,15 +567,27 @@ const mintAssetByKitId = (kitId, currency, amount, description, transaction_id) 
 			} else if (!user.network_id) {
 				throw new Error(USER_NOT_REGISTERED_ON_NETWORK);
 			}
-			return getNodeLib().mintAsset(user.network_id, currency, amount, { description, transaction_id });
+			return getNodeLib().mintAsset(user.network_id, currency, amount, { description, transactionId, status });
 		});
 };
 
-const mintAssetByNetworkId = (networkId, currency, amount, description, transaction_id) => {
-	return getNodeLib().mintAsset(networkId, currency, amount, { description, transaction_id });
+const mintAssetByNetworkId = (networkId, currency, amount, description, transactionId, status) => {
+	return getNodeLib().mintAsset(networkId, currency, amount, { description, transactionId, status });
 };
 
-const burnAssetByKitId = (kitId, currency, amount, description, transaction_id) => {
+const updatePendingMint = (
+	transactionId,
+	opts = {
+		status: null,
+		dismissed: null,
+		rejected: null,
+		updatedTransactionId: null
+	}
+) => {
+	return getNodeLib().updatePendingMint(transactionId, opts);
+};
+
+const burnAssetByKitId = (kitId, currency, amount, description, transactionId, status) => {
 	return getUserByKitId(kitId)
 		.then((user) => {
 			if (!user) {
@@ -583,12 +595,24 @@ const burnAssetByKitId = (kitId, currency, amount, description, transaction_id) 
 			} else if (!user.network_id) {
 				throw new Error(USER_NOT_REGISTERED_ON_NETWORK);
 			}
-			return getNodeLib().burnAsset(user.network_id, currency, amount, { description, transaction_id });
+			return getNodeLib().burnAsset(user.network_id, currency, amount, { description, transactionId, status });
 		});
 };
 
-const burnAssetByNetworkId = (networkId, currency, amount, description, transaction_id) => {
-	return getNodeLib().burnAsset(networkId, currency, amount, { description, transaction_id });
+const burnAssetByNetworkId = (networkId, currency, amount, description, transactionId, status) => {
+	return getNodeLib().burnAsset(networkId, currency, amount, { description, transactionId, status });
+};
+
+const updatePendingBurn = (
+	transactionId,
+	opts = {
+		status: null,
+		dismissed: null,
+		rejected: null,
+		updatedTransactionId: null
+	}
+) => {
+	return getNodeLib().updatePendingBurn(transactionId, opts);
 };
 
 module.exports = {
@@ -611,5 +635,7 @@ module.exports = {
 	mintAssetByNetworkId,
 	burnAssetByKitId,
 	burnAssetByNetworkId,
-	getKitBalance
+	getKitBalance,
+	updatePendingMint,
+	updatePendingBurn
 };
