52e506a33c4fc60d4c158e6a82563d92fc9880c8
ram <ram@bitholla.com>
2021-05-03 21:20:23 +0530

    Changes for user discount

--- web/src/containers/Admin/User/AboutData.js
+++ web/src/containers/Admin/User/AboutData.js
@@ -1,14 +1,15 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { ReactSVG } from 'react-svg';
-import { Button, Modal, Select } from 'antd';
+import { Button, Input, message, Modal, Select } from 'antd';
 import { Link } from 'react-router';
 import {
 	ExclamationCircleFilled,
 	CaretUpFilled,
 	CaretDownFilled,
-	UserOutlined,
+	UserOutlined
 } from '@ant-design/icons';
 import { SubmissionError } from 'redux-form';
+import classnames from 'classnames';
 
 import Notes from './Notes';
 import UserData from './UserData';
@@ -17,7 +18,7 @@ import Audits from '../Audits';
 import Logins from '../Logins';
 import Verification from '../Verification';
 import DataDisplay, { renderRowInformation } from '../Verification/DataDisplay';
-import { performVerificationLevelUpdate } from './actions';
+import { performVerificationLevelUpdate, updateDiscount } from './actions';
 import {
 	validateRequired,
 	validateRange,
@@ -37,7 +38,12 @@ const RenderModalContent = ({
 	refreshData,
 	allIcons,
 	userTiers,
+	handleApply,
+	handleDiscount
 }) => {
+	const [discount, setDiscount] = useState(userData.discount);
+	const [isDisable, setBtnDisable] = useState(false);
+
 	const onSubmit = (refreshData) => (values) => {
 		// redux form set numbers as string, se we have to parse them
 		const postValues = {
@@ -66,6 +72,35 @@ const RenderModalContent = ({
 				</div>
 			</Select.Option>
 		));
+	
+	const handleApplyConfirm = (user, discount) => {
+		const body = {
+			discount
+		}
+		return updateDiscount(user, body)
+			.then((data) => {
+				handleDiscount(true);
+				handleClose();
+				message.success('Discount added successfully');
+			})
+			.catch((err) => {
+				const errMsg = err.data ? err.data.message : err.message;
+				message.error(errMsg);
+			});
+	}
+
+	const handleInput = (e) => {
+		if (e.target.value) {
+			if (parseFloat(e.target.value) >= 0 && parseFloat(e.target.value) <= 100) {
+				setDiscount(parseFloat(e.target.value));
+				setBtnDisable(false);
+			} else {
+				setBtnDisable(true);
+			}
+		} else {
+			setBtnDisable(true);
+		}
+	}
 	switch (modalKey) {
 		case 'notes':
 			return (
@@ -126,6 +161,64 @@ const RenderModalContent = ({
 					/>
 				</div>
 			);
+		case 'fee-discount':
+			return (
+				<div className="user-discount-wrapper">
+					<div className="title">Trading fee discount</div>
+					<div>
+						Reduce this users trading fees by applying inputting the specific discount below. Note, the reduction is applied on top of the current user trading fee level.
+					</div>
+					<div className="mt-4 mb-3">
+						<div>Fee discount</div>
+						<Input
+							type="number"
+							defaultValue={discount}
+							suffix="%"
+							min="0"
+							onChange={handleInput}
+						/>
+					</div>
+					<div>When applying a fee reduction an email notification will be sent to the user.</div>
+					<div>
+						<Button
+							type="primary"
+							className="green-btn w-100 mt-4"
+							onClick={() => handleApply('fee-discount-confirm')}
+							disabled={isDisable}
+						>
+							Next
+						</Button>
+					</div>
+				</div>
+			);
+		case 'fee-discount-confirm':
+			return (
+				<div className="user-discount-wrapper">
+					<div className="title">Check and confirm</div>
+					<div>Please check that the details below are correct.</div>
+					<div className="box-content">
+						<div><b>Current user level:</b> {userData.verification_level}</div>
+						<div className="mt-2"><b>Fee discount:</b> {discount}</div>
+					</div>
+					<div>When applying a fee reduction an email notification will be sent to the user.</div>
+					<div className="button-wrapper">
+						<Button
+							type="primary"
+							className="green-btn"
+							onClick={() => handleApply('fee-discount')}
+						>
+							Back
+						</Button>
+						<Button
+							type="primary"
+							className="green-btn"
+							onClick={() => handleApplyConfirm(userData, discount)}
+						>
+							Apply
+						</Button>
+					</div>
+				</div>
+			);
 		default:
 			return <div></div>;
 	}
@@ -148,6 +241,8 @@ const AboutData = ({
 	const [isEdit, setEdit] = useState(false);
 	const [showRemaining, setShow] = useState(false);
 	const [modalKey, setModalKey] = useState('');
+	const [isApply, setApply] = useState(false);
+	const [isDiscount, setDiscountApply] = useState(false);
 	const userDocs = {
 		front: userImages.front ? userImages.front : '',
 		back: userImages.back ? userImages.back : '',
@@ -155,6 +250,13 @@ const AboutData = ({
 			? userImages.proof_of_residency
 			: '',
 	};
+	useEffect(() => {
+		if (userData.discount !== 0) {
+			setDiscountApply(true);
+		} else {
+			setDiscountApply(false);
+		}
+	}, [userData])
 	const handleNotesRemove = () => {
 		Modal.confirm({
 			icon: <ExclamationCircleFilled />,
@@ -171,7 +273,23 @@ const AboutData = ({
 	const handleClose = () => {
 		setEdit(false);
 		setModalKey('');
+		setApply(false);
 	};
+
+	const handleApply = (key, isApply = false) => {
+		if (isApply) {
+			setModalKey(key);
+			setApply(true);
+		} else {
+			setModalKey(key);
+		}
+	}
+
+	const handleDiscount = (value) => {
+		if (value) {
+			setDiscountApply(value);
+		}
+	}
 	const {
 		email,
 		full_name,
@@ -257,6 +375,30 @@ const AboutData = ({
 		<div>
 			<div className="d-flex justify-content-end header-section mb-5">
 				<div className="d-flex align-items-center my-5">
+					<div className="about-info d-flex align-items-center justify-content-center">
+						<Fragment>
+							<div className="about-info-content">
+								<div className="info-txt">Trading Fee discount</div>
+								<div className="info-link" onClick={() => handleApply('fee-discount', true)}>
+									{isDiscount
+										? 'Adjust'
+										: 'Apply'
+									}
+								</div>
+							</div>
+							<div className={classnames("percentage-txt ml-2", { "percentage-txt-active": isDiscount})}>%</div>
+							{isDiscount
+								?
+									<div className={'about-icon-active'}>
+										<ReactSVG
+											src={STATIC_ICONS.VERIFICATION_ICON}
+											className={'verification-icon'}
+										/>
+									</div>
+								: null
+							}
+						</Fragment>
+					</div>
 					<div className="about-info d-flex align-items-center justify-content-center">
 						{userData.email_verified ? (
 							<Fragment>
@@ -530,7 +672,7 @@ const AboutData = ({
 					<div className="about-title">Login</div>
 					<Logins userId={userData.id} />
 				</div>
-				<Modal visible={isEdit} footer={null} onCancel={handleClose}>
+				<Modal visible={isEdit || isApply} footer={null} onCancel={handleClose}>
 					<RenderModalContent
 						modalKey={modalKey}
 						userData={userData}
@@ -540,6 +682,8 @@ const AboutData = ({
 						refreshData={refreshData}
 						allIcons={allIcons}
 						userTiers={userTiers}
+						handleApply={handleApply}
+						handleDiscount={handleDiscount}
 					/>
 				</Modal>
 			</div>
