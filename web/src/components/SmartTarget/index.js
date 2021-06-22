b28d6344ce0116901887e9135d3f65d5838c4060
Amir Hossein Salar <ah.salar@gmail.com>
2021-06-22 11:29:58 +0430

    change for the web_view injection logic

--- web/src/components/SmartTarget/index.js
+++ web/src/components/SmartTarget/index.js
@@ -1,76 +1,22 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { RemoteComponent } from 'RemoteComponent';
-import { injectPlugin } from 'utils/plugin';
 import STRINGS from 'config/localizedStrings';
 import withConfig from 'components/ConfigProvider/withConfig';
 
-class SmartTarget extends Component {
-	componentDidMount() {
-		const { webViews, targets, id } = this.props;
-		if (targets.includes(id)) {
-			const components = [];
-			webViews[id].forEach(
-				({ src, props: propertiesData, all_props = false }) => {
-					let props = {};
-					if (all_props) {
-						props = { ...this.props };
-					} else {
-						propertiesData.forEach(({ key, store_key }) => {
-							props[key] = this.props[store_key];
-						});
-					}
+const SmartTarget = (props) => {
+	const { targets, id, children, webViews } = props;
 
-					const CustomComponent = (props) => (
-						<RemoteComponent url={src} {...props} />
-					);
-
-					components.push(<CustomComponent strings={STRINGS} {...props} />);
-				}
-			);
-
-			injectPlugin(components, id);
-		}
-	}
-
-	componentDidUpdate(prevProps) {
-		const { webViews, targets, id } = this.props;
-
-		if (!prevProps.targets.includes(id) && targets.includes(id)) {
-			const components = [];
-			webViews[id].forEach(
-				({ src, props: propertiesData, all_props = false }) => {
-					let props = {};
-					if (all_props) {
-						props = { ...this.props };
-					} else {
-						propertiesData.forEach(({ key, store_key }) => {
-							props[key] = this.props[store_key];
-						});
-					}
-
-					const CustomComponent = (props) => (
-						<RemoteComponent url={src} {...props} />
-					);
-
-					components.push(<CustomComponent strings={STRINGS} {...props} />);
-				}
-			);
-
-			injectPlugin(components, id);
-		}
-	}
-
-	render() {
-		const { targets, id, children } = this.props;
-
-		return targets.includes(id) ? (
-			<div id={id} />
-		) : (
-			<Fragment>{children}</Fragment>
-		);
-	}
-}
+	return targets.includes(id) ? (
+		<Fragment>
+			{webViews[id].map(({ src }) => (
+				<RemoteComponent url={src} strings={STRINGS} {...props} />
+			))}
+		</Fragment>
+	) : (
+		<Fragment>{children}</Fragment>
+	);
+};
 
 const mapStateToProps = (store) => ({
 	targets: store.app.targets,
