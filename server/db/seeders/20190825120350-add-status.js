37a62429c5dd98b35bf888cd1575379f83c22e2a
brandonkimmmm <bkim2490@gmail.com>
2021-08-18 16:06:47 +0900

    status seeder checks if user info is passed

--- server/db/seeders/20190825120350-add-status.js
+++ server/db/seeders/20190825120350-add-status.js
@@ -24,7 +24,10 @@ const {
 	NATIVE_CURRENCY,
 	API_KEY,
 	API_SECRET,
-	KIT_VERSION
+	KIT_VERSION,
+	ADMIN_NETWORK_ID,
+	ADMIN_EMAIL,
+	ADMIN_PASSWORD
 } = process.env;
 
 const {
@@ -32,6 +35,8 @@ const {
 	HOLLAEX_NETWORK_BASE_URL,
 	HOLLAEX_NETWORK_PATH_ACTIVATE
 } = require('../../constants');
+
+const { generateUserObject } = require('../seedsHelper');
 
 const checkActivation = (activation_code) => {
 	const body = {
@@ -53,7 +58,21 @@ const TABLE = 'Statuses';
 module.exports = {
 	up: (queryInterface) => {
 		return checkActivation(ACTIVATION_CODE)
-			.then((exchange) => {
+			.then(async (exchange) => {
+				let user = null;
+
+				if (ADMIN_EMAIL && ADMIN_PASSWORD && ADMIN_NETWORK_ID) {
+					user = generateUserObject(
+						1,
+						ADMIN_EMAIL,
+						ADMIN_PASSWORD,
+						parseInt(ADMIN_NETWORK_ID),
+						true
+					);
+
+					await queryInterface.bulkInsert('Users', [user], {});
+				}
+
 				const status = [{
 					kit: JSON.stringify({
 						api_name: API_NAME || '',
@@ -122,12 +141,12 @@ module.exports = {
 						}
 					}),
 					activation_code: ACTIVATION_CODE,
-					initialized: false,
+					initialized: user ? true : false,
 					api_key: API_KEY,
 					api_secret: API_SECRET,
 					kit_version: KIT_VERSION
 				}];
-				queryInterface.bulkInsert(TABLE, status, {});
+				return queryInterface.bulkInsert(TABLE, status, {});
 			});
 	},
 	down: (queryInterface) => {
--- server/db/seedsHelper.js
+++ server/db/seedsHelper.js
@@ -6,19 +6,24 @@ const {
 } = require('../utils/security');
 
 const generateUserObject = ({
+	id,
 	email,
 	password,
+	network_id,
 	is_admin,
 	is_supervisor,
 	is_support,
 	is_kyc,
+	is_communicator,
 	...rest
 }) => {
 	const now = new Date();
 
 	return {
+		id,
 		email,
 		password: generateHashSync(password),
+		network_id,
 		full_name: '',
 		gender: false,
 		nationality: '',
@@ -28,6 +33,7 @@ const generateUserObject = ({
 		is_supervisor: is_supervisor || false,
 		is_support: is_support || false,
 		is_kyc: is_kyc || false,
+		is_communicator: is_communicator || false,
 		username: email.toLowerCase().substr(0, email.indexOf('@')),
 		affiliation_code: generateAffiliationCode(),
 		created_at: now,
