6d1d3a691fb21b727cd9b96452c6793526695c32
brandonkimmmm <bkim2490@gmail.com>
2021-11-18 12:51:31 +0900

    delete, post plugin

--- server/plugins/controllers.js
+++ server/plugins/controllers.js
@@ -5,6 +5,8 @@ const { validationResult } = require('express-validator');
 const lodash = require('lodash');
 const sequelize = require('sequelize');
 const { loggerPlugin } = require('../config/logger');
+const { omit, pick, isUndefined, isPlainObject, cloneDeep, isString } = require('lodash');
+const uglifyEs = require('uglify-es');
 
 const getPlugins = async (req, res) => {
 	const errors = validationResult(req);
@@ -13,18 +15,18 @@ const getPlugins = async (req, res) => {
 		return res.status(400).json({ errors: errors.array() });
 	}
 
+	const { name, search } = req.query;
+
+	loggerPlugin.verbose(
+		req.uuid,
+		'plugins/controllers/getPlugins',
+		'name:',
+		name,
+		'search:',
+		search
+	);
+
 	try {
-		const { name, search } = req.query;
-
-		loggerPlugin.verbose(
-			req.uuid,
-			'plugins/controllers/getPlugins',
-			'name:',
-			name,
-			'search:',
-			search
-		);
-
 		const options = {
 			raw: true,
 			attributes: {
@@ -73,6 +75,220 @@ const getPlugins = async (req, res) => {
 	}
 };
 
+const deletePlugin = async (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+
+	loggerPlugin.verbose(
+		req.uuid,
+		'plugins/controllers/deletePlugin auth',
+		req.auth.sub
+	);
+
+	const { name } = req.query;
+
+	loggerPlugin.info(
+		req.uuid,
+		'plugins/controllers/deletePlugin name:',
+		name
+	);
+
+	try {
+		const plugin = await Plugin.findOne({
+			where: { name }
+		});
+
+		if (!plugin) {
+			throw new Error('Plugin not found');
+		}
+
+		const restartAfterDelete = plugin.enabled && plugin.script;
+
+		await plugin.destory();
+
+		loggerPlugin.verbose(
+			req.uuid,
+			'plugins/controllers/deletePlugin',
+			'plugin deleted',
+			name
+		);
+
+		res.json({ message: 'Success' });
+
+		if (restartAfterDelete) {
+			loggerPlugin.verbose(
+				req.uuid,
+				'plugins/controllers/deletePlugin',
+				'restarting plugin process'
+			);
+
+			process.exit();
+		}
+	} catch (err) {
+		loggerPlugin.error(
+			req.uuid,
+			'plugins/controllers/deletePlugin err',
+			err.message
+		);
+
+		return res.status(err.status || 400).json({ message: err.message });
+	}
+};
+
+const postPlugin = async (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+
+	loggerPlugin.verbose(
+		req.uuid,
+		'plugins/controllers/postPlugin',
+		req.auth.sub
+	);
+
+	const { name, version, author, enabled } = req.body;
+
+	loggerPlugin.info(
+		req.uuid,
+		'plugins/controllers/postPlugin',
+		name,
+		'version:',
+		version,
+		'author:',
+		author
+	);
+
+	const configValues = pick(req.body, [
+		'script',
+		'description',
+		'icon',
+		'bio',
+		'documentation',
+		'web_view',
+		'admin_view',
+		'url',
+		'logo',
+		'prescript',
+		'postscript',
+		'meta',
+		'public_meta',
+		'type'
+	]);
+
+	try {
+		if (configValues.type) {
+			const sameTypePlugin = await Plugin.findOne({
+				where: { type: configValues.type },
+				raw: true,
+				attributes: ['id', 'name', 'type']
+			});
+
+			if (sameTypePlugin) {
+				throw new Error(`${name} cannot be ran in parallel with an installed plugin (${sameTypePlugin.name}). Uninstall the plugin ${sameTypePlugin.name} before installing this plugin.`);
+			}
+		}
+
+		const sameNamePlugin = await Plugin.findOne({
+			where: { name },
+			raw: true,
+			attributes: ['id', 'name']
+		});
+
+		if (sameNamePlugin) {
+			throw new Error(`Plugin ${name} is already installed`);
+		}
+
+		const newPlugin = {
+			name,
+			version,
+			author,
+			enabled
+		};
+
+		for (const field in configValues) {
+			const value = configValues[field];
+
+			switch (field) {
+				case 'script':
+					if (value) {
+						const minifiedScript = uglifyEs.minify(value);
+
+						if (minifiedScript.error) {
+							throw new Error(`Error while minifying script: ${minifiedScript.error.message}`);
+						}
+
+						newPlugin[field] =  minifiedScript.code;
+					}
+					break;
+				case 'description':
+				case 'bio':
+				case 'documentation':
+				case 'icon':
+				case 'url':
+				case 'logo':
+				case 'type':
+					if (isString(value)) {
+						newPlugin[field] = value;
+					}
+					break;
+				case 'web_view':
+				case 'admin_view':
+					if (!isUndefined(value)) {
+						newPlugin[field] = value;
+					}
+					break;
+				case 'prescript':
+				case 'postscript':
+				case 'meta':
+				case 'public_meta':
+					if (isPlainObject(value)) {
+						newPlugin[field] = value;
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		const plugin = await Plugin.create(newPlugin);
+		const formattedPlugin = cloneDeep(plugin.dataValues);
+
+		loggerPlugin.info(
+			req.uuid,
+			'plugins/controllers/postPlugin plugin installed',
+			name
+		);
+
+		formattedPlugin.enabled_admin_view = !!formattedPlugin.admin_view;
+
+		res.json(omit(formattedPlugin, [
+			'id',
+			'meta',
+			'admin_view',
+			'script',
+			'prescript',
+			'postscript'
+		]));
+
+		if (plugin.enabled && plugin.script) {
+			process.exit();
+		}
+	} catch (err) {
+		loggerPlugin.error(
+			req.uuid,
+			'plugins/controllers/postPlugin',
+			err.message
+		);
+
+		return res.status(err.status || 400).json({ message: err.message });
+	}
+};
+
 module.exports = {
-	getPlugins
+	getPlugins,
+	deletePlugin,
+	postPlugin
 };
--- server/plugins/routes.js
+++ server/plugins/routes.js
@@ -2,34 +2,110 @@
 
 const express = require('express');
 const router = express.Router();
-const { getPlugins } = require('./controllers');
-const { checkSchema } = require('express-validator');
+const { getPlugins, deletePlugin, postPlugin, putPlugin } = require('./controllers');
+const { checkSchema, query, body } = require('express-validator');
+const toolsLib = require('../utils/toolsLib');
+const lodash = require('lodash');
 
 router.get(
 	'/',
-	checkSchema({
-		name: {
-			in: ['query'],
-			errorMessage: 'must be a string',
-			isString: true,
-			isLength: {
-				errorMessage: 'must be minimum length of 1',
-				options: { min: 1 }
-			},
-			optional: true
-		},
-		search: {
-			in: ['query'],
-			errorMessage: 'must be a string',
-			isString: true,
-			isLength: {
-				errorMessage: 'must be minimum length of 1',
-				options: { min: 1 }
-			},
-			optional: true
-		}
-	}),
+	[
+		query('name').isString().notEmpty().optional(),
+		query('search').isString().notEmpty().optional(),
+	],
 	getPlugins
+);
+
+router.delete(
+	'/',
+	[
+		toolsLib.security.verifyBearerTokenExpressMiddleware(['admin']),
+		query('name').isString().notEmpty().trim().toLowerCase()
+	],
+	deletePlugin
+);
+
+router.post(
+	'/',
+	[
+		toolsLib.security.verifyBearerTokenExpressMiddleware(['admin']),
+		body('name').isString().notEmpty().trim().toLowerCase(),
+		body('version').isInt({ min: 1 }),
+		body('author').isString(),
+		body('enabled').isBoolean(),
+		body('script').isString().notEmpty().optional(),
+		body('description').isString().optional(),
+		body('bio').isString().optional(),
+		body('documentation').isString().optional(),
+		body('icon').isString().optional(),
+		body('url').isString().optional(),
+		body('logo').isString().optional(),
+		body('admin_view').isString().optional(),
+		body('web_view').isArray().optional(),
+		body('type').isString().optional(),
+		checkSchema({
+			prescript: {
+				in: ['body'],
+				custom: {
+					options: (value) => {
+						if (!lodash.isPlainObject(value)) {
+							return false;
+						}
+						if (value.install && lodash.isArray(value.install)) {
+							for (let lib of value.install) {
+								if (!lodash.isString(lib)) {
+									return false;
+								}
+							}
+						}
+						if (value.run && !lodash.isString(value.run)) {
+							return false;
+						}
+						return true;
+					},
+					errorMessage: 'must be an object. install value must be an array of strings. run value must be a string'
+				},
+				optional: { options: { nullable: true } }
+			},
+			postscript: {
+				in: ['body'],
+				custom: {
+					options: (value) => {
+						if (!lodash.isPlainObject(value)) {
+							return false;
+						}
+						if (value.run && !lodash.isString(value.run)) {
+							return false;
+						}
+						return true;
+					},
+					errorMessage: 'must be an object. run value must be a string'
+				},
+				optional: { options: { nullable: true } }
+			},
+			meta: {
+				in: ['body'],
+				custom: {
+					options: (value) => {
+						return lodash.isPlainObject(value);
+					},
+					errorMessage: 'must be an object'
+				},
+				optional: { options: { nullable: true } }
+			},
+			public_meta: {
+				in: ['body'],
+				custom: {
+					options: (value) => {
+						return lodash.isPlainObject(value);
+					},
+					errorMessage: 'must be an object'
+				},
+				optional: { options: { nullable: true } }
+			}
+		})
+	],
+	postPlugin
 );
 
 module.exports = router;
