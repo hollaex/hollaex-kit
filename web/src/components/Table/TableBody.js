66321a76c75c4437e3b205965292d5941caf80d9
Amir Hossein Salar <ahsalar1991@gmail.com>
2022-02-03 03:53:58 +0330

    fix for increasing hooks issue

--- web/src/components/Table/TableBody.js
+++ web/src/components/Table/TableBody.js
@@ -1,48 +1,69 @@
-import React, { useState, Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import classnames from 'classnames';
 
 const RENDER_CELL = (row, key, cellIndex) => (
 	<td key={cellIndex}>{row[key]}</td>
 );
 
-const renderRow = (headers = [], cancelDelayData, expandable = {}) => (
-	row,
-	rowIndex
-) => {
-	const { rowExpandable, expandedRowRender } = expandable;
-	const [isExpanded, setIsExpanded] = useState(false);
-	const isRemoveData = cancelDelayData.filter((data) => data === row.id);
-	const isExpandable = rowExpandable(row);
+class TableRow extends Component {
+	state = {
+		isExpanded: false,
+	};
 
-	const onExpand = () => {
+	setIsExpanded = () => {
+		this.setState((prevState) => ({
+			...prevState,
+			isExpanded: !prevState.isExpanded,
+		}));
+	};
+
+	onExpand = () => {
+		const {
+			expandable: { rowExpandable },
+			row,
+		} = this.props;
+		const isExpandable = rowExpandable(row);
 		if (isExpandable) {
-			setIsExpanded(!isExpanded);
+			this.setIsExpanded();
 		}
 	};
 
-	return (
-		<Fragment>
-			<tr
-				className={classnames('table_body-row', {
-					'cancel-row-color': !!isRemoveData.length,
-				})}
-				key={`row_${rowIndex}`}
-				onClick={onExpand}
-			>
-				{headers.map(({ key, renderCell = RENDER_CELL }, cellIndex) =>
-					renderCell(row, key, cellIndex, isExpandable, isExpanded)
-				)}
-			</tr>
-			{isExpandable && isExpanded && (
-				<tr key={`expandable_row_${rowIndex}`}>
-					<td colSpan={6}>
-						<div>{expandedRowRender(row)}</div>
-					</td>
+	render() {
+		const {
+			expandable: { rowExpandable, expandedRowRender },
+			cancelDelayData,
+			row,
+			rowIndex,
+			headers,
+		} = this.props;
+		const { isExpanded } = this.state;
+		const isRemoveData = cancelDelayData.filter((data) => data === row.id);
+		const isExpandable = rowExpandable(row);
+
+		return (
+			<Fragment>
+				<tr
+					className={classnames('table_body-row', {
+						'cancel-row-color': !!isRemoveData.length,
+					})}
+					key={`row_${rowIndex}`}
+					onClick={this.onExpand}
+				>
+					{headers.map(({ key, renderCell = RENDER_CELL }, cellIndex) =>
+						renderCell(row, key, cellIndex, isExpandable, isExpanded)
+					)}
 				</tr>
-			)}
-		</Fragment>
-	);
-};
+				{isExpandable && isExpanded && (
+					<tr key={`expandable_row_${rowIndex}`}>
+						<td colSpan={6}>
+							<div>{expandedRowRender(row)}</div>
+						</td>
+					</tr>
+				)}
+			</Fragment>
+		);
+	}
+}
 
 const TableBody = ({
 	withIcon,
@@ -56,7 +77,15 @@ const TableBody = ({
 			with_icon: withIcon || false,
 		})}
 	>
-		{data.map(renderRow(headers, cancelDelayData, expandable))}
+		{data.map((row, rowIndex) => (
+			<TableRow
+				headers={headers}
+				cancelDelayData={cancelDelayData}
+				expandable={expandable}
+				row={row}
+				rowIndex={rowIndex}
+			/>
+		))}
 	</tbody>
 );
 
