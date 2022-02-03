ba87639c73d6c3b91dfa56573d355f692458ee6f
Amir Hossein Salar <ahsalar1991@gmail.com>
2022-02-03 03:46:30 +0330

    expandable table

--- web/src/components/Table/TableBody.js
+++ web/src/components/Table/TableBody.js
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import classnames from 'classnames';
 
 const RENDER_CELL = (row, key, cellIndex) => (
 	<td key={cellIndex}>{row[key]}</td>
 );
 
-const renderRow = (headers = [], cancelDelayData) => (row, rowIndex) => {
+const renderRow = (headers = [], cancelDelayData, expandable = {}) => (
+	row,
+	rowIndex
+) => {
+	const { rowExpandable, expandedRowRender } = expandable;
+	const [isExpanded, setIsExpanded] = useState(false);
 	const isRemoveData = cancelDelayData.filter((data) => data === row.id);
+	const isExpandable = rowExpandable(row);
+
+	const onExpand = () => {
+		if (isExpandable) {
+			setIsExpanded(!isExpanded);
+		}
+	};
+
 	return (
-		<tr
-			className={classnames('table_body-row', {
-				'cancel-row-color': !!isRemoveData.length,
-			})}
-			key={`row_${rowIndex}`}
-		>
-			{headers.map(({ key, renderCell = RENDER_CELL }, cellIndex) =>
-				renderCell(row, key, cellIndex)
+		<Fragment>
+			<tr
+				className={classnames('table_body-row', {
+					'cancel-row-color': !!isRemoveData.length,
+				})}
+				key={`row_${rowIndex}`}
+				onClick={onExpand}
+			>
+				{headers.map(({ key, renderCell = RENDER_CELL }, cellIndex) =>
+					renderCell(row, key, cellIndex, isExpandable, isExpanded)
+				)}
+			</tr>
+			{isExpandable && isExpanded && (
+				<tr key={`expandable_row_${rowIndex}`}>
+					<td colSpan={6}>
+						<div>{expandedRowRender(row)}</div>
+					</td>
+				</tr>
 			)}
-		</tr>
+		</Fragment>
 	);
 };
 
-const TableBody = ({ withIcon, data, headers, cancelDelayData }) => (
+const TableBody = ({
+	withIcon,
+	data,
+	headers,
+	cancelDelayData,
+	expandable,
+}) => (
 	<tbody
 		className={classnames('table_body-wrapper', {
 			with_icon: withIcon || false,
 		})}
 	>
-		{data.map(renderRow(headers, cancelDelayData))}
+		{data.map(renderRow(headers, cancelDelayData, expandable))}
 	</tbody>
 );
 
