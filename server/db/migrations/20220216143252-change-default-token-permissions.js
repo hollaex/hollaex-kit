e33db2289540504cef6a472550fc4d363435beaa
Ali Beikverdi <ali@bitholla.com>
2022-02-24 13:52:39 +0900

    fixed default token permission migration bug

--- server/db/migrations/20220216143252-change-default-token-permissions.js
+++ server/db/migrations/20220216143252-change-default-token-permissions.js
@@ -4,11 +4,19 @@ const TABLE = 'Tokens';
 
 module.exports = {
 	async up (queryInterface, Sequelize) {
-		await queryInterface.changeColumn(TABLE, 'can_read', { defaultValue: true });
+		await queryInterface.changeColumn(TABLE, 'can_read', {
+			type: Sequelize.BOOLEAN,
+			allowNull: false,
+			defaultValue: true
+		});
 		await queryInterface.bulkUpdate(TABLE, { can_read: true, can_trade: true });
 	},
 
 	async down (queryInterface, Sequelize) {
-		await queryInterface.changeColumn(TABLE, 'can_read', { defaultValue: false });
+		await queryInterface.changeColumn(TABLE, 'can_read', {
+			type: Sequelize.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		});
 	}
 };
