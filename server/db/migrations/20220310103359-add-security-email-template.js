dc4a9ccd3849fa1a74794a3ee03c57c8738dd7ba
hoan@bitholla.com <hoan@bitholla.com>
2022-03-11 09:44:59 +0700

    update file migration for confirm email

--- server/db/migrations/20220310103359-add-security-email-template.js
+++ server/db/migrations/20220310103359-add-security-email-template.js
@@ -1,21 +1,26 @@
 'use strict';
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+const TABLE = 'Statuses';
+const COLUMN = 'email';
+const LANGUAGE_DEFAULT = 'en';
+const MAILTYPE = 'CONFIRM_EMAIL';
+
+module.exports = {
+	up: (queryInterface) => queryInterface.sequelize.query (
+		`UPDATE public."${TABLE}"SET
+		${COLUMN} = jsonb_set(${COLUMN}, '{${LANGUAGE_DEFAULT}, ${MAILTYPE}}', '${JSON.stringify(confirm_email_template)}')`
+	),
+	down: () => {
+		return new Promise((resolve) => {
+			resolve();
+		});
+	}
 };
+
+const confirm_email_template = {
+	"html": "<div><p><p> Dear ${name} </p></p><p>You have made sensitive request related to your accounts security. To verify the operation you would require to use to code below to authorize this operation.<br /><p style=\"font-size: 1.2rem; text-align: center;\">${code}</p>If you did not make this request, report this immidiately and proceed to change your crendetials as soon as possible.</p><p> Regards<br> ${api_name} team </p></div>",
+	"title": "Security Verification"
+}
+
+
