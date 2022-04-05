fd4e461105fc0182209069da63e9162173d8e2ee
ram <ram@bitholla.com>
2022-04-05 13:02:12 +0530

    Fix for the pairs loading issue

--- web/src/containers/Admin/Trades/PairsSummary.js
+++ web/src/containers/Admin/Trades/PairsSummary.js
@@ -458,11 +458,15 @@ class PairsSummary extends Component {
 						{configurePairs.map((item, key) => {
 							const pairs = item.split('-');
 							const pair_base_fullname =
-								allCoins.filter((data) => data.symbol === pairs[0])[0]
-									.fullname || '';
+								(allCoins.filter((data) => data.symbol === pairs[0])[0] &&
+									allCoins.filter((data) => data.symbol === pairs[0])[0]
+										.fullname) ||
+								'';
 							const pair2_fullname =
-								allCoins.filter((data) => data.symbol === pairs[1])[0]
-									.fullname || '';
+								(allCoins.filter((data) => data.symbol === pairs[1])[0] &&
+									allCoins.filter((data) => data.symbol === pairs[1])[0]
+										.fullname) ||
+								'';
 							return (
 								<div
 									className="box-content"
@@ -746,7 +750,7 @@ class PairsSummary extends Component {
 		const pendingPairs =
 			allPairs &&
 			allPairs.filter((data) => pairs.includes(data.name) && !data.verified);
-		if (this.state.pairs.length === 0 && pendingPairs.length === 0) {
+		if (!this.state.pairs || !this.state.pairs.length) {
 			return <Spin size="large" className="m-top" />;
 		}
 		return (
