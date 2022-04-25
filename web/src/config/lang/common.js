a6c7faa202fb7ef11de64b267826b3f6e8ce2361
Amir Hossein Salar <ah.salar@gmail.com>
2022-04-25 18:34:41 +0430

    extracting options from strings

--- web/src/config/lang/common.js
+++ web/src/config/lang/common.js
@@ -6,28 +6,4 @@ export default {
 	TERMS_OF_SERVICES: {
 		SERVICE_AGREEMENT: AGREEMENT,
 	},
-	SIDES: [
-		{ value: 'buy' }, // label: 'buy'
-		{ value: 'sell' }, // label: 'sell'
-	],
-	TYPES: [
-		{ value: 'market' }, // label: 'market'
-		{ value: 'limit' }, // label: 'limit'
-	],
-	DEFAULT_TOGGLE_OPTIONS: [
-		{ value: true }, // label: 'on'
-		{ value: false }, // label: 'off'
-	],
-	SETTINGS_ORDERPOPUP_OPTIONS: [
-		{ value: false }, // label: 'NO'
-		{ value: true }, // label: 'YES'
-	],
-	SETTINGS_THEME_OPTIONS: [
-		{ value: 'white' }, // label: 'White'
-		{ value: 'dark' }, // label: 'Dark'
-	],
-	MARKET_OPTIONS: [
-		{ value: 'List' }, // label: 'List'
-		{ value: 'Card' }, // label: 'Card'
-	],
 };
