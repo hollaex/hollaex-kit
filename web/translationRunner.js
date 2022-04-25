56a8999221ffd28e5402ef8e8edca3c827873e2a
Amir Hossein Salar <ah.salar@gmail.com>
2022-04-25 18:36:38 +0430

    translation runner save diff, validation and errors, and merge functionality

--- web/translationRunner.js
+++ web/translationRunner.js
@@ -1,7 +1,49 @@
 const fs = require('fs');
 const beautify = require('json-beautify');
-const mkdirp = require('mkdirp');
+const merge = require('lodash.merge');
 const isEmpty = require('lodash.isempty');
+
+const PLACEHOLDER_REGEX = /[^{}]+(?=})/g;
+const LETTER_REGEX = /[a-zA-Z]/g;
+const ERRORS = {
+	SIGN: '___ERROR:',
+	get PLACE_HOLDERS() {
+		return `${this.SIGN} INVALID PLACEHOLDER`;
+	},
+	get NOT_TRANSLATED() {
+		return `${this.SIGN} THE STRING IS NOT TRANSLATED`;
+	},
+};
+
+const removeError = (string = '') => string.split(ERRORS.SIGN)[0];
+
+const dropPlaceholders = (string = '') =>
+	string.replace(PLACEHOLDER_REGEX, '').replace(/{}/g, '').trim();
+
+const hasLetters = (string = '') => LETTER_REGEX.test(string);
+
+const equalityCheck = (base, target) =>
+	has('--ignore-equals')
+		? false
+		: base === target && hasLetters(dropPlaceholders(base));
+
+const validatePlaceholder = (string) => !isNaN(Number(string));
+
+const validatePlaceholders = (matches = []) => {
+	let isValid = true;
+	if (!matches) {
+		return isValid;
+	} else {
+		matches.forEach((match) => {
+			if (!validatePlaceholder(match)) {
+				isValid = false;
+			}
+		});
+		return isValid;
+	}
+};
+
+const pushError = (string, error) => `${string} ${error}`;
 
 const saveFile = (output, content) => {
 	fs.writeFileSync(output, beautify(content, null, 4, 100));
@@ -17,11 +59,32 @@ const readFile = (pathname) => {
 	}
 };
 
+const getPlaceHolders = (string = '') => string.match(PLACEHOLDER_REGEX);
+
+const countPlaceholders = (matches) => (matches ? matches.length : 0);
+
+const validateString = (base, target) => {
+	const benchmarkPlaceholdersCount = countPlaceholders(getPlaceHolders(base));
+	const placeholders = getPlaceHolders(target);
+
+	return (
+		countPlaceholders(placeholders) === benchmarkPlaceholdersCount &&
+		validatePlaceholders(placeholders)
+	);
+};
+
+const lang = process.env.REACT_APP_LANG;
 const baseLangDir = './src/config/lang/en.json';
-const targetLangDir = './src/config/lang/fa.json';
+const targetLangDir = `./src/config/lang/${lang}.json`;
+const diffDir = './diff.json';
 
 const isObject = (input) =>
 	typeof input === 'object' && !Array.isArray(input) && input !== null;
+
+const isString = (input) =>
+	typeof input === 'string' || input instanceof String;
+
+const has = (input) => process.argv.includes(input);
 
 const compare = (base = {}, target = {}) => {
 	const diff = {};
@@ -31,12 +94,20 @@ const compare = (base = {}, target = {}) => {
 		} else {
 			if (isObject(value)) {
 				if (isObject(target[key])) {
-					const comparissonResult = compare(value, target[key]);
-					if (!isEmpty(comparissonResult)) {
-						diff[key] = compare(value, target[key]);
+					const comparisonResult = compare(value, target[key]);
+					if (!isEmpty(comparisonResult)) {
+						diff[key] = comparisonResult;
 					}
 				} else {
 					diff[key] = value;
+				}
+			} else if (isString(value)) {
+				if (isString(target[key])) {
+					if (!validateString(value, target[key])) {
+						diff[key] = pushError(value, ERRORS.PLACE_HOLDERS);
+					} else if (equalityCheck(value, target[key])) {
+						diff[key] = pushError(value, ERRORS.NOT_TRANSLATED);
+					}
 				}
 			}
 		}
@@ -45,10 +116,31 @@ const compare = (base = {}, target = {}) => {
 	return diff;
 };
 
-if (!fs.existsSync('diff')) {
-	mkdirp.sync('diff');
+const manipulateObject = (object = {}, cb) => {
+	const result = {};
+	Object.entries(object).forEach(([key, value]) => {
+		if (isObject(value)) {
+			result[key] = manipulateObject(value, cb);
+		} else if (isString(value)) {
+			result[key] = cb(value);
+		}
+	});
+
+	return result;
+};
+
+if (!lang) {
+	console.error('No language given');
+	process.exit(1);
 }
 
-const diff = compare(readFile(baseLangDir), readFile(targetLangDir));
+if (has('--save-diff')) {
+	const diff = compare(readFile(baseLangDir), readFile(targetLangDir));
+	saveFile(`diff.json`, diff);
+}
 
-saveFile(`diff.json`, diff);
+if (has('--merge-diff')) {
+	const diff = manipulateObject(readFile(diffDir), removeError);
+	const content = merge({}, readFile(targetLangDir), diff);
+	saveFile(targetLangDir, content);
+}
