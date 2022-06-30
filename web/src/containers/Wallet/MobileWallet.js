28b83cde731d4b1187d4890fe71ee3130d42cad5
ram <ram@bitholla.com>
2022-06-30 23:04:14 +0530

    Fix for the landing page card section

--- web/src/containers/Wallet/MobileWallet.js
+++ web/src/containers/Wallet/MobileWallet.js
@@ -3,30 +3,40 @@ import classnames from 'classnames';
 import { Accordion } from '../../components';
 import CurrencySlider from './components/CurrencySlider';
 
-const MobileWallet = ({ sections, balance, prices, navigate, coins }) => (
-	<div
-		className={classnames(
-			'flex-column',
-			'd-flex',
-			'justify-content-between',
-			'f-1',
-			'apply_rtl',
-			'h-100',
-			'w-100'
-		)}
-	>
-		<div className="d-flex f-05">
-			<CurrencySlider
-				balance={balance}
-				prices={prices}
-				navigate={navigate}
-				coins={coins}
-			/>
+const MobileWallet = ({
+	sections,
+	balance,
+	prices,
+	navigate,
+	coins,
+	searchResult,
+}) => {
+	return (
+		<div
+			className={classnames(
+				'flex-column',
+				'd-flex',
+				'justify-content-between',
+				'f-1',
+				'apply_rtl',
+				'h-100',
+				'w-100'
+			)}
+		>
+			<div className="d-flex f-05">
+				<CurrencySlider
+					balance={balance}
+					prices={prices}
+					navigate={navigate}
+					coins={coins}
+					searchResult={searchResult}
+				/>
+			</div>
+			<div className="f-1 wallet-container">
+				<Accordion sections={sections} showActionText={true} />
+			</div>
 		</div>
-		<div className="f-1 wallet-container">
-			<Accordion sections={sections} showActionText={true} />
-		</div>
-	</div>
-);
+	);
+};
 
 export default MobileWallet;
