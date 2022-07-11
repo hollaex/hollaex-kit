dd754e0c4b97980ffe758e4955452106cd5bc1f1
Ali Beikverdi <ali@bitholla.com>
2022-07-11 21:35:29 +0900

    fiat controller syntax eslint

--- server/api/controllers/fiat.js
+++ server/api/controllers/fiat.js
@@ -4,16 +4,16 @@ const { loggerFiat } = require('../../config/logger');
 const toolsLib = require('hollaex-tools-lib');
 
 const createDepositRequest = (req, res) => {
-    loggerFiat.verbose(
+	loggerFiat.verbose(
 		req.uuid,
 		'controllers/fiat/createDepositRequest auth',
 		req.auth
 	);
 
-    const ip = req.headers['x-real-ip'];
-    const userId = req.auth.sub.id;
+	const ip = req.headers['x-real-ip'];
+	const userId = req.auth.sub.id;
 
-    const {
+	const {
 		amount,
 		transaction_id,
 		currency
@@ -26,72 +26,72 @@ const createDepositRequest = (req, res) => {
 		amount,
 		transaction_id,
 		currency,
-        userId
+		userId
 	);
 
-    // TODO check TX amount range
+	// TODO check TX amount range
 
 
-    toolsLib.user.getUserByKitId(userId)
+	toolsLib.user.getUserByKitId(userId)
 		.then(async (user) => {
-            if (!user) {
-                throw new Error('User not found');
-            }
+			if (!user) {
+				throw new Error('User not found');
+			}
 
-            // TODO CHECK TRANASCTION ID TO BE UNIQUE
+			// TODO CHECK TRANASCTION ID TO BE UNIQUE
 			// await validateTxid(transaction_id);
-            // CHECK DEPOSIT FEE
-            let fee = 0;
+			// CHECK DEPOSIT FEE
+			let fee = 0;
 
-            return toolsLib.wallet.mintAssetByKitId(
-                userId,
-                currency,
-                amount,
-                {
-                    description: `Pending deposit created with ${currency.toUpperCase()} for username: ${user.username}`,
-                    transactionId: transaction_id,
-                    status: false,
-                    fee
-                }
-            );
-				})
-				.then((deposit) => {
-					loggerFiat.verbose(
-						req.uuid,
-						'controllers/fiat/createDepositRequest then',
-						'Pending mint created',
-						'transaction_id:',
-						deposit.transaction_id,
-						'amount:',
-						deposit.amount,
-						'fee:',
-						deposit.fee
-					);
+			return toolsLib.wallet.mintAssetByKitId(
+				userId,
+				currency,
+				amount,
+				{
+					description: `Pending deposit created with ${currency.toUpperCase()} for username: ${user.username}`,
+					transactionId: transaction_id,
+					status: false,
+					fee
+				}
+			);
+		})
+		.then((deposit) => {
+			loggerFiat.verbose(
+				req.uuid,
+				'controllers/fiat/createDepositRequest then',
+				'Pending mint created',
+				'transaction_id:',
+				deposit.transaction_id,
+				'amount:',
+				deposit.amount,
+				'fee:',
+				deposit.fee
+			);
 
-					return res.json(deposit);
-				})
-				.catch((err) => {
-					loggerFiat.error(
-						req.uuid,
-						'controllers/fiat/createDepositRequest catch',
-						err.message
-					);
-					return res.status(400).json({ message: err.message });
-				});
+			return res.json(deposit);
+		})
+		.catch((err) => {
+			loggerFiat.error(
+				req.uuid,
+				'controllers/fiat/createDepositRequest catch',
+				err.message
+			);
+			return res.status(400).json({ message: err.message });
+		});
 
 };
 
 const createWithdrawalRequest = (req, res) => {
-    loggerFiat.verbose(
+	loggerFiat.verbose(
 		req.uuid,
 		'controllers/fiat/createWithdrawalRequest auth',
 		req.auth
 	);
 
-    const ip = req.headers['x-real-ip'];
-    const userId = req.auth.sub.id;
+	const ip = req.headers['x-real-ip'];
+	const userId = req.auth.sub.id;
 
-    const {
+	const {
 		amount,
 		transaction_id,
 		currency
@@ -104,16 +104,16 @@ const createWithdrawalRequest = (req, res) => {
 		amount,
 		transaction_id,
 		currency,
-        userId
+		userId
 	);
 
-    // TODO
+	// TODO
 
-    return res.status(400).json({ message: 'Under construction'})
+	return res.status(400).json({ message: 'Under construction' });
 
 };
 
 module.exports = {
-    createDepositRequest,
-    createWithdrawalRequest
-}
+	createDepositRequest,
+	createWithdrawalRequest
+};
