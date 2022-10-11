f96bc3dfa02f10f45f881e935d43b50f118df86a
Amir Hossein Salar <ah.salar@gmail.com>
2022-10-11 17:58:01 +0330

    change for kit and admin apps selector logic

--- web/src/containers/Admin/Apps/utils.js
+++ web/src/containers/Admin/Apps/utils.js
@@ -3,5 +3,9 @@ import { createSelector } from 'reselect';
 const getPlugins = (state) => state.app.plugins;
 
 export const appsSelector = createSelector([getPlugins], (plugins) =>
-	plugins.filter(({ type }) => type === 'app')
+	plugins.filter(
+		({ web_view }) =>
+			web_view &&
+			web_view.some(({ meta: { is_app, type } }) => is_app && type === 'admin')
+	)
 );
