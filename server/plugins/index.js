82f8878b4a55ba99ec259759b2f36f11e9ef08fa
fetok12 <fetok12@gmail.com>
2022-10-14 00:33:51 +0300

    Complete Node Upgrade

--- server/plugins/index.js
+++ server/plugins/index.js
@@ -15,41 +15,13 @@ const fs = require('fs');
 const latestVersion = require('latest-version');
 const npm = require('npm-programmatic');
 const sequelize = require('sequelize');
-const _eval = require('eval');
-const toolsLib = require('hollaex-tools-lib');
 const lodash = require('lodash');
-const expressValidator = require('express-validator');
-const multer = require('multer');
-const moment = require('moment');
-const mathjs = require('mathjs');
-const bluebird = require('bluebird');
-const umzug = require('umzug');
-const rp = require('request-promise');
-const uuid = require('uuid/v4');
-const jwt = require('jsonwebtoken');
-const momentTz = require('moment-timezone');
-const json2csv = require('json2csv');
-const flat = require('flat');
-const ws = require('ws');
-const cron = require('node-cron');
-const randomString = require('random-string');
-const bcryptjs = require('bcryptjs');
-const expectCt = require('expect-ct');
-const validator = require('validator');
-const otp = require('otp');
-const geoipLite = require('geoip-lite');
-const nodemailer = require('nodemailer');
-const wsHeartbeatServer = require('ws-heartbeat/server');
-const wsHeartbeatClient = require('ws-heartbeat/client');
-const winston = require('winston');
-const elasticApmNode = require('elastic-apm-node');
-const winstonElasticsearchApm = require('winston-elasticsearch-apm');
-const tripleBeam = require('triple-beam');
-const uglifyEs = require('uglify-es');
-const bodyParser = require('body-parser');
+const pluginProcess = path.join(__dirname, "./plugin-process.js");
+const { Worker } = require('worker_threads')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 let app;
-let disabledPlugins = {};
+let activePlugins = {}
 
 const getInstalledLibrary = async (name, version) => {
 	const jsonFilePath = path.resolve(__dirname, '../node_modules', name, 'package.json');
@@ -106,10 +78,14 @@ const installLibrary = async (library) => {
 };
 
 const stopPlugin = async (plugin) => {
-
 	try {
-		const subStr = plugin.script.match(/\"\/plugins(.*?)\"/g);
-		disabledPlugins[plugin.name] = subStr || [];
+		loggerPlugin.verbose(
+			'plugins/index/kill_plugin',
+			`killing plugin ${plugin.name}`
+		);
+
+		activePlugins[plugin.name].process.terminate();
+		delete activePlugins[plugin.name];
 
 	} catch (err) {
 		loggerPlugin.error(
@@ -127,58 +103,6 @@ const startPlugin = async (plugin) => {
 			`starting plugin ${plugin.name}`
 		);
 
-		const context = {
-			configValues: {
-				publicMeta: plugin.public_meta,
-				meta: plugin.meta
-			},
-			pluginLibraries: {
-				app,
-				loggerPlugin,
-				toolsLib
-			},
-			app,
-			toolsLib,
-			lodash,
-			expressValidator,
-			loggerPlugin,
-			multer,
-			moment,
-			mathjs,
-			bluebird,
-			umzug,
-			rp,
-			sequelize,
-			uuid,
-			jwt,
-			momentTz,
-			json2csv,
-			flat,
-			ws,
-			cron,
-			randomString,
-			bcryptjs,
-			expectCt,
-			validator,
-			uglifyEs,
-			otp,
-			latestVersion,
-			geoipLite,
-			nodemailer,
-			wsHeartbeatServer,
-			wsHeartbeatClient,
-			cors,
-			winston,
-			elasticApmNode,
-			winstonElasticsearchApm,
-			tripleBeam,
-			bodyParser,
-			morgan,
-			meta: plugin.meta,
-			publicMeta: plugin.public_meta,
-			installedLibraries: {}
-		};
-
 		if (plugin.prescript && lodash.isArray(plugin.prescript.install) && !lodash.isEmpty(plugin.prescript.install)) {
 			loggerPlugin.verbose(
 				'plugins/index/initialization',
@@ -186,7 +110,7 @@ const startPlugin = async (plugin) => {
 			);
 
 			for (const library of plugin.prescript.install) {
-				context.installedLibraries[library] = await installLibrary(library);
+				await installLibrary(library);
 			}
 
 			loggerPlugin.verbose(
@@ -195,9 +119,17 @@ const startPlugin = async (plugin) => {
 			);
 		}
 
-		_eval(plugin.script, plugin.name, context, true);
+		const pluginData = { PORT: 10011 + plugin.id, plugin }
+		const childProcess = new Worker(pluginProcess, {
+			workerData: JSON.stringify(pluginData)
+		})
+		const subStr = plugin.script.match(/\"\/plugins(.*?)\"/g);
 
-		if (disabledPlugins[plugin.name]) delete disabledPlugins[plugin.name];
+		activePlugins[plugin.name] = {
+			process: childProcess,
+			port: pluginData.PORT,
+			endpoints: subStr || [],
+		};
 
 
 		loggerPlugin.verbose(
@@ -232,17 +164,27 @@ checkStatus()
 		app.use(domainMiddleware);
 		helmetMiddleware(app);
 
-		app.use((req, res, next) => {
+
+		const defaultURL = 'http://localhost:10012';
+		const customRouter = function (req) {
 			if (req.path.length > 1 && req.path.includes('/plugins/')) {
-				//Check if plugin is disabled
-				for (let endpoints of Object.values(disabledPlugins)) {
-					if (endpoints.some(endpoint => endpoint.includes(req.path))) { return res.status(404).send() }
+				for (let plugin of Object.values(activePlugins)) {
+					if (plugin.endpoints.some(endpoint => endpoint.includes(req.path))) {
+						return `http://localhost:${plugin.port}`;
+					}
 				}
 			}
-			next();
-		})
+			return defaultURL;
 
-		app.use('/plugins', routes);
+		};
+
+		const options = {
+			target: defaultURL,
+			router: customRouter,
+			changeOrigin: true
+		};
+
+		app.use('/plugins', routes, createProxyMiddleware(options));
 
 		const plugins = await Plugin.findAll({
 			where: {
