c9e85f6eaf87fbee03617cfb83a86942132e9fa3
Amir Hossein Salar <ah.salar@gmail.com>
2023-01-11 18:40:53 +0330

    QR code task refinements

--- web/src/containers/Withdraw/QRScanner.js
+++ web/src/containers/Withdraw/QRScanner.js
@@ -1,35 +1,54 @@
-import React, { useState } from 'react';
-// import { isMobile } from 'react-device-detect';
-import { Button, EditWrapper, QrReader } from 'components';
+import React, { useState, useEffect } from 'react';
+import { isMobile } from 'react-device-detect';
+import BarcodeScannerComponent from 'react-qr-barcode-scanner';
+import { Button, EditWrapper } from 'components';
 import STRINGS from 'config/localizedStrings';
 
 const QRScanner = ({ closeQRScanner, getQRData }) => {
-	const [result, setResult] = useState('No result');
+	const [result, setResult] = useState(STRINGS['QR_CODE.NO_RESULT']);
+	const [error, setError] = useState();
+	const [stopStream, setStopStream] = useState(false);
 
-	const handleScan = (data) => {
+	const onUpdate = (err, data) => {
 		if (data) {
-			setResult(data);
-			getQRData(data);
-			setTimeout(closeQRScanner, 2500);
+			const { text } = data;
+			setResult(text);
+			getQRData(text);
+			setTimeout(closeQRScanner, 2000);
+		} else {
+			setResult(STRINGS['QR_CODE.NOT_FOUND']);
 		}
 	};
 
-	const handleError = (err) => {
-		console.log('err', err);
+	const onError = (error) => {
+		if (error.name === 'NotAllowedError') {
+			setError(STRINGS['QR_CODE.PERMISSION_DENIED']);
+		} else {
+			console.error(error);
+		}
 	};
+
+	useEffect(() => {
+		return setStopStream(true);
+	}, []);
+
+	const facingMode = isMobile ? 'environment' : 'user';
 
 	return (
 		<div className="margin-auto">
 			<div className="qr_reader_wrapper">
-				<QrReader
-					fps={10}
-					qrbox={250}
-					disableFlip={false}
-					qrCodeSuccessCallback={handleScan}
-					qrCodeErrorCallback={handleError}
+				<BarcodeScannerComponent
+					onUpdate={onUpdate}
+					onError={onError}
+					facingMode={facingMode}
+					stopStream={stopStream}
 				/>
 			</div>
-			<p className="text-center">{result}</p>
+			{error ? (
+				<p className="text-center warning_text">{error}</p>
+			) : (
+				<p className="text-center">{result}</p>
+			)}
 			<div className="w-100">
 				<EditWrapper stringId="BACK_TEXT" />
 				<Button label={STRINGS['BACK_TEXT']} onClick={closeQRScanner} />
