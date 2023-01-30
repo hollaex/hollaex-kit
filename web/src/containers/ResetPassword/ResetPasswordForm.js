0fe9aeb213ccfd3c4a63a9c718890e8c0e939720
ram <ram@bitholla.com>
2023-01-30 16:30:19 +0530

    Minor Update

--- web/src/containers/ResetPassword/ResetPasswordForm.js
+++ web/src/containers/ResetPassword/ResetPasswordForm.js
@@ -4,14 +4,13 @@ import { required, password } from 'components/Form/validations';
 import { AuthForm } from 'components';
 import STRINGS from 'config/localizedStrings';
 
-export const generateFormFields = (onBlur) => ({
+export const generateFormFields = () => ({
 	password: {
 		type: 'password',
 		validate: [required, password],
 		fullWidth: true,
 		label: STRINGS['FORM_FIELDS.PASSWORD_LABEL'],
 		placeholder: STRINGS['FORM_FIELDS.PASSWORD_PLACEHOLDER'],
-		onBlur: onBlur,
 	},
 	password_repeat: {
 		type: 'password',
@@ -19,7 +18,6 @@ export const generateFormFields = (onBlur) => ({
 		fullWidth: true,
 		label: STRINGS['FORM_FIELDS.PASSWORD_REPEAT_LABEL'],
 		placeholder: STRINGS['FORM_FIELDS.PASSWORD_REPEAT_PLACEHOLDER'],
-		onBlur: onBlur,
 	},
 });
 
@@ -34,21 +32,15 @@ const validate = (values) => {
 	return errors;
 };
 
-const Form = (props) => {
-	const onChange = (values) => {
-		console.log('onChange');
-	};
-	return (
-		<AuthForm
-			{...props}
-			formFields={generateFormFields(onChange)}
-			buttonLabel={STRINGS['RESET_PASSWORD.BUTTON']}
-		/>
-	);
-};
+const Form = (props) => (
+	<AuthForm
+		{...props}
+		formFields={generateFormFields()}
+		buttonLabel={STRINGS['RESET_PASSWORD.BUTTON']}
+	/>
+);
+
 export default reduxForm({
 	form: 'RequestPasswordForm',
-	touchOnBlur: true,
-	touchOnChange: true,
 	validate,
 })(Form);
