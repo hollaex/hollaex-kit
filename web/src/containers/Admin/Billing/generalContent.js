1dc3bd819e049878e49a7515c72e878810d38bf8
ram <ram@bitholla.com>
2023-02-01 21:15:24 +0530

    Changes for the Billing payment process.

--- web/src/containers/Admin/Billing/generalContent.js
+++ web/src/containers/Admin/Billing/generalContent.js
@@ -26,7 +26,7 @@ import {
 	CopyOutlined,
 	CheckCircleFilled,
 } from '@ant-design/icons';
-import _get from 'lodash/get';
+// import _get from 'lodash/get';
 import isEmpty from 'lodash.isempty';
 
 import { STATIC_ICONS } from 'config/icons';
@@ -55,6 +55,7 @@ import {
 } from 'actions/adminBillingActions';
 import EnterpriseForm from '../EnterPriseForm';
 import { getExchange } from '../AdminFinancials/action';
+import Subscription from './subscription';
 import './Billing.scss';
 
 const { Option } = Select;
@@ -493,6 +494,7 @@ const GeneralContent = ({
 
 			if (name === 'item' || exchangePlanType === 'item') {
 				setExchangePlanType('item');
+				setSelectedPayment('');
 			}
 			setFiatBreadCrumb(false);
 		}
@@ -515,15 +517,6 @@ const GeneralContent = ({
 			setShowCloudPlanDetails(true);
 			setCloudPlanDetails(false);
 			setIsMonthly(false);
-		}
-	};
-
-	const isCloud = () => {
-		const exchangePlans = ['basic', 'crypto', 'fiat'];
-		if (exchangePlans.includes(dashExchange.plan)) {
-			return true;
-		} else {
-			return false;
 		}
 	};
 
@@ -608,74 +601,19 @@ const GeneralContent = ({
 
 	const renderFooter = () => {
 		return (
-			<div>
-				<div className="horizantal-line">
-					<div className="plan-header">
-						{exchangeCardKey === 'diy'
-							? 'Selected DIY plan'
-							: 'Selected cloud plan'}
-					</div>
-					<div className="subscription-container">
-						<div className="plan-card">
-							{exchangeCardKey === 'cloudExchange' ? (
-								<div className="card-icon">
-									<ReactSVG
-										src={STATIC_ICONS['CLOUD_BASIC']}
-										className="cloud-background"
-									/>
-									<ReactSVG
-										src={STATIC_ICONS['CLOUD_CRYPTO']}
-										className="cloud-icon"
-									/>
-								</div>
-							) : (
-								<ReactSVG
-									src={STATIC_ICONS['DIY_FIRE_MAN_ICON']}
-									className="diy-icon"
-								/>
-							)}
-
-							<div>
-								<p className="f-16">{selectedPlanData[selectedType]?.title}</p>
-								<h6>
-									{isCloud()
-										? isMonthly
-											? 'HollaEx Monthly Cloud Hosting:'
-											: 'HollaEx Yearly Cloud Hosting:'
-										: 'HollaEx Yearly DIY Hosting:'}
-								</h6>
-							</div>
-						</div>
-						<div className="exchange-text">
-							<span>
-								<ReactSVG
-									src={STATIC_ICONS['EXCHANGE_LOGO_LIGHT_THEME']}
-									className="cloud-icon"
-								/>
-							</span>
-							<h6>{dashExchange?.name}</h6>
-						</div>
-						<div className="payment-container">
-							<p className="f-20">
-								{isMonthly ? 'Monthly payment:' : 'Yearly payment:'}
-							</p>
-							<p className="f-20">
-								{exchangePlanType === 'payment'
-									? `${selectedCrypto.symbol.toUpperCase()} ${
-											paymentAddressDetails?.amount
-									  }`
-									: paymentAddressDetails?.amount
-									? `${paymentAddressDetails.currency.toUpperCase()} ${
-											paymentAddressDetails?.amount
-									  }`
-									: isMonthly
-									? `USD${_get(planPriceData, 'month.price')}`
-									: `USD ${_get(planPriceData, 'year.price')}`}
-							</p>
-						</div>
-					</div>
-					<div>{renderBtn()}</div>
-				</div>
+			<div className="horizantal-line">
+				<Subscription
+					selectedCrypto={selectedCrypto}
+					selectedType={selectedType}
+					planPriceData={planPriceData}
+					isMonthly={isMonthly}
+					dashExchange={dashExchange}
+					selectedPlanData={selectedPlanData}
+					exchangeCardKey={exchangeCardKey}
+					paymentAddressDetails={paymentAddressDetails}
+					exchangePlanType={exchangePlanType}
+				/>
+				<div>{renderBtn()}</div>
 			</div>
 		);
 	};
@@ -795,7 +733,9 @@ const GeneralContent = ({
 							{fiatBreadCrumb
 								? name.charAt(0).toUpperCase() + name.slice(1)
 								: name === 'crypto'
-								? selectedPayment === 'cryptoCurrency' && 'Crypto'
+								? selectedPayment === 'cryptoCurrency' &&
+								  exchangePlanType === 'crypto' &&
+								  'Crypto'
 								: name.charAt(0).toUpperCase() + name.slice(1)}
 						</Breadcrumb.Item>
 					);
@@ -810,6 +750,7 @@ const GeneralContent = ({
 		} else {
 			setExchangeCardKey('cloudExchange');
 		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [selectedType]);
 
 	const handleOpenModal = () => {
@@ -1403,6 +1344,7 @@ const GeneralContent = ({
 		} else if (exchangePlanType === 'method') {
 			setExchangePlanType('item');
 			setIsMonthly(!isMonthly);
+			setPaymentAddressDetails({ address: '', amount: 0, currency: '' });
 		} else if (exchangePlanType === 'crypto') {
 			setExchangePlanType('method');
 			setSelectedCrypto({ coin: 'XHT', symbol: 'xht' });
