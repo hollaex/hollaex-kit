9469026996921e3372478af46b08a6c1528d6756
fetok12 <fetok12@gmail.com>
2023-02-07 18:14:57 +0300

    Update index.js

--- web/src/config/index.js
+++ web/src/config/index.js
@@ -1,7 +1,9 @@
 const PRODUCTION_ENDPOINT =
 	process.env.REACT_APP_SERVER_ENDPOINT || 'https://api.hollaex.com';
 
-const DEVELOPMENT_ENDPOINT = 'http://localhost'
+const DEVELOPMENT_ENDPOINT =
+	process.env.REACT_APP_DEVELOPMENT_ENDPOINT ||
+	'https://api.sandbox.hollaex.com';
 
 const PRODUCTION_NETWORK_ENDPOINT =
 	process.env.REACT_APP_SERVER_NETWORK_ENDPOINT ||
