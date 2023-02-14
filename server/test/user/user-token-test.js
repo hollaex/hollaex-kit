49978dbc701b51a46cacac8b1639416229457ea4
fetok12 <fetok12@gmail.com>
2023-02-14 20:40:21 +0300

    Admin Tests

---
+++ server/test/user/user-token-test.js
@@ -0,0 +1,143 @@
+const {
+    request,
+    generateFuzz,
+    loginAs,
+    getAdminUser
+} = require('../helpers');
+const tools = require('hollaex-tools-lib');
+
+
+describe('tests for /user/token', function () {
+    let user, bearerToken;
+    before(async () => {
+        user = await tools.user.getUserByEmail(getAdminUser().email);
+        user.should.be.an('object');
+        bearerToken = await loginAs(user);
+        bearerToken.should.be.a('string');
+    });
+
+    const getOtpCode = async () => {
+        let secret;
+    
+        if (!(await tools.security.userHasOtpEnabled(user.id))) {
+            secret = await tools.security.createOtp(user.id);
+            await tools.security.setActiveUserOtp(user.id);
+        } else {
+            const otpCode = await tools.database.findOne('otp code', { where: {
+                used: true,
+                user_id:user.id
+            },
+            attributes: ['id', 'secret'],
+            order: [['updated_at', 'DESC']]
+            });
+            secret = otpCode.secret;
+
+        }
+        return await tools.security.generateOtp(secret);
+    }
+
+    const getEmailCode = async () => {
+        await request()
+        .get('/v2/user/request-email-confirmation')
+        .set('Authorization', `Bearer ${bearerToken}`)
+
+        const code = await client.getAsync(`ConfirmationEmail:${user.id}`);
+        return code;
+    }
+
+
+    //Integration Testing
+    it('Integration Test -should respond 200 for "Success"', async () => {
+
+        const tokenModel = getModel('token');
+        let token = await tokenModel.findOne({ user_id: user.id })
+
+        if(!token){
+            const response = await request()
+			.post('/v2/user/token')
+			.set('Authorization', `Bearer ${bearerToken}`)
+			.send({
+				name: 'tokenTest',
+				otp_code: await getOtpCode(),
+				email_code: await getEmailCode(),
+			});
+
+            response.should.have.status(200);
+            response.should.be.json;
+        }
+    });
+
+
+    it('Integration Test -should respond 200 for "Success"', async () => {
+
+        const tokenModel = getModel('token');
+        let token = await tokenModel.findOne({ user_id: user.id })
+
+        if(token){
+
+            const responseFirst = await request()
+			.put('/v2/user/token')
+			.set('Authorization', `Bearer ${bearerToken}`)
+			.send({
+                token_id: token.id,
+				otp_code: await getOtpCode(),
+				email_code: await getEmailCode(),
+				permissions: {
+					can_withdraw: false
+				},
+				whitelisting_enabled: false
+			});
+
+            responseFirst.should.have.status(200);
+            responseFirst.should.be.json;
+            responseFirst.body.can_withdraw.should.equal(false)
+
+
+            const responseSecond = await request()
+			.put('/v2/user/token')
+			.set('Authorization', `Bearer ${bearerToken}`)
+			.send({
+                token_id: token.id,
+				otp_code: await getOtpCode(),
+				email_code: await getEmailCode(),
+				permissions: {
+					can_withdraw: true
+				},
+				whitelisting_enabled: false
+			});
+
+            responseSecond.should.have.status(200);
+            responseSecond.should.be.json;
+            responseSecond.body.can_withdraw.should.equal(true)
+        }
+
+    });
+
+
+    it('Integration Test -should respond 200 for "Success"', async () => {
+
+        const tokenModel = getModel('token');
+        let token = await tokenModel.findOne({ user_id: user.id })
+
+        if(token){
+
+            const response = await request()
+			.delete('/v2/user/token')
+			.set('Authorization', `Bearer ${bearerToken}`)
+			.send({
+                token_id: token.id,
+				otp_code: await getOtpCode(),
+				email_code: await getEmailCode(),
+			});
+
+            response.should.have.status(200);
+            response.should.be.json;
+
+            token = await tokenModel.findOne({ user_id: user.id })
+
+            if(token) { throw new Error('Token cannot exist')}
+        }
+
+    });
+
+});
--- server/test/user/user-withdrawal-test.js
+++ server/test/user/user-withdrawal-test.js
@@ -18,40 +18,38 @@ describe('tests for /user/withdrawal', function () {
         bearerToken.should.be.a('string');
     });
 
+    const getOtpCode = async () => {
+        let secret;
+    
+        if (!(await tools.security.userHasOtpEnabled(user.id))) {
+            secret = await tools.security.createOtp(user.id);
+            await tools.security.setActiveUserOtp(user.id);
+        } else {
+            const otpCode = await tools.database.findOne('otp code', { where: {
+                used: true,
+                user_id:user.id
+            },
+            attributes: ['id', 'secret'],
+            order: [['updated_at', 'DESC']]
+            });
+            secret = otpCode.secret;
 
-      //Integration Testing
-      it('Integration Test -should respond 200 for "Success"', async () => {
-
-        const getOtpCode = async () => {
-            let secret;
-        
-            if (!(await tools.security.userHasOtpEnabled(user.id))) {
-                secret = await tools.security.createOtp(user.id);
-                await tools.security.setActiveUserOtp(user.id);
-            } else {
-                const otpCode = await tools.database.findOne('otp code', { where: {
-                    used: true,
-                    user_id:user.id
-                },
-                attributes: ['id', 'secret'],
-                order: [['updated_at', 'DESC']]
-                });
-                secret = otpCode.secret;
-
-            }
-            return await tools.security.generateOtp(secret);
         }
+        return await tools.security.generateOtp(secret);
+    }
 
-        const getEmailCode = async () => {
-            await request()
-            .get('/v2/user/request-email-confirmation')
-            .set('Authorization', `Bearer ${bearerToken}`)
+    const getEmailCode = async () => {
+        await request()
+        .get('/v2/user/request-email-confirmation')
+        .set('Authorization', `Bearer ${bearerToken}`)
 
-            const code = await client.getAsync(`ConfirmationEmail:${user.id}`);
-            return code;
-        }
+        const code = await client.getAsync(`ConfirmationEmail:${user.id}`);
+        return code;
+    }
 
-  
+    //Integration Testing
+    it('Integration Test -should respond 200 for "Success"', async () => {
+
         const tokenModel = getModel('token');
         let token = await tokenModel.findOne({ user_id: user.id })
 
@@ -107,9 +105,140 @@ describe('tests for /user/withdrawal', function () {
 
             response.should.have.status(200);
             response.should.be.json;
-
             response.body.should.have.property("transaction_id");
+    
+        const cancelWithdrawal = await request()
+			.delete(`/v2/user/withdrawal?id=${response.body.id}`)
+			.set('Authorization', `Bearer ${bearerToken}`)
+
+            if(cancelWithdrawal.body.message){
+                cancelWithdrawal.should.have.status(400);
+            }else cancelWithdrawal.should.have.status(200);
+            
+            cancelWithdrawal.should.be.json;
+    
     });
 
-    //delete
+    it('Integration Test -should respond 403 for lack of authority for withdraw', async () => {
+
+        const tokenModel = getModel('token');
+        let token = await tokenModel.findOne({ user_id: user.id })
+
+        let apiKey = token?.key;
+        let apiSecret = token?.secret;
+
+        if(!token){
+
+            let hmac;
+            hmac = await request()
+			.post('/v2/user/token')
+			.set('Authorization', `Bearer ${bearerToken}`)
+			.send({
+				name: 'tokenTest',
+				otp_code: await getOtpCode(),
+				email_code: await getEmailCode(),
+			});
+
+            apiKey = hmac.body.key;
+            apiSecret = hmac.body.secret;
+
+            token = hmac.body;
+        }
+
+        await request()
+			.put('/v2/user/token')
+			.set('Authorization', `Bearer ${bearerToken}`)
+			.send({
+				token_id: token.id,
+				otp_code: await getOtpCode(),
+				email_code: await getEmailCode(),
+				permissions: {
+					can_withdraw: false
+				},
+				whitelisting_enabled: false
+			});
+
+        const body = {
+            network: "trx",
+            address: "TFQ9gxeMEkmKoxgrbnHNdu4e3VdNL11vyy",
+            amount: 1,
+            currency: "usdt"
+        }
+
+        const expires = token.expiry / 1000;
+		const signature = tools.security.calculateSignature(token.secret, 'POST', '/v2/user/withdrawal', expires, body);
+        const response = await request()
+			.post('/v2/user/withdrawal')
+			.set('Api-key', apiKey)
+			.set('Api-expires', expires)
+			.set('Api-signature', signature)
+			.send(body);
+
+            response.should.have.status(403);
+            response.should.be.json;
+    
+    });
+
+
+    //Fuzz Testing
+    it('Fuzz Test -should return error', async () => {
+
+        const tokenModel = getModel('token');
+        let token = await tokenModel.findOne({ user_id: user.id })
+
+        let apiKey = token?.key;
+        let apiSecret = token?.secret;
+
+        if(!token){
+
+            let hmac;
+            hmac = await request()
+			.post('/v2/user/token')
+			.set('Authorization', `Bearer ${bearerToken}`)
+			.send({
+				name: 'tokenTest',
+				otp_code: await getOtpCode(),
+				email_code: await getEmailCode(),
+			});
+
+            apiKey = hmac.body.key;
+            apiSecret = hmac.body.secret;
+
+            token = hmac.body;
+        }
+
+        await request()
+			.put('/v2/user/token')
+			.set('Authorization', `Bearer ${bearerToken}`)
+			.send({
+				token_id: token.id,
+				otp_code: await getOtpCode(),
+				email_code: await getEmailCode(),
+				permissions: {
+					can_withdraw: true
+				},
+				whitelisting_enabled: false
+			});
+
+        const body = {
+            network: "trx",
+            address: generateFuzz(),
+            amount: generateFuzz(),
+            currency: "usdt"
+        }
+
+        const expires = token.expiry / 1000;
+		const signature = tools.security.calculateSignature(token.secret, 'POST', '/v2/user/withdrawal', expires, body);
+        const response = await request()
+			.post('/v2/user/withdrawal')
+			.set('Api-key', apiKey)
+			.set('Api-expires', expires)
+			.set('Api-signature', signature)
+			.send(body);
+
+            response.should.have.status(500);
+            response.should.be.json;
+    
+    });
+    
 });
