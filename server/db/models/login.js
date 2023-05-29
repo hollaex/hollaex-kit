267e1c1194eec31c4bca73c657d2ea6b9ae91da9
fetok12 <fetok12@gmail.com>
2023-05-29 17:04:24 +0300

    Update Data value integer

--- server/db/models/login.js
+++ server/db/models/login.js
@@ -28,7 +28,7 @@ module.exports = function (sequelize, DataTypes) {
 				defaultValue: ''
 			},
 			attempt: {
-				type: DataTypes.NUMBER,
+				type: DataTypes.INTEGER,
 				allowNull: false,
 				defaultValue: 0
 			},
