19d0b1ae83e13dfb0ffd4b97fcfee57e613a2200
fetok12 <fetok12@gmail.com>
2023-10-04 01:56:03 +0300

    Logic Refinements

--- server/db/migrations/20230929620261-add-transaction-limit-data.js
+++ server/db/migrations/20230929620261-add-transaction-limit-data.js
@@ -2,22 +2,23 @@
 
 const models = require('../models');
 
-
 module.exports = {
 	async up(queryInterface) {
 		const transactionLimitModel = models['TransactionLimit'];
 		const tierModel = models['Tier'];
 		const tiers = await tierModel.findAll({});
 		const types = ['withdrawal', 'deposit'];
+		const statusModel = models['Status'];
+		const status = await statusModel.findOne({});
 
 		for(const tier of tiers) {
 			for(const type of types) {
-				const limit = await transactionLimitModel.findOne({ tier: tier.id, type });
+				const limit = await transactionLimitModel.findOne({ where: { tier: tier.id, type } });
 				if (!limit) {
 					await transactionLimitModel.create({
 						tier: tier.id,
-						amount: 0,
-						currency: getKitConfig().native_currency,
+						amount: type === 'withdrawal' ? tier.withdrawal_limit || 0 : tier.deposit_limit || 0,
+						currency: status?.kit?.native_currency || 'usdt',
 						limit_currency: 'default', 
 						type, 
 						period: '24h'
