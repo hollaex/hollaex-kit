4d802ecc5a947a060d507fa58fcbdbf11460e928
fetok12 <fetok12@gmail.com>
2023-11-06 13:07:24 +0300

    Remove checkActivation

--- server/db/seeders/20231105027546-add-limits.js
+++ server/db/seeders/20231105027546-add-limits.js
@@ -27,28 +27,25 @@ const checkActivation = (activation_code) => {
 const TABLE = 'TransactionLimits';
 
 module.exports = {
-	up: (queryInterface) => {
-		return checkActivation(process.env.ACTIVATION_CODE)
-			.then(async (exchange) => {
-				const transactionLimitModel = models['TransactionLimit'];
-				const types = ['withdrawal', 'deposit'];
-				const tierLevels = [1, 2];
-				for (const level of tierLevels) {
-					for (const type of types) {
-						const limit = await transactionLimitModel.findOne({ where: { tier: level, type } });
-						if (!limit) {
-							await transactionLimitModel.create({
-								tier: level,
-								amount: 0,
-								monthly_amount: 0,
-								currency: 'usdt',
-								limit_currency: 'default', 
-								type, 
-							});
-						}
-					}
+	up: async (queryInterface) => {
+		const transactionLimitModel = models['TransactionLimit'];
+		const types = ['withdrawal', 'deposit'];
+		const tierLevels = [1, 2];
+		for (const level of tierLevels) {
+			for (const type of types) {
+				const limit = await transactionLimitModel.findOne({ where: { tier: level, type } });
+				if (!limit) {
+					await transactionLimitModel.create({
+						tier: level,
+						amount: 0,
+						monthly_amount: 0,
+						currency: 'usdt',
+						limit_currency: 'default', 
+						type, 
+					});
 				}
-			});
+			}
+		}
 	},
 	down: (queryInterface) => queryInterface.bulkDelete(TABLE)
 };
