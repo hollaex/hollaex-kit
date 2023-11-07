0920ce2d58c2e13ab36547d6d947bc9afcee1a7a
fetok12 <fetok12@gmail.com>
2023-11-08 01:03:06 +0300

    Drop Table

---
+++ server/db/migrations/20231107351733-drop-triggers-verification.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const TABLE = 'Audits';
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		const drops = 
+		`
+		DROP TRIGGER IF EXISTS create_verificationcode_after_user ON "Users";
+		DROP TRIGGER IF EXISTS update_userlevel_after_verify ON "VerificationCodes";
+		`;
+		await queryInterface.sequelize.query(drops);
+
+		queryInterface.dropTable('VerificationCodes');
+	},
+	down: (queryInterface, Sequelize) =>
+	new Promise((resolve) => {
+		resolve();
+	})
+};
