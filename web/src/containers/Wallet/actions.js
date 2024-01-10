658364c9d8dce77a799a67bba4d85f3a11d6710c
fetok12 <fetok12@gmail.com>
2024-01-11 01:16:37 +0300

    Logic Refinements

--- web/src/containers/Wallet/actions.js
+++ web/src/containers/Wallet/actions.js
@@ -12,11 +12,3 @@ export const fetchPlHistory = (values) => {
 		values && Object.keys(values).length ? querystring.stringify(values) : '';
 	return requestAuthenticated(`/user/balance-pl?${queryValues}`);
 };
-
-// export const get = (values) => {
-// 	return axios({
-// 		method: 'PUT',
-// 		url: '/admin/transaction/limit',
-// 		data: values,
-// 	});
-// };
