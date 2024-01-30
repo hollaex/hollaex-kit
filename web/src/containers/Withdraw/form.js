7efb56b5f72ac009861cb6efb78545443bfddc21
fetok12 <fetok12@gmail.com>
2024-01-30 11:09:24 +0300

    revert commit

--- web/src/containers/Withdraw/form.js
+++ web/src/containers/Withdraw/form.js
@@ -122,7 +122,7 @@ class Form extends Component {
 	};
 
 	onAcceptDialog = () => {
-		if (!this.props.otp_enabled) {
+		if (this.props.otp_enabled) {
 			this.setState({ dialogOtpOpen: true });
 		} else {
 			this.onCloseDialog();
