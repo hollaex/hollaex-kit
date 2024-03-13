cd8c0ab981a78d75ad46e30ee38b2cf92bb0a941
ram <ram@hollaex.com>
2024-03-13 20:53:28 +0530

    Improved the Assets page UI and displayed the estimated balance on the wallet page

--- web/src/containers/Wallet/CurrencyWallet.js
+++ web/src/containers/Wallet/CurrencyWallet.js
@@ -11,7 +11,12 @@ import {
 	Help,
 	ActionNotification,
 } from 'components';
-import { DEFAULT_COIN_DATA } from 'config/constants';
+import { DEFAULT_COIN_DATA, BASE_CURRENCY } from 'config/constants';
+import {
+	formatCurrencyByIncrementalUnit,
+	calculateOraclePrice,
+} from 'utils/currency';
+import { assetsSelector } from './utils';
 import { formatToCurrency, getCurrencyFromName } from 'utils/currency';
 import STRINGS from 'config/localizedStrings';
 import withConfig from 'components/ConfigProvider/withConfig';
@@ -129,7 +134,31 @@ class Wallet extends Component {
 			coins[currency] || DEFAULT_COIN_DATA;
 		const balanceValue = balance[`${currency}_balance`] || 0;
 		const availableBalanceValue = balance[`${currency}_available`] || 0;
-
+		const baseCoin = coins[BASE_CURRENCY] || DEFAULT_COIN_DATA;
+		const filteredAssets = this.props.assets.filter(
+			(val) => val[1].symbol === currency
+		);
+		const [
+			assetsValue,
+		] = filteredAssets.map(([_, { increment_unit, oraclePrice }]) => ({
+			increment_unit,
+			oraclePrice,
+		}));
+		const balanceText =
+			assetsValue &&
+			assetsValue.increment_unit &&
+			assetsValue &&
+			assetsValue.oraclePrice
+				? currency === BASE_CURRENCY
+					? formatCurrencyByIncrementalUnit(
+							balanceValue,
+							assetsValue.increment_unit
+					  )
+					: formatCurrencyByIncrementalUnit(
+							calculateOraclePrice(balanceValue, assetsValue.oraclePrice),
+							baseCoin.increment_unit
+					  )
+				: '';
 		return (
 			<div className="currency-wallet-wrapper">
 				<div className="d-flex mt-5 mb-5">
@@ -195,6 +224,13 @@ class Wallet extends Component {
 									)}
 								</EditWrapper>
 							</span>
+							{!isMobile &&
+								currency !== BASE_CURRENCY &&
+								parseFloat(balanceText || 0) > 0 && (
+									<p className="estimated-balance">
+										{`(≈ ${baseCoin.display_name} ${balanceText})`}
+									</p>
+								)}
 							<p className="available-balance-wrapper">
 								<EditWrapper stringId="CURRENCY_WALLET.AVAILABLE_BALANCE">
 									{STRINGS.formatString(
@@ -319,17 +355,26 @@ class Wallet extends Component {
 	}
 }
 
-const mapStateToProps = ({ app, orderbook, user, asset: { chartData } }) => ({
-	coins: app.coins,
-	price: orderbook.price,
-	balance: user.balance,
-	activeLanguage: app.language,
-	contracts: app.contracts,
-	constants: app.constants,
-	quicktrade: app.quicktrade,
-	pairs: app.pairs,
-	pair: app.pair,
-	chartData,
-});
+const mapStateToProps = (store) => {
+	const {
+		app,
+		orderbook,
+		user,
+		asset: { chartData },
+	} = store;
+	return {
+		coins: app.coins,
+		price: orderbook.price,
+		balance: user.balance,
+		activeLanguage: app.language,
+		contracts: app.contracts,
+		constants: app.constants,
+		quicktrade: app.quicktrade,
+		pairs: app.pairs,
+		pair: app.pair,
+		chartData,
+		assets: assetsSelector(store),
+	};
+};
 
 export default connect(mapStateToProps)(withConfig(Wallet));
