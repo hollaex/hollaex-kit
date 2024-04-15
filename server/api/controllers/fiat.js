a5d083ff7d0e791390dacfa1348842e2db5a5474
fetok12 <fetok12@gmail.com>
2024-04-15 10:44:43 +0300

    create-withdrawal-by-admin

--- server/api/controllers/fiat.js
+++ server/api/controllers/fiat.js
@@ -202,7 +202,116 @@ const createWithdrawalRequest = (req, res) => {
 		});
 };
 
+const createWithdrawalRequestByAdmin = (req, res) => {
+	loggerFiat.verbose(
+		req.uuid,
+		'controllers/fiat/createWithdrawalRequestByAdmin auth',
+		req.auth
+	);
+
+	const ip = req.headers['x-real-ip'];
+
+	const {
+		amount,
+		bank_id,
+		currency,
+		user_id
+	} = req.swagger.params.data.value;
+
+	const userId = user_id;
+
+	loggerFiat.verbose(
+		req.uuid,
+		'controllers/fiat/createWithdrawalRequestByAdmin data',
+		ip,
+		amount,
+		bank_id,
+		currency,
+		userId
+	);
+
+	toolsLib.user.getUserByKitId(userId)
+		.then(async (user) => {
+			if (!user) {
+				throw new Error('User not found');
+			}
+
+			const bank = user.bank_account.find((bank) => {
+				return bank.id === bank_id;
+			});
+
+			if (!bank) {
+				throw new Error('The selected payment option is not registered');
+			}
+
+			const { count: withdrawalCount } = await toolsLib.wallet.getUserWithdrawalsByKitId(
+				userId,
+				currency,
+				false,
+				false,
+				false,
+				false,
+				false
+			);
+
+			if (withdrawalCount > 3) {
+				throw new Error('You already have 3 pending withdrawals. Please wait for them to be processed before creating new withdrawals.');
+			}
+
+			// fee_coin is always the same as currency in fiat
+			const { fee } = await toolsLib.wallet.validateWithdrawal(user, bank_id, amount, currency, 'fiat');
+			
+			let bankFormat = '';
+
+			Object.keys(bank).forEach((key, i) => {
+				if (key !== 'id' && key !== 'status' && bank[key] && bank[key] != undefined) {
+					bankFormat += `${key}: ${bank[key]}`;
+					if (i !== Object.keys(bank).length - 1) {
+						bankFormat += ' - ';
+					}
+				}
+			});
+
+			return toolsLib.wallet.burnAssetByKitId(
+				userId,
+				currency,
+				amount,
+				{
+					description: `withdrawal to ${bankFormat}`,
+					transactionId: req.uuid,
+					address: bank_id,
+					status: false,
+					fee
+				}
+			);
+		})
+		.then((deposit) => {
+			loggerFiat.verbose(
+				req.uuid,
+				'controllers/fiat/createWithdrawalRequestByAdmin then',
+				'Pending mint created',
+				'transaction_id:',
+				deposit.transaction_id,
+				'amount:',
+				deposit.amount,
+				'fee:',
+				deposit.fee
+			);
+
+			return res.json(deposit);
+		})
+		.catch((err) => {
+			loggerFiat.error(
+				req.uuid,
+				'controllers/fiat/createWithdrawalRequestByAdmin catch',
+				err.message
+			);
+			return res.status(400).json({ message: err.message });
+		});
+};
+
 module.exports = {
 	createDepositRequest,
-	createWithdrawalRequest
+	createWithdrawalRequest,
+	createWithdrawalRequestByAdmin
 };
