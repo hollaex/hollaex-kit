912fcbdd8e6afc2e218eb8df882b820a7c5f50df
Ali Beikverdi <ali@bitholla.com>
2024-04-28 19:01:51 +0900

    increase tier description length

--- server/db/models/tier.js
+++ server/db/models/tier.js
@@ -18,7 +18,7 @@ module.exports = function (sequelize, DataTypes) {
 				defaultValue: ''
 			},
 			description: {
-				type: DataTypes.STRING,
+				type: DataTypes.TEXT,
 				allowNull: false
 			},
 			fees: {
