5dc07d2adc5d2e5c52056b6ab8588a9e9c8a1f69
fetok12 <fetok12@gmail.com>
2024-06-14 03:17:22 +0300

    fix lint

--- server/db/migrations/20240610126439-add-p2p-transaction-closed-enum.js
+++ server/db/migrations/20240610126439-add-p2p-transaction-closed-enum.js
@@ -2,14 +2,15 @@
 
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-    try {
-      await queryInterface.sequelize.query(`
-		    ALTER TYPE "enum_P2pTransactions_transaction_status"
-		    ADD VALUE 'closed';
-		  `);
-    } catch (error) {
-       return error;
-    }
+    	try {
+    	  	await queryInterface.sequelize.query(`
+			    ALTER TYPE "enum_P2pTransactions_transaction_status"
+			    ADD VALUE 'closed';
+			  `);
+    	}
+		catch (error) {
+    		return error;
+    	}
 	},
 	down: () =>
 		new Promise((resolve) => {
