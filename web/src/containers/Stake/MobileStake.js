f69a1eef9546dbf2e1e40ec6328ade2f0dae7261
ram <ram@hollaex.com>
2024-06-24 23:12:35 +0530

    Updated the stake mobile view

--- web/src/containers/Stake/MobileStake.js
+++ web/src/containers/Stake/MobileStake.js
@@ -6,13 +6,7 @@ import { Button as AntBtn } from 'antd';
 import { openConnectViaDesktop } from 'actions/appActions';
 
 import STRINGS from 'config/localizedStrings';
-import {
-	IconTitle,
-	HeaderSection,
-	EditWrapper,
-	Button,
-	Coin,
-} from 'components';
+import { HeaderSection, EditWrapper, Button, Coin, Image } from 'components';
 import withConfig from 'components/ConfigProvider/withConfig';
 
 import Account from './components/Account';
@@ -48,29 +42,29 @@ class Stake extends Component {
 		const { display_name } = coins[STAKING_INDEX_COIN];
 
 		return (
-			<div className="presentation_container apply_rtl wallet-wrapper">
-				<div
-					style={{
-						marginTop: 20,
-						marginBottom: 20,
-						display: 'flex',
-						justifyContent: 'center',
-					}}
-				>
+			<div className="presentation_container apply_rtl wallet-wrapper stake-wrapper">
+				<div className="stake-icon-container">
+					<div className="stake-icon-wrapper">
+						<div className="stake-icon">
+							<Image icon={ICONS['TAB_STAKE']} iconId="TAB_STAKE" />
+						</div>
+						<div>
+							<EditWrapper stringId="STAKE.TITLE">
+								<span className="stake-title">{STRINGS['STAKE.TITLE']}</span>
+							</EditWrapper>
+						</div>
+					</div>
+				</div>
+				<div className="stake-button-wrapper">
 					{this.props?.constants?.features?.cefi_stake &&
 						this.props?.constants?.features?.stake_page && (
-							<div className="d-flex">
+							<div className="stake-button-container">
 								<span
-									className="stakingOption"
-									style={{
-										marginRight: 5,
-										padding: 10,
-										borderRadius: 10,
-										cursor: 'pointer',
-										fontWeight:
-											this.state.selectedStaking === 'defi' ? 'bold' : 'normal',
-										opacity: this.state.selectedStaking === 'defi' ? 1 : 0.7,
-									}}
+									className={`stakingOption defi-button ${
+										this.state.selectedStaking === 'defi'
+											? 'stake-text-bold'
+											: 'stake-text-normal'
+									}`}
 									onClick={() => {
 										this.setState({
 											selectedStaking: 'defi',
@@ -80,16 +74,11 @@ class Stake extends Component {
 									DeFi Staking
 								</span>
 								<span
-									className="stakingOption"
-									style={{
-										marginLeft: 5,
-										padding: 10,
-										borderRadius: 10,
-										cursor: 'pointer',
-										fontWeight:
-											this.state.selectedStaking === 'cefi' ? 'bold' : 'normal',
-										opacity: this.state.selectedStaking === 'cefi' ? 1 : 0.7,
-									}}
+									className={`stakingOption cefi-button ${
+										this.state.selectedStaking === 'cefi'
+											? 'stake-text-bold'
+											: 'stake-text-normal'
+									}`}
 									onClick={() => {
 										this.setState({
 											selectedStaking: 'cefi',
@@ -112,137 +101,132 @@ class Stake extends Component {
 
 				{this.state.selectedStaking === 'defi' && (
 					<>
-						<div className="d-flex align-end justify-content-between">
-							<IconTitle
-								stringId="STAKE.TITLE"
-								text={STRINGS['STAKE.TITLE']}
-								iconPath={ICONS['TAB_STAKE']}
-								iconId="TAB_STAKE"
-								textType="title"
-							/>
+						<div className="d-flex align-end justify-content-end">
 							<Account />
 						</div>
 						<div className={classnames('wallet-container', 'no-border')}>
 							<div className="wallet-assets_block">
-								<div className="d-flex justify-content-between align-start">
-									<div>
-										<HeaderSection
-											stringId="STAKE.DEFI_TITLE"
-											title={STRINGS['STAKE.DEFI_TITLE']}
-										>
-											<div className="header-content">
-												<div>
-													<EditWrapper stringId="STAKE.DEFI_TEXT">
-														{STRINGS['STAKE.DEFI_TEXT']}
-													</EditWrapper>
-												</div>
+								<div>
+									<HeaderSection
+										stringId="STAKE.DEFI_TITLE"
+										title={STRINGS['STAKE.DEFI_TITLE']}
+									>
+										<div className="header-content">
+											<div>
+												<EditWrapper stringId="STAKE.DEFI_TEXT">
+													{STRINGS['STAKE.DEFI_TEXT']}
+												</EditWrapper>
 											</div>
-										</HeaderSection>
-										<div className="secondary-text">
-											{STRINGS.formatString(
-												STRINGS['STAKE.CURRENT_ETH_BLOCK'],
-												<span className="blue-link pointer underline-text" />
-											)}
 										</div>
-										<div className="secondary-text">
-											{STRINGS.formatString(
-												STRINGS['STAKE.ON_EXCHANGE_XHT'],
-												display_name,
-												<ConnectWrapper onClick={openConnectViaDesktop} />,
-												''
-											)}
-										</div>
+									</HeaderSection>
+									<div className="secondary-text">
+										{STRINGS.formatString(
+											STRINGS['STAKE.CURRENT_ETH_BLOCK'],
+											<span className="blue-link pointer underline-text" />
+										)}
 									</div>
-									<StakesAndEarnings />
+									<div className="secondary-text">
+										{STRINGS.formatString(
+											STRINGS['STAKE.ON_EXCHANGE_XHT'],
+											display_name,
+											<ConnectWrapper onClick={openConnectViaDesktop} />,
+											''
+										)}
+									</div>
 								</div>
-								<table className="wallet-assets_block-table">
-									<thead>
-										<tr className="table-bottom-border">
-											<th />
-											<th>
-												<EditWrapper stringId="STAKE_TABLE.CURRENCY">
-													{STRINGS['STAKE_TABLE.CURRENCY']}
-												</EditWrapper>
-											</th>
-											<th>
-												<EditWrapper stringId="STAKE_TABLE.AVAILABLE">
-													{STRINGS['STAKE_TABLE.AVAILABLE']}
-												</EditWrapper>
-											</th>
-											<th>
-												<EditWrapper stringId="STAKE_TABLE.TOTAL">
-													{STRINGS['STAKE_TABLE.TOTAL']}
-												</EditWrapper>
-											</th>
-											<th>
-												<EditWrapper stringId="STAKE_TABLE.REWARD_RATE">
-													{STRINGS['STAKE_TABLE.REWARD_RATE']}
-												</EditWrapper>
-											</th>
-											<th>
-												<EditWrapper stringId="STAKE_TABLE.EARNING">
-													{STRINGS['STAKE_TABLE.EARNINGS']}
-												</EditWrapper>
-											</th>
-											<th>
-												<EditWrapper stringId="STAKE_TABLE.STAKE">
-													{STRINGS['STAKE_TABLE.STAKE']}
-												</EditWrapper>
-											</th>
-										</tr>
-									</thead>
-									<tbody>
-										{stakables.map((tokenData, index) => {
-											const { symbol } = tokenData;
-											const { fullname, icon_id, display_name } = coins[symbol];
-											const commonCellProps = {};
-											return (
-												<tr
-													className="hoverable pointer table-row table-bottom-border"
-													key={index}
-												>
-													<td />
-													<td className="td-name td-fit">
-														<div className="d-flex align-items-center">
-															<Coin iconId={icon_id} />
-															<div className="px-2">
-																{fullname}
-																<span className="pl-2 secondary-text">
-																	{display_name}
-																</span>
+								<StakesAndEarnings />
+								<div className="stake-table">
+									<table className="wallet-assets_block-table">
+										<thead>
+											<tr className="table-bottom-border">
+												<th />
+												<th>
+													<EditWrapper stringId="STAKE_TABLE.CURRENCY">
+														{STRINGS['STAKE_TABLE.CURRENCY']}
+													</EditWrapper>
+												</th>
+												<th>
+													<EditWrapper stringId="STAKE_TABLE.AVAILABLE">
+														{STRINGS['STAKE_TABLE.AVAILABLE']}
+													</EditWrapper>
+												</th>
+												<th>
+													<EditWrapper stringId="STAKE_TABLE.TOTAL">
+														{STRINGS['STAKE_TABLE.TOTAL']}
+													</EditWrapper>
+												</th>
+												<th>
+													<EditWrapper stringId="STAKE_TABLE.REWARD_RATE">
+														{STRINGS['STAKE_TABLE.REWARD_RATE']}
+													</EditWrapper>
+												</th>
+												<th>
+													<EditWrapper stringId="STAKE_TABLE.EARNING">
+														{STRINGS['STAKE_TABLE.EARNINGS']}
+													</EditWrapper>
+												</th>
+												<th>
+													<EditWrapper stringId="STAKE_TABLE.STAKE">
+														{STRINGS['STAKE_TABLE.STAKE']}
+													</EditWrapper>
+												</th>
+											</tr>
+										</thead>
+										<tbody>
+											{stakables.map((tokenData, index) => {
+												const { symbol } = tokenData;
+												const { fullname, icon_id, display_name } = coins[
+													symbol
+												];
+												const commonCellProps = {};
+												return (
+													<tr
+														className="hoverable pointer table-row table-bottom-border"
+														key={index}
+													>
+														<td />
+														<td className="td-name td-fit">
+															<div className="d-flex align-items-center justify-content-center">
+																<Coin iconId={icon_id} />
+																<div className="px-2">
+																	{fullname}
+																	<span className="pl-2 secondary-text">
+																		{display_name}
+																	</span>
+																</div>
 															</div>
-														</div>
-													</td>
-													<td {...commonCellProps}>
-														<ConnectWrapper onClick={openConnectViaDesktop} />
-													</td>
-													<td {...commonCellProps}>
-														<ConnectWrapper onClick={openConnectViaDesktop} />
-													</td>
-													<td {...commonCellProps}>
-														<ConnectWrapper onClick={openConnectViaDesktop} />
-													</td>
-													<td {...commonCellProps}>
-														<ConnectWrapper onClick={openConnectViaDesktop} />
-													</td>
-													<td>
-														<div className="d-flex">
-															<AntBtn
-																className="stake-btn caps"
-																type="primary"
-																ghost
-																onClick={() => {}}
-																disabled={true}
-															>
-																{STRINGS['STAKE_TABLE.STAKE']}
-															</AntBtn>
-														</div>
-													</td>
-												</tr>
-											);
-										})}
-									</tbody>
-								</table>
+														</td>
+														<td {...commonCellProps}>
+															<ConnectWrapper onClick={openConnectViaDesktop} />
+														</td>
+														<td {...commonCellProps}>
+															<ConnectWrapper onClick={openConnectViaDesktop} />
+														</td>
+														<td {...commonCellProps}>
+															<ConnectWrapper onClick={openConnectViaDesktop} />
+														</td>
+														<td {...commonCellProps}>
+															<ConnectWrapper onClick={openConnectViaDesktop} />
+														</td>
+														<td>
+															<div className="d-flex align-items-center justify-content-center">
+																<AntBtn
+																	className="stake-btn caps"
+																	type="primary"
+																	ghost
+																	onClick={() => {}}
+																	disabled={true}
+																>
+																	{STRINGS['STAKE_TABLE.STAKE']}
+																</AntBtn>
+															</div>
+														</td>
+													</tr>
+												);
+											})}
+										</tbody>
+									</table>
+								</div>
 							</div>
 						</div>
 						<div className="btn-wrapper">
