a0a3a2b6e02479a451bf3414961e962b4dfe73c8
fetok12 <fetok12@gmail.com>
2024-06-26 10:57:43 +0300

    check-balance-fix-condition

--- server/utils/hollaex-tools-lib/tools/p2p.js
+++ server/utils/hollaex-tools-lib/tools/p2p.js
@@ -187,7 +187,7 @@ const getP2PAccountBalance = async (account_id, coin) => {
     let symbols = {};
 
     for (const key of Object.keys(balance)) {
-        if (key.includes('available') && balance[key]) {
+        if (key.includes('available') && balance[key] != null) {
             let symbol = key?.split('_')?.[0];
             symbols[symbol] = balance[key];
         }
