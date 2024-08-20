281f34e4782ac6567e243da7ea73e87e052d0984
ram <ram@hollaex.com>
2024-08-20 18:00:22 +0530

    Changes for the new assets page

--- web/src/containers/DigitalAssets/components/AssetsRow.js
+++ web/src/containers/DigitalAssets/components/AssetsRow.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { PriceChange, Coin, ActionNotification } from 'components';
+import { connect } from 'react-redux';
+
 import TradeInputGroup from 'containers/Wallet/components/TradeInputGroup';
+import STRINGS from 'config/localizedStrings';
+import { PriceChange, Coin, ActionNotification } from 'components';
 import { MiniSparkLine } from 'containers/TradeTabs/components/MiniSparkLine';
 import { getLastValuesFromParts } from 'utils/array';
-import STRINGS from 'config/localizedStrings';
 import { isMobile } from 'react-device-detect';
 import { browserHistory } from 'react-router';
 import { unique } from 'utils/data';
@@ -19,7 +21,15 @@ const Loading = ({ index }) => {
 	);
 };
 
-const AssetsRow = ({ coinData, loading, index, quicktrade, pairs, icons }) => {
+const AssetsRow = ({
+	coinData,
+	loading,
+	index,
+	quicktrade,
+	pairs,
+	icons,
+	coins,
+}) => {
 	const {
 		icon_id,
 		symbol,
@@ -98,73 +108,185 @@ const AssetsRow = ({ coinData, loading, index, quicktrade, pairs, icons }) => {
 		<tr id={`market-list-row-${key}`} className="table-row table-bottom-border">
 			<td className="sticky-col">
 				{!loading ? (
-					<div
-						className="d-flex align-items-center"
-						onClick={() => browserHistory.push(`/prices/coin/${symbol}`)}
-					>
-						<Coin iconId={icon_id} />
-						<div className="px-2">{fullname}</div>
-					</div>
+					isMobile ? (
+						<div
+							className="d-flex align-items-center"
+							onClick={() => browserHistory.push(`/prices/coin/${symbol}`)}
+						>
+							<Coin iconId={icon_id} type="CS10" />
+							<div className="px-2 ml-2 d-flex flex-column table-asset-container">
+								<span className="asset-text">{symbol?.toUpperCase()}</span>
+								<span className="coin-symbol">{fullname}</span>
+							</div>
+						</div>
+					) : (
+						<div
+							className="d-flex align-items-center"
+							onClick={() => browserHistory.push(`/prices/coin/${symbol}`)}
+						>
+							<Coin iconId={icon_id} />
+							<div className="px-2">
+								<span className="asset-text">{fullname}</span>
+								<span className="ml-2 coin-symbol">
+									{symbol?.toUpperCase()}
+								</span>
+							</div>
+						</div>
+					)
 				) : (
 					<Loading index={index} />
 				)}
 			</td>
-			<td>
-				{!loading ? (
-					<div className="ml-1">{symbol.toUpperCase()}</div>
-				) : (
-					<Loading index={index} />
-				)}
-			</td>
-			<td>
-				{!loading ? (
-					<div>
-						<span className="title-font ml-1 last-price-label">
-							{lastPrice}
-						</span>
-						<span className="title-font ml-2">{'USDT'}</span>
-					</div>
-				) : (
-					<Loading index={index} />
-				)}
-			</td>
-			<td>
-				{!loading ? (
-					<PriceChange
-						market={{
-							priceDifference: oneDayPriceDifference,
-							priceDifferencePercent: oneDayPriceDifferencePercent,
-						}}
-						key={key}
-					/>
-				) : (
-					<Loading index={index} />
-				)}
-			</td>
-			<td>
-				{!loading ? (
-					<PriceChange
-						market={{
-							priceDifference: priceDifference,
-							priceDifferencePercent: priceDifferencePercent,
-						}}
-						key={key}
-					/>
-				) : (
-					<Loading index={index} />
-				)}
-			</td>
-			<td className="pr-4">
-				{!loading ? (
-					<MiniSparkLine
-						chartData={getLastValuesFromParts(chartData?.price || [])}
-						isArea
-					/>
-				) : (
-					<Loading index={index} />
-				)}
-			</td>
-			<td className="td-trade align-items-center justify-content-between">
+			{isMobile && (
+				<td>
+					{!loading ? (
+						<div>
+							<div className="d-flex justify-content-end">
+								<span className="title-font last-price-label">
+									{lastPrice && '$'}
+								</span>
+								<span className="title-font last-price-label">
+									{lastPrice ? lastPrice : '-'}
+								</span>
+							</div>
+							{(oneDayPriceDifferencePercent && oneDayPriceDifference) ||
+							oneDayPriceDifference === 0 ? (
+								<PriceChange
+									market={{
+										priceDifference: oneDayPriceDifference,
+										priceDifferencePercent: oneDayPriceDifferencePercent,
+									}}
+									key={key}
+								/>
+							) : (
+								<span className="d-flex justify-content-end"> {'0%'}</span>
+							)}
+						</div>
+					) : (
+						<Loading index={index} />
+					)}
+				</td>
+			)}
+			{isMobile && (
+				<td>
+					{!loading ? (
+						<div className="d-flex flex-column justify-content-end">
+							{(priceDifference && priceDifferencePercent) ||
+							priceDifference === 0 ? (
+								<PriceChange
+									market={{
+										priceDifference: priceDifference,
+										priceDifferencePercent: priceDifferencePercent,
+									}}
+									key={key}
+									isMobileMarket={true}
+								/>
+							) : (
+								<span> {'0%'}</span>
+							)}
+							{chartData?.price ? (
+								<MiniSparkLine
+									chartData={getLastValuesFromParts(chartData?.price || [])}
+									isArea
+								/>
+							) : (
+								<span> {'- '}</span>
+							)}
+						</div>
+					) : (
+						<Loading index={index} />
+					)}
+				</td>
+			)}
+			{!isMobile && (
+				<td>
+					{!loading ? (
+						<div className="d-flex justify-content-end">
+							<span className="title-font last-price-label">
+								{lastPrice && '$'}
+							</span>
+							<span className="title-font last-price-label">
+								{lastPrice ? lastPrice : '-'}
+							</span>
+						</div>
+					) : (
+						<Loading index={index} />
+					)}
+				</td>
+			)}
+			{!isMobile && (
+				<td>
+					{!loading ? (
+						(oneDayPriceDifferencePercent && oneDayPriceDifference) ||
+						oneDayPriceDifference === 0 ? (
+							<PriceChange
+								market={{
+									priceDifference: oneDayPriceDifference,
+									priceDifferencePercent: oneDayPriceDifferencePercent,
+								}}
+								key={key}
+							/>
+						) : (
+							'0%'
+						)
+					) : (
+						<Loading index={index} />
+					)}
+				</td>
+			)}
+			{!isMobile && (
+				<td>
+					{!loading ? (
+						(priceDifference && priceDifferencePercent) ||
+						priceDifference === 0 ? (
+							<PriceChange
+								market={{
+									priceDifference: priceDifference,
+									priceDifferencePercent: priceDifferencePercent,
+								}}
+								key={key}
+							/>
+						) : (
+							'0%'
+						)
+					) : (
+						<Loading index={index} />
+					)}
+				</td>
+			)}
+			{!isMobile && (
+				<td className="market-chart">
+					{!loading ? (
+						chartData?.price ? (
+							<MiniSparkLine
+								chartData={getLastValuesFromParts(chartData?.price || [])}
+								isArea
+							/>
+						) : (
+							'-'
+						)
+					) : (
+						<Loading index={index} />
+					)}
+				</td>
+			)}
+			{!isMobile && (
+				<td>
+					{!loading ? (
+						<div className="ml-1 market-capital mr-3">{`${
+							coins[symbol]?.market_cap
+								? coins[symbol]?.market_cap.toLocaleString('en-US', {
+										style: 'currency',
+										currency: 'USD',
+								  })
+								: '0'
+						}`}</div>
+					) : (
+						<Loading index={index} />
+					)}
+				</td>
+			)}
+			<td className="td-trade align-items-center justify-content-center">
 				{!loading ? (
 					markets.length > 1 ? (
 						<TradeInputGroup
@@ -172,17 +294,25 @@ const AssetsRow = ({ coinData, loading, index, quicktrade, pairs, icons }) => {
 							markets={markets}
 							goToTrade={goToTrade}
 							pairs={pairs}
+							tradeClassName="market-asset-row"
 						/>
 					) : (
 						<ActionNotification
-							stringId="TRADE_TAB_TRADE"
-							text={STRINGS['TRADE_TAB_TRADE']}
-							iconId="BLUE_TRADE_ICON"
-							iconPath={icons['BLUE_TRADE_ICON']}
+							stringId={STRINGS.formatString(
+								STRINGS['MARKETS_TABLE.BUY'],
+								<span>/</span>,
+								<span>{STRINGS['SIDES.SELL']}</span>
+							)}
+							text={STRINGS.formatString(
+								STRINGS['MARKETS_TABLE.BUY'],
+								<span>/</span>,
+								<span>{STRINGS['SIDES.SELL']}</span>
+							)}
 							onClick={() => goToTrade(markets[0])}
 							className="csv-action"
 							showActionText={isMobile}
 							disable={markets.length === 0}
+							tradeClassName="market-asset-row"
 						/>
 					)
 				) : (
@@ -193,4 +323,8 @@ const AssetsRow = ({ coinData, loading, index, quicktrade, pairs, icons }) => {
 	);
 };
 
-export default AssetsRow;
+const mapStateToProps = (state) => ({
+	coins: state.app.coins,
+});
+
+export default connect(mapStateToProps)(AssetsRow);
