c4a2f0703bfa9378d3efb2c05a2869e2bd982ec5
fetok12 <fetok12@gmail.com>
2024-10-01 18:54:28 +0300

    temporary-enable-source-map

--- web/src/components/AppBar/Connections.js
+++ web/src/components/AppBar/Connections.js
@@ -27,21 +27,6 @@ const Connections = () => {
 		isDisplayPing: false,
 	});
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				await requestLogins();
-				await fetchHealthData();
-			} catch (error) {
-				setHasResponseData(false);
-				console.error('Error fetching data:', error);
-			}
-		};
-
-		fetchData();
-		//eslint-disable-next-line
-	}, [fetchHealthData]);
-
 	const requestLogins = useCallback((page = 1) => {
 		getLogins({ page })
 			.then(({ data: { count, data } }) => {
@@ -73,6 +58,21 @@ const Connections = () => {
 			setIsLoading(false);
 		}
 	};
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				await requestLogins();
+				await fetchHealthData();
+			} catch (error) {
+				setHasResponseData(false);
+				console.error('Error fetching data:', error);
+			}
+		};
+
+		fetchData();
+		//eslint-disable-next-line
+	}, [fetchHealthData]);
 
 	const onHandleClose = (value) => {
 		if (value === 'connection') {
